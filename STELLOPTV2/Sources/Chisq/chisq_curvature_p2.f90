76e11421931662a6c4bca1aae71d4b2e6f3bcb39
aaroncbader <abader@engr.wisc.edu>
2018-04-19 09:47:17 -0400

    fix formatting to align with Sam's convention

--- STELLOPTV2/Sources/Chisq/chisq_curvature_p2.f90
+++ STELLOPTV2/Sources/Chisq/chisq_curvature_p2.f90
@@ -5,155 +5,153 @@
 !     Description:   This subroutine targets the second principal
 !           curvature to reduce concave regions
 !-----------------------------------------------------------------------
-SUBROUTINE chisq_curvature_P2(target,sigma,niter,iflag)
-  !-----------------------------------------------------------------------
-  !     Libraries
-  !-----------------------------------------------------------------------
+      SUBROUTINE chisq_curvature_P2(target,sigma,niter,iflag)
+      !-----------------------------------------------------------------------
+      !     Libraries
+      !-----------------------------------------------------------------------
 
-  USE stellopt_runtime
-  USE stellopt_targets  
-  USE read_wout_mod, ONLY: m => xm, n => xn, &
-       rmnc_full => rmnc, zmns_full => zmns, nfp, &
-       num_coeffs => mnmax, ns
+      USE stellopt_runtime
+      USE stellopt_targets  
+      USE read_wout_mod, ONLY: m => xm, n => xn, &
+          rmnc_full => rmnc, zmns_full => zmns, nfp, &
+          num_coeffs => mnmax, ns
 
-  !-----------------------------------------------------------------------
-  !     Input/Output Variables
-  !
-  !-----------------------------------------------------------------------
-  IMPLICIT NONE
-  REAL(rprec), INTENT(in)    ::  target
-  REAL(rprec), INTENT(in)    ::  sigma
-  INTEGER,     INTENT(in)    ::  niter
-  INTEGER,     INTENT(inout) ::  iflag
+      !-----------------------------------------------------------------------
+      !     Input/Output Variables
+      !
+      !-----------------------------------------------------------------------
+      IMPLICIT NONE
+      REAL(rprec), INTENT(in)    ::  target
+      REAL(rprec), INTENT(in)    ::  sigma
+      INTEGER,     INTENT(in)    ::  niter
+      INTEGER,     INTENT(inout) ::  iflag
 
-  integer, parameter :: np=128, nt=128 !hard code resolution for now
-  real(rprec), dimension(np, nt) :: x,y,z,r
-  real(rprec), dimension(np, nt) :: xu,xv,yu,yv,zu,zv !first derivs
-  real(rprec), dimension(np, nt) :: xuu, xuv, yuu, yuv, zuu, zuv !2nd d
-  real(rprec), dimension(np, nt) :: xvv, yvv, zvv !2nd d
-  real(rprec), dimension(np, nt) :: EE,FF,GG,Nx,Ny,Nz,magnorm
-  real(rprec), dimension(np, nt) :: LL,MM,NN
-  real(rprec), dimension(np, nt) ::HH,KK,P1,P2
-  real(rprec), dimension(num_coeffs) :: rmnc, zmns
-  real(rprec) :: cosangle, sinangle, cosphi, sinphi
-  integer :: i,j,k
-  real(rprec) :: u,v, angle, phi
-  !real(rprec), parameter :: pi = 3.141592653589793
+      INTEGER, parameter :: np=128, nt=128 !hard code resolution for now
+      REAL(rprec), DIMENSION(np, nt) :: x,y,z,r
+      REAL(rprec), DIMENSION(np, nt) :: xu,xv,yu,yv,zu,zv !first derivs
+      REAL(rprec), DIMENSION(np, nt) :: xuu, xuv, yuu, yuv, zuu, zuv !2nd d
+      REAL(rprec), DIMENSION(np, nt) :: xvv, yvv, zvv !2nd d
+      REAL(rprec), DIMENSION(np, nt) :: EE,FF,GG,Nx,Ny,Nz,magnorm
+      REAL(rprec), DIMENSION(np, nt) :: LL,MM,NN
+      REAL(rprec), DIMENSION(np, nt) ::HH,KK,P1,P2
+      REAL(rprec), DIMENSION(num_coeffs) :: rmnc, zmns
+      REAL(rprec) :: cosangle, sinangle, cosphi, sinphi
+      INTEGER :: i,j,k
+      REAL(rprec) :: u,v, angle, phi
 
 
-  !----------------BEGIN SUBROUTINE --------------
+      !----------------BEGIN SUBROUTINE --------------
 
-  if (iflag < 0) return
-  IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'P2     ',1,3
-  IF (iflag == 1) WRITE(iunit_out,'(A)') 'TARGET SIGMA P2    '
-  if (niter >= 0) then
-    !Only consider the boundary
-    rmnc = rmnc_full(:, ns)
-    zmns = zmns_full(:, ns)
+      IF (iflag < 0) RETURN
+      IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'P2     ',1,3
+      IF (niter >= 0) THEN
+        !Only consider the boundary
+        rmnc = rmnc_full(:, ns)
+        zmns = zmns_full(:, ns)
 
-     !---------------calculate x,y,z, and derivs--------------!
-     !initialize
-     r = 0
-     x = 0
-     y = 0
-     z = 0
-     xu = 0
-     xv = 0
-     xuu = 0
-     xuv = 0
-     xvv = 0
-     yu = 0
-     yv = 0
-     yuu = 0
-     yuv = 0
-     yvv = 0
-     zu = 0
-     zv = 0
-     zuu = 0
-     zuv = 0
-     zvv = 0
+        !---------------calculate x,y,z, and derivs--------------!
+        !initialize
+        r = 0
+        x = 0
+        y = 0
+        z = 0
+        xu = 0
+        xv = 0
+        xuu = 0
+        xuv = 0
+        xvv = 0
+        yu = 0
+        yv = 0
+        yuu = 0
+        yuv = 0
+        yvv = 0
+        zu = 0
+        zv = 0
+        zuu = 0
+        zuv = 0
+        zvv = 0
 
-     do i = 1,np
-        u = real(i-1)/(np - 1)*2*pi
-        do j = 1,nt
-           v = real(j-1)/(nt - 1)*2*pi
-           cosphi = cos(v)
-           sinphi = sin(v)
-           do k = 1,num_coeffs
-              angle = (m(k)*u - n(k)*v)
-              sinangle = sin(angle)
-              cosangle = cos(angle)
+        DO i = 1,np
+            u = REAL(i-1)/(np - 1)*2*pi
+            DO j = 1,nt
+                v = REAL(j-1)/(nt - 1)*2*pi
+                cosphi = cos(v)
+                sinphi = sin(v)
+                DO k = 1,num_coeffs
+                    angle = (m(k)*u - n(k)*v)
+                    sinangle = sin(angle)
+                    cosangle = cos(angle)
               
               
-              !X derivatives
-              xu(i,j) = xu(i,j) - rmnc(k)*sinangle*cosphi*m(k)
+                    !X derivatives
+                    xu(i,j) = xu(i,j) - rmnc(k)*sinangle*cosphi*m(k)
              
 
-              xv(i,j) = xv(i,j) + rmnc(k)*(sinangle*cosphi*n(k) - &
-                   cosangle*sinphi)
-              xuu(i,j) = xuu(i,j) - rmnc(k)*cosangle*cosphi*m(k)*m(k)
-              xuv(i,j) = xuv(i,j) + rmnc(k)*(cosangle*cosphi*m(k)*n(k) + &
-                   sinangle*sinphi*m(k))
-              xvv(i,j) = xvv(i,j) - rmnc(k)*(2*sinangle*sinphi*n(k) + &
-                   cosangle*cosphi*(n(k)*n(k) + 1))
-              !Y derivatives
-              yu(i,j) = yu(i,j) - rmnc(k)*sinangle*sinphi*m(k)
-              yv(i,j) = yv(i,j) + rmnc(k)*(sinangle*sinphi*n(k) + &
-                   cosangle*cosphi)
-              yuu(i,j) = yuu(i,j) - rmnc(k)*cosangle*sinphi*m(k)*m(k)
-              yuv(i,j) = yuv(i,j) + rmnc(k)*(cosangle*sinphi*m(k)*n(k) - &
-                   sinangle*cosphi*m(k))
-              yvv(i,j) = yvv(i,j) + rmnc(k)*(2*sinangle*cosphi*n(k) - &
-                   cosangle*sinphi*(n(k)*n(k) + 1))
-              !Z derivatives
-              zu(i,j) = zu(i,j) + zmns(k)*cosangle*m(k)
-              zv(i,j) = zv(i,j) - zmns(k)*cosangle*n(k)
-              zuu(i,j) = zuu(i,j) - zmns(k)*sinangle*m(k)*m(k)
-              zuv(i,j) = zuv(i,j) + zmns(k)*sinangle*m(k)*n(k)
-              zvv(i,j) = zvv(i,j) - zmns(k)*sinangle*n(k)*n(k)
+                    xv(i,j) = xv(i,j) + rmnc(k)*(sinangle*cosphi*n(k) - &
+                        cosangle*sinphi)
+                    xuu(i,j) = xuu(i,j) - rmnc(k)*cosangle*cosphi*m(k)*m(k)
+                    xuv(i,j) = xuv(i,j) + rmnc(k)*(cosangle*cosphi*m(k)*n(k) + &
+                        sinangle*sinphi*m(k))
+                    xvv(i,j) = xvv(i,j) - rmnc(k)*(2*sinangle*sinphi*n(k) + &
+                        cosangle*cosphi*(n(k)*n(k) + 1))
+                    !Y derivatives
+                    yu(i,j) = yu(i,j) - rmnc(k)*sinangle*sinphi*m(k)
+                    yv(i,j) = yv(i,j) + rmnc(k)*(sinangle*sinphi*n(k) + &
+                        cosangle*cosphi)
+                    yuu(i,j) = yuu(i,j) - rmnc(k)*cosangle*sinphi*m(k)*m(k)
+                    yuv(i,j) = yuv(i,j) + rmnc(k)*(cosangle*sinphi*m(k)*n(k) - &
+                        sinangle*cosphi*m(k))
+                    yvv(i,j) = yvv(i,j) + rmnc(k)*(2*sinangle*cosphi*n(k) - &
+                        cosangle*sinphi*(n(k)*n(k) + 1))
+                    !Z derivatives
+                    zu(i,j) = zu(i,j) + zmns(k)*cosangle*m(k)
+                    zv(i,j) = zv(i,j) - zmns(k)*cosangle*n(k)
+                    zuu(i,j) = zuu(i,j) - zmns(k)*sinangle*m(k)*m(k)
+                    zuv(i,j) = zuv(i,j) + zmns(k)*sinangle*m(k)*n(k)
+                    zvv(i,j) = zvv(i,j) - zmns(k)*sinangle*n(k)*n(k)
 
 
 
-           end do
+                end do
+            end do
         end do
-     end do
 
      
      
      
-     !---------------Curvature calculation-----------------------!
-     EE = xu*xu + yu*yu + zu*zu
-     FF = xu*xv + yu*yv + zu*zv
-     GG = xv*xv + yv*yv + zv*zv
+        !---------------Curvature calculation-----------------------!
+        EE = xu*xu + yu*yu + zu*zu
+        FF = xu*xv + yu*yv + zu*zv
+        GG = xv*xv + yv*yv + zv*zv
 
-     Nx = yu*zv - zu*yv
-     Ny = zu*xv - xu*zv
-     Nz = xu*yv - yu*xv
-     magnorm = sqrt(Nx*Nx + Ny*Ny + Nz*Nz)
-     Nx = Nx/magnorm
-     Ny = Ny/magnorm
-     Nz = Nz/magnorm
+        Nx = yu*zv - zu*yv
+        Ny = zu*xv - xu*zv
+        Nz = xu*yv - yu*xv
+        magnorm = sqrt(Nx*Nx + Ny*Ny + Nz*Nz)
+        Nx = Nx/magnorm
+        Ny = Ny/magnorm
+        Nz = Nz/magnorm
 
-     LL = xuu*Nx + yuu*Ny + zuu*Nz
-     MM = xuv*Nx + yuv*Ny + zuv*Nz
-     NN = xvv*Nx + yvv*Ny + zvv*Nz
+        LL = xuu*Nx + yuu*Ny + zuu*Nz
+        MM = xuv*Nx + yuv*Ny + zuv*Nz
+        NN = xvv*Nx + yvv*Ny + zvv*Nz
 
-     HH = (EE*NN +GG*LL - 2*FF*MM)/(2*(EE*GG - FF*FF))
-     KK = (LL*NN - MM*MM)/(EE*GG - FF*FF)
-     P1 = HH + sqrt(HH*HH - KK)
-     P2 = HH - sqrt(HH*HH - KK)
+        HH = (EE*NN +GG*LL - 2*FF*MM)/(2*(EE*GG - FF*FF))
+        KK = (LL*NN - MM*MM)/(EE*GG - FF*FF)
+        P1 = HH + sqrt(HH*HH - KK)
+        P2 = HH - sqrt(HH*HH - KK)
 
-     mtargets = mtargets + 1
-     targets(mtargets) = target
-     sigmas(mtargets) = sigma
-     vals(mtargets) = -1*minval(P2)
-     IF (iflag == 1) WRITE(iunit_out,'(3ES22.12E3)') target,sigma,-1*minval(P2)
-
-  else
-     if (sigma < bigno) then
         mtargets = mtargets + 1
-        if (niter == -2) target_dex(mtargets)=jtarget_curvature_P2
-     end if
-  end if
-  return
-end subroutine chisq_curvature_P2
+        targets(mtargets) = target
+        sigmas(mtargets) = sigma
+        vals(mtargets) = -1*minval(P2)
+        IF (iflag == 1) WRITE(iunit_out,'(3ES22.12E3)') target,sigma,-1*minval(P2)
+
+      ELSE
+        IF (sigma < bigno) THEN
+            mtargets = mtargets + 1
+            IF (niter == -2) target_dex(mtargets)=jtarget_curvature_P2
+        END IF
+      END IF
+      RETURN
+      END SUBROUTINE chisq_curvature_P2
