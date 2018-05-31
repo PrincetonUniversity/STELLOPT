35b2573b28b17295e5d2b6e1f683aaa8d9d12b0c
aaroncbader <abader@engr.wisc.edu>
2018-05-31 16:38:32 -0400

    implement optional output in chisq function

--- STELLOPTV2/Sources/Chisq/chisq_gs2_ptsm3d.f90
+++ STELLOPTV2/Sources/Chisq/chisq_gs2_ptsm3d.f90
@@ -13,6 +13,7 @@
       USE stellopt_runtime
       USE stellopt_targets  
       USE vmec2gs2_mod
+      USE safe_open_mod
       !-----------------------------------------------------------------------
       !     Input/Output Variables
       !
@@ -22,14 +23,15 @@
       REAL(rprec), INTENT(in)    ::  sigma
       INTEGER,     INTENT(in)    ::  niter
       INTEGER,     INTENT(inout) ::  iflag
-      INTEGER                    :: nalpha, nzgrid, vmec_option
+      INTEGER                    :: nalpha, nzgrid, vmec_option, i, j
+      INTEGER                    :: iunit, ierr
       REAL(rprec)                :: s, zeta_center, periods
-      LOGICAL                    :: verbose
+      LOGICAL                    :: verbose, should_print_gs2
       REAL(rprec)                :: s_used, q, shat, L_ref, B_ref
       REAL(rprec), DIMENSION(:), ALLOCATABLE :: alpha, zeta
       REAL(rprec), DIMENSION(:,:), ALLOCATABLE :: bmag, gradpar, &
                           gds2, gds21, gds22, gbdrift, gbdrift0, cvdrift, &
-                          cvdrift0, sqrt_g
+                          cvdrift0, sqrt_jac
 
       !----------------BEGIN SUBROUTINE --------------
 
@@ -43,6 +45,8 @@
       zeta_center = 0.0
       vmec_option = 0
       verbose = .false.
+      should_print_gs2 = .true.
+      iunit = 7276 !Figure out what number I should actually use later
 
       ALLOCATE(alpha(nalpha))
       ALLOCATE(zeta(-nzgrid:nzgrid))
@@ -55,7 +59,7 @@
       ALLOCATE(gbdrift0(nalpha, -nzgrid:nzgrid))
       ALLOCATE(cvdrift(nalpha, -nzgrid:nzgrid))
       ALLOCATE(cvdrift0(nalpha, -nzgrid:nzgrid))
-      ALLOCATE(sqrt_g(nalpha, -nzgrid:nzgrid))
+      ALLOCATE(sqrt_jac(nalpha, -nzgrid:nzgrid))
 
 
       IF (iflag < 0) RETURN
@@ -66,7 +70,33 @@
         CALL vmec2gs2('wout_'//TRIM(PROC_STRING)//'.nc', nalpha, nzgrid,&
              zeta_center, periods, s, vmec_option, verbose, s_used, q, shat, &
              L_ref, B_ref, alpha, zeta, bmag, gradpar, gds2, gds21, gds22, & 
-             gbdrift, gbdrift0, cvdrift, cvdrift0, sqrt_g)
+             gbdrift, gbdrift0, cvdrift, cvdrift0, sqrt_jac)
+        write (*,*) 'Calculated gs2 coordinates'
+        IF (should_print_gs2) THEN
+            CALL safe_open(iunit, ierr, 'gs2_stell_'//TRIM(PROC_STRING)//'.txt', &
+                           'unknown', 'formatted')
+
+            !Write the header
+            DO i = 1,nalpha
+                WRITE(iunit, "(A)") "&parameters"
+                WRITE(iunit, "(A,2F12.7)") "s0 = ",s
+                WRITE(iunit, "(A,2F12.7)") "minor_a = ",L_ref
+                WRITE(iunit, "(A,5F12.7)") "Bref = ",B_ref
+                WRITE(iunit, "(A,F12.7)") "q0 = ",ABS(q)
+                WRITE(iunit, "(A,F12.7)") "shat = ",shat
+                WRITE(iunit, "(A,I6)") "gridpoints = ",nzgrid
+                WRITE(iunit, "(A,F12.7)") "n_pol = ",periods
+                WRITE(iunit, "(A)") "/"
+                DO j = -nzgrid,nzgrid
+                    WRITE(iunit, "(9ES22.12E3)") gds2(i,j), gds21(i,j), &
+                          gds22(i,j), bmag(i,j), sqrt_jac(i,j), cvdrift(i,j), &
+                          cvdrift0(i,j), gradpar(i,j), zeta(j)
+                END DO
+            END DO
+            CLOSE(iunit)
+        END IF
+            
+             
         mtargets = mtargets + 1
         targets(mtargets) = target
         sigmas(mtargets) = sigma
@@ -92,6 +122,6 @@
       DEALLOCATE(gbdrift0)
       DEALLOCATE(cvdrift)
       DEALLOCATE(cvdrift0)
-      DEALLOCATE(sqrt_g)
+      DEALLOCATE(sqrt_jac)
 
       END SUBROUTINE chisq_gs2_ptsm3d
