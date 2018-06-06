03a9fadde20346311777eae5255b5647ddbd6359
aaroncbader <abader@engr.wisc.edu>
2018-06-06 09:43:58 -0400

    remove old calculation from stellopt_ptsm3d

--- STELLOPTV2/Sources/General/stellopt_ptsm3d.f90
+++ STELLOPTV2/Sources/General/stellopt_ptsm3d.f90
@@ -38,20 +38,14 @@
 !     Local variables
 !
 !----------------------------------------------------------------------
-      INTEGER :: maxPnt, nalpha0_, ialpha, i, iunit, ik, ier, ncnt 
+      INTEGER :: maxPnt, nalpha0_, i, iunit, ier, ncnt 
       INTEGER :: j, k, global_npol, nzgrid, nalpha, vmec_option
-      REAL(rprec) :: a, s, Ba, Fa, iot, iotp, qprim, pval, pprime
-      REAL(rprec) :: dalpha, alpha0_start_, phi0, th, jac1, c
-      REAL(rprec) :: gaa, gat, gst, gsa, gss, thetastar
-      REAL(rprec) :: dBds, dBda, q, dpdx, sqrtg, absb, u, v, R, Z
-      REAL(rprec) :: temp1, temp2, temp3, abserr, alpha0_end
-      REAL(rprec) :: alpha0_start, maxTheta, zeta_center, s_used
+      REAL(rprec) :: a, s, Ba, Fa, iot, iotp, q, qprim
+      REAL(rprec) :: dpdx, maxTheta, pval, pprime
+      REAL(rprec) :: zeta_center, s_used
       REAL(rprec) :: L_ref, B_ref, periods
       REAL(rprec), DIMENSION(:), ALLOCATABLE :: g11,g12,g22,Bhat,abs_jac,L1,L2, &
                                                 dBdt,zeta,alpha
-      REAL(rprec), DIMENSION(3) :: sflCrd0,sflCrd, sflCrd_sav, gradS,gradThetaStar,&
-                                   gradPhi,mag,gradAlpha, wrk, gradB,R_grad,Z_grad,&
-                                   esubs, esubu, esubv, es, eu, ev, gradlam, ea, et
       REAL(rprec), DIMENSION(:,:), ALLOCATABLE :: bmag, gradpar, gds2, gds21, &
          gds22, gbdrift, gbdrift0, cvdrift, cvdrift0, jac_gist_inv, d_B_d_par
       character(len=128) :: temp_str, gist_filename, num_str
@@ -131,7 +125,7 @@
 
 
 
-
+      !Call Matt's program to calculate the quantities on a gs2 grid
       CALL vmec2gs2('wout_'//TRIM(PROC_STRING)//'.nc', nalpha, nzgrid,&
              zeta_center, periods, s, vmec_option, verbose, s_used, q, shat, &
              L_ref, B_ref, alpha, zeta, bmag, gradpar, gds2, gds21, gds22, & 
@@ -178,127 +172,8 @@
 
       CALL PTSM3D_initialize_itg_solve
 
-      
-!      sflCrd0(1) = s
-!      sflCrd0(2) = 0.0 
-!      phi0 = 0.0
-!      sflCrd0(3) = phi0
-!      DO i = 1, maxPnt ! Loop over field line
-!        th = -maxTheta + (i-1)*dtheta
-!        sflCrd(1) = sflCrd0(1)
-!        sflCrd(2) = th
-!        sflCrd(3) = sflCrd0(3) + q*(th-sflCrd0(2))
-!        ! Get Metric Elements
-!        sflCrd_sav = sflCrd
-!        CALL pest2vmec(sflCrd) ! Returns on 2pi grid
-!        u = sflCrd(2)
-!        v = sflCrd(3)
-!        IF (u < 0) THEN
-!           u = -MOD(ABS(u),pi2)
-!           u = u + pi2
-!        END IF
-!        IF (v < 0) THEN
-!           v = -MOD(ABS(v),pi2)
-!           v = v + pi2
-!        END IF
-!        IF (u > pi2) u = MOD(u,pi2)
-!        IF (v > pi2) v = MOD(v,pi2)
-!        CALL get_equil_RZ(sflCrd(1),u,v,R,Z,ier,R_GRAD=R_grad,Z_GRAD=Z_grad)
-!        ! Get equil_RZ returns dR/drho and dZ/drho
-!        !   dR/ds=(0.5/rho)*dR/drho=(0.5/sqrt(s))*dR/drho
-!        R_grad(3) = 0.5*R_grad(3)/SQRT(sflCrd(1))
-!        Z_grad(3) = 0.5*Z_grad(3)/SQRT(sflCrd(1))
-!        ! e_s
-!        esubs(1) = R_grad(3)
-!        esubs(2) = zero
-!        esubs(3) = Z_grad(3)
-!        ! e_u
-!        esubu(1) = R_grad(1)
-!        esubu(2) = zero
-!        esubu(3) = Z_grad(1)
-!        ! e_v
-!        esubv(1) = R_grad(2)
-!        esubv(2) = one
-!        esubv(3) = Z_grad(2) 
-!        esubv(1) = esubv(1)*nfp
-!        esubv(3) = esubv(3)*nfp
-!        ! Cylindrical Coordianates
-!        !CALL EZspline_interp(R_spl,u,v,sflCrd(1),R,iflag)
-!        esubs(2) = esubs(2)*R
-!        esubu(2) = esubu(2)*R
-!        esubv(2) = esubv(2)*R
-!        ! sqrt(g) = R*(Ru*Zs-Rs*Zu)
-!        sqrtg = R*(R_grad(1)*Z_grad(3)-R_grad(3)*Z_grad(1))
-!        ! e^i = (e_j x e_k)/sqrt(g)
-!        !CALL EZspline_interp(G_spl,u,v,sflCrd(1),sqrtg,iflag)
-!        CALL cross_product(esubu,esubv,es)
-!        CALL cross_product(esubv,esubs,eu)
-!        CALL cross_product(esubs,esubu,ev)
-!        es = es/sqrtg
-!        eu = eu/sqrtg
-!        ev = ev/sqrtg
-!        ! Get Field (before we adjust eu so gradB is correct)
-!        CALL get_equil_Bflx(sflCrd(1),u,v,temp1,temp2,temp3,ier,absb,gradB)
-!        gradB = gradB(3)*es + gradB(1)*eu + gradB(2)*ev*nfp
-!        ! Now Adjust e^u for lambda
-!        !IF (pest) THEN
-!           CALL get_equil_L(sflCrd(1),u,v,temp1,ier,gradlam)
-!           eu = eu + gradlam(3)*es + gradlam(1)*eu + gradlam(2)*ev*nfp
-!        !ELSE
-!        !END IF
-!        ! Now do some calculations
-!        sflCrd = sflCrd_sav
-!        thetastar = sflCrd(2)
-!        gradS = es
-!        gradThetaStar = eu
-!        gradPhi = ev
-!        gradAlpha = qprim * thetaStar*gradS+q*gradThetaStar-gradPhi
-!        alpha     = q*thetaStar - sflCrd(3)
-!        ! Metrice and Jacobian
-!        gss = DOT_PRODUCT(gradS,gradS)
-!        gsa = DOT_PRODUCT(gradS,gradAlpha)
-!        gst = DOT_PRODUCT(gradS,gradThetaStar)
-!        gaa = DOT_PRODUCT(gradAlpha,gradAlpha)
-!        gat = DOT_PRODUCT(gradAlpha,gradThetaStar)
-!        CALL cross_product(gradS,gradAlpha,wrk)
-!        jac1 = one/DOT_PRODUCT(wrk,gradThetaStar)
-!        
-!        Bhat = absb/Ba
-!        modB(i-1) = Bhat
-!
-!        g11 = gss*a**2/(4*s)
-!        !g12(ialpha,i) = gsa*a**2*iot/2*sloc_fac
-!        g12 = gsa*a**2*iot/2
-!        g22 = (Bhat**2+g12**2)/g11
-!        abs_jac = ABS(jac1*2*q/a**3)
-!        gxx(i-1) = g11
-!        gxy(i-1) = g12
-!        gyy(i-1) = g22
-!        jac(i-1) = minor_a/(Bref*q0)*abs_jac 
-!                 
-!        CALL cross_product(gradAlpha,gradThetaStar, es) 
-!        CALL cross_product(gradThetaStar,gradS,     ea) 
-!        CALL cross_product(gradS,gradAlpha,         et)  
-!        es(:) = es(:)*jac1
-!        ea(:) = ea(:)*jac1
-!        et(:) = et(:)*jac1
-!        gradB = gradB/Ba
-!        dBds = DOT_PRODUCT(gradB,es)
-!        dBda = DOT_PRODUCT(gradB,ea)
-!        dBdt = DOT_PRODUCT(gradB,et)
-!        
-!        c = iot*iot*a**4
-!        L1 = q/sqrt(s)*(dBda + c*(gss*gat-gsa*gst)*dBdt/(4*Bhat**2))
-!        L2 = two*sqrt(s)*(dBds + c*(gaa*gst-gsa*gat)*dBdt/(4*Bhat**2))
-!        dBdx(i-1) = L1
-!        dBdy(i-1) = L2
-!        IF (write_gist) THEN
-!          WRITE(iunit,"(9ES20.10)") g11,g12,g22,Bhat,&
-!          & abs_jac, L2, L1, th, 0.0; CALL FLUSH(iunit)
-!        END IF
-!        
-!      ENDDO ! End loop over field line
-      
+      ! Convert to GIST-GENE coordinates
+
       i = 1 !eventually we may allow for multiple s values
       g11 = gds22(i,:)/shat**2
       g12 = gds21(i,:)/shat
@@ -306,7 +181,7 @@
       Bhat = Bmag(i,:)
       abs_jac = 1.0/abs(jac_gist_inv(i,:))
       L2 = Bhat/2 * cvdrift(i,:)
-      L1 = Bhat/2/shat**2 * cvdrift0(i,:)
+      L1 = -Bhat/2/shat * cvdrift0(i,:)
       dBdt = d_B_d_par(i,:)
      
       DO j = -nzgrid,nzgrid
