e821f65881b293217544ce954f1f3ea59cd8d185
Samuel Lazerson <lazerson@pppl.gov>
2019-04-05 08:25:55 -0700

    Added feature to easily scale profiles in BEAMS3D

--- BEAMS3D/Sources/beams3d_runtime.f90
+++ BEAMS3D/Sources/beams3d_runtime.f90
@@ -106,7 +106,7 @@ MODULE beams3d_runtime
                ldepo, lbeam_simple, ldebug, lcollision, lw7x
     INTEGER :: nextcur, npoinc, nbeams, nparticles_start, nprocs_beams
     INTEGER, ALLOCATABLE :: beam(:)
-    REAL(rprec) :: dt, follow_tol, pi, pi2, mu0, to3, dt_save
+    REAL(rprec) :: dt, follow_tol, pi, pi2, mu0, to3, dt_save, ne_scale, te_scale, ti_scale, zeff_scale
     REAL(rprec), DIMENSION(MAXBEAMS) :: Adist_beams, Asize_beams, Div_beams, E_beams, mass_beams, &
                                         charge_beams, Zatom_beams, P_beams
     REAL(rprec), DIMENSION(MAXBEAMS, 2) :: r_beams, z_beams, phi_beams
