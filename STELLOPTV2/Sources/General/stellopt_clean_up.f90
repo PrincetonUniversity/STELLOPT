46edd388c37c314a6db7f8a16f9f0e4721d1383b
John Schmitt <jschmitt@pppl.gov>
2019-04-09 09:26:39 -0400

    Trying to fix a merge messup from last week

--- STELLOPTV2/Sources/General/stellopt_clean_up.f90
+++ STELLOPTV2/Sources/General/stellopt_clean_up.f90
@@ -224,41 +224,6 @@
                END DO
             END IF
 !DEC$ ENDIF
-<<<<<<< HEAD
-!<<<<<<< HEAD
-               END IF
-               ! Now open the Output file
-               ALLOCATE(fvec_temp(mtargets))
-               CALL safe_open(iunit_out,iflag,TRIM('stellopt.'//TRIM(id_string)),'unknown','formatted',ACCESS_IN='APPEND')
-               iflag = 1
-               IF (ncnt == 0) WRITE(iunit_out,'(A,1X,F5.2)') 'VERSION',STELLOPT_VERSION
-               WRITE(iunit_out,'(A,1X,I5.5)') 'ITER',ncnt
-               CALL stellopt_load_targets(mtargets,fvec_temp,iflag,ncnt)          ! Count
-               WRITE(iunit_out,'(A,2(2X,I8))') 'TARGETS ',mtargets,1
-               WRITE(iunit_out,'(A)') 'TARGETS'
-               WRITE(iunit_out,'(ES22.12E3)') targets(1:mtargets)
-               WRITE(iunit_out,'(A,2(2X,I8))') 'SIGMAS ',mtargets,1
-               WRITE(iunit_out,'(A)') 'SIGMAS'
-               WRITE(iunit_out,'(ES22.12E3)') sigmas(1:mtargets)
-               WRITE(iunit_out,'(A,2(2X,I8))') 'VALS ',mtargets,1
-               WRITE(iunit_out,'(A)') 'VALUES'
-               WRITE(iunit_out,'(ES22.12E3)') vals(1:mtargets)
-               CLOSE(iunit_out)
-               DEALLOCATE(fvec_temp)
-            ELSE IF ((ctype == LEV_CLEANUP) .or. &
-                     (ctype == GADE_CLEANUP) .or. &
-                     (ctype == BFGS_CLEANUP) ) THEN
-               IF (ncnt /= 1 .or. ctype == GADE_CLEANUP) THEN
-                  ! Write the input file
-                  WRITE(temp_str,'(i5.5)') ncnt
-                  proc_string = TRIM(id_string) // '.' // TRIM(ADJUSTL(temp_str))
-                  CALL safe_open(iunit_out,iflag,TRIM('input.'//TRIM(proc_string)),'unknown','formatted')
-!=======
-             CALL safe_open(iunit_out,iflag,TRIM('input.'//TRIM(proc_string)),'unknown','formatted')
-             SELECT CASE(TRIM(equil_type))
-               CASE('vmec2000','animec','flow','satire','parvmec','paravmec','vboot','vmec2000_oneeq')
-!>>>>>>> origin
-=======
          END IF
          ! Now open the Output file
          ALLOCATE(fvec_temp(mtargets))
@@ -278,7 +243,10 @@
          WRITE(iunit_out,'(ES22.12E3)') vals(1:mtargets)
          CLOSE(iunit_out)
          DEALLOCATE(fvec_temp)
-      ELSE IF ((ctype == LEV_CLEANUP) .or. (ctype == GADE_CLEANUP)) THEN
+      # ELSE IF ((ctype == LEV_CLEANUP) .or. (ctype == GADE_CLEANUP)) THEN
+      ELSE IF ((ctype == LEV_CLEANUP) .or. &
+               (ctype == GADE_CLEANUP) .or. &
+               (ctype == BFGS_CLEANUP) ) THEN
           IF (ncnt /= 1 .or. ctype == GADE_CLEANUP) THEN
              ! Write the input file
              WRITE(temp_str,'(i5.5)') ncnt
@@ -286,7 +254,6 @@
              CALL safe_open(iunit_out,iflag,TRIM('input.'//TRIM(proc_string)),'unknown','formatted')
              SELECT CASE(TRIM(equil_type))
                CASE('vmec2000','animec','flow','satire','parvmec','paravmec','vboot','vmec2000_oneeq')
->>>>>>> f892badb24fd1f5b036c831835ac3993631960da
                   CALL write_indata_namelist(iunit_out,ier)
                CASE('test')
              END SELECT
