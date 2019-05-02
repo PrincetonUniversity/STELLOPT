54387b47d41510185bc36815ca5cc71bebcc1539
aaroncbader <abader@engr.wisc.edu>
2019-05-02 12:55:57 -0500

    add e_theta_norm calculation

--- STELLOPTV2/Sources/Chisq/chisq_gamma_c.f90
+++ STELLOPTV2/Sources/Chisq/chisq_gamma_c.f90
@@ -39,17 +39,19 @@
       LOGICAL :: lsym
       INTEGER :: dex, ik, i, j, ier
       REAL(rprec) :: s, rovera, theta, zeta, delzeta, u, v, coszeta, sinzeta
-      REAL(rprec) :: iota, iotap, minB, maxB, B_refl, psi_a
+      REAL(rprec) :: iota, iotap, minB, maxB, B_refl, psi_a, B_zeta
+      REAL(rprec) :: X, Y, dpsidr, dpsidz
 
-      REAL(rprec), DIMENSION(3) :: sflCrd, crossnum, crossden
+      REAL(rprec), DIMENSION(3) :: sflCrd, Bxyz, crossnum, crossden
+      REAL(rprec), DIMENSION(3) :: e_phi, e_r, e_z, grads
       INTEGER, PARAMETER :: nsteps = 100
       INTEGER :: delzetadiv =200
       INTEGER, PARAMETER :: bpstep = 101 !division in b'
 
 
       REAL(rprec), DIMENSION(nsteps) :: R, Z, Bx, By, Bz, Bsupv, dBsupvdpsi, modB
-      REAL(rprec), DIMENSION(nsteps) :: kappa_g
-      real(rprec), DIMENSION(nsteps,3) :: gradR, gradZ, gradB, grads
+      REAL(rprec), DIMENSION(nsteps) :: kappa_g, e_theta_norm
+      real(rprec), DIMENSION(nsteps,3) :: gradR, gradZ, gradB, gradpsi
       real(rprec), DIMENSION(nsteps,3) :: dxyzdu, dxyzdv, dxyzds, dBsupv 
       real(rprec), DIMENSION(nsteps) :: grad_psi_i
       integer, dimension(nsteps) :: well_mask
@@ -93,35 +95,47 @@
             u = sflCrd(2)
             v = sflCrd(3)
 
-            !Get rz values and gradients, store gradients in temporary arrays
+
+            !Get rz values and gradients
             CALL get_equil_RZ(s, u, v, R(j), Z(j), ier, gradR(j,:), gradZ(j,:))
-            !the R and Z derivatives are with respect to rho and not s
+
+            !X and Y values from cyclindrical
+            X=R(j)*cos(zeta)
+            Y=R(j)*sin(zeta)
+
+            !Note, the R and Z derivatives are with respect to rho and not s
             !drds = drdrho/2sqrt(s)  
             gradR(j,3) = gradR(j,3)/2/sqrt(s)
             gradZ(j,3) = gradZ(j,3)/2/sqrt(s)
             !now form dX/du, dX/dv, dX/ds where X=(x,y,z) 
             !start with the z terms, since we have them already
             !we note that in the PEST coordinates zeta = cylindrical angle phi
+            !we'll use this relation a lot
             dxyzdu(j,3) = gradZ(j,1)
             dxyzdv(j,3) = gradZ(j,2)
             dxyzds(j,3) = gradZ(j,3)
-            !now the x terms
+            !now the x terms TODO verify that 2pi shouldn't be in dxyzdv
             coszeta = cos(zeta)
             sinzeta = sin(zeta)
             dxyzdu(j,1) = gradR(j,1)*coszeta
-            dxyzdv(j,1) = gradR(j,2)*coszeta - pi2*R(j)/nfp*sinzeta
+            dxyzdv(j,1) = gradR(j,2)*coszeta - R(j)/nfp*sinzeta
             dxyzds(j,1) = gradR(j,3)*coszeta
             !and the y terms
             dxyzdu(j,2) = gradR(j,1)*sinzeta
-            dxyzdv(j,2) = gradR(j,2)*sinzeta + pi2*R(j)/nfp*coszeta
+            dxyzdv(j,2) = gradR(j,2)*sinzeta + R(j)/nfp*coszeta
             dxyzds(j,2) = gradR(j,3)*sinzeta
 
+            !Now form gradpsi
             CALL cross_product(dxyzdu, dxyzdv, crossnum)
             CALL cross_product(dxyzdv, dxyzds, crossden)
-            grads(j,:) = crossnum/dot_product(crossden, dxyzdu(j,:))
+            grads = crossnum/dot_product(crossden, dxyzdu(j,:))
+            gradpsi(j,:) =grads * psi_a
 
             !Get B field
             CALL get_equil_B(R(j), zeta, Z(j), Bx(j), By(j), Bz(j), ier, modB(j), gradB(j,:))
+            Bxyz(1) = Bx(j)
+            Bxyz(2) = By(j)
+            Bxyz(3) = Bz(j)
 
             !Get B^v and derivatives
             CALL get_equil_Bsupv(s,u,v,Bsupv(j),ier,dBsupv(j,:))
@@ -129,6 +143,29 @@
 
             !Get gaussian curvature
             CALL get_equil_kappa(s,u,v,kappa_g(j),ier)
+
+            !Calculate |e_theta|
+
+            !e_r
+            e_r(1) = X/R(j)
+            e_r(2) = y/R(j)
+            e_r(3) = 0.0_rprec
+
+            !e_phi
+            e_phi(1) = -Y/R(j)
+            e_phi(2) = X/R(j)
+            e_phi(3) = 0.0_rprec
+
+            !e_z (can move this outside the loop if desired for speed)
+            e_z(1) = 0.0
+            e_z(2) = 0.0
+            e_z(3) = 1.0
+
+            dpsidr = dot_product(gradpsi(j,:), e_r)
+            dpsidz = dot_product(gradpsi(j,:), e_z)
+            B_zeta = dot_product(Bxyz, e_phi)
+
+            e_theta_norm(j) = sqrt(dpsidr*dpsidr + dpsidz*dpsidz)/B_zeta
 
 
             !advance the step
