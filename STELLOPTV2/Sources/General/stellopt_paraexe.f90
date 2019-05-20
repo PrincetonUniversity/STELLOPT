380ab6dd6ee08413654db5a46f9ce5f31315ba20
Samuel Lazerson <lazerson@pppl.gov>
2019-05-20 06:53:39 -0700

    Switch to DUP MPI_COMM_BEAMS Communicator

--- STELLOPTV2/Sources/General/stellopt_paraexe.f90
+++ STELLOPTV2/Sources/General/stellopt_paraexe.f90
@@ -259,7 +259,8 @@
                IF (ierr_mpi /= MPI_SUCCESS) CALL handle_err(MPI_ERR,'stellopt_paraexe',ierr_mpi)
 
                ! Set vars so BEAMS3D knows it's being called from stellopt
-               MPI_COMM_BEAMS     = MPI_COMM_MYWORLD
+               !MPI_COMM_BEAMS     = MPI_COMM_MYWORLD
+               CALL MPI_COMM_DUP(MPI_COMM_BEAMS, MPI_COMM_MYWORLD, ierr_mpi)
                CALL MPI_COMM_SPLIT_TYPE(MPI_COMM_BEAMS, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, MPI_COMM_SHARMEM, ierr_mpi)
                CALL MPI_COMM_RANK(MPI_COMM_SHARMEM, myid_sharmem, ierr_mpi)
                lverb_beams        = .FALSE.
@@ -337,6 +338,7 @@
               
                ! Free the Shared Memory region
                CALL MPI_COMM_FREE(MPI_COMM_SHARMEM,ierr_mpi)
+               CALL MPI_COMM_FREE(MPI_COMM_BEAMS,ierr_mpi)
 
                IF (lverb_beams) WRITE(6, '(A)') '----- BEAMS3D DONE -----'
 
