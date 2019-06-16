698f104b36e4aacb82674dd5dc378ab3a07cb4f3
Samuel Lazerson <lazerson@princeton.edu>
2019-06-15 20:08:57 -0400

    Updated read_fieldlines to include an iota calculation routine.

--- pySTEL/libstell/fieldlines.py
+++ pySTEL/libstell/fieldlines.py
@@ -16,4 +16,36 @@ def read_fieldlines(file):
         # 1D Arrays
         for temp in ['phiaxis', 'raxis', 'zaxis', 'B_lines', 'PHI_lines', 'R_lines', 'Z_lines', 'B_PHI', 'B_R', 'B_Z']:
             fieldline_data[temp] = np.array(f[temp][:])
+    # Make derived arrays
+    fieldline_data['X_lines'] = fieldline_data['R_lines']*np.cos(fieldline_data['PHI_lines'])
+    fieldline_data['Y_lines'] = fieldline_data['R_lines']*np.sin(fieldline_data['PHI_lines'])
+    brtemp = np.zeros(fieldline_data['B_R'].shape)
+    bztemp = np.zeros(fieldline_data['B_Z'].shape)
+    for i in range(fieldline_data['nr']):
+        brtemp[i,:,:] = fieldline_data['B_R'][i,:,:]*fieldline_data['B_PHI'][i,:,:]/fieldline_data['raxis'][i]
+        bztemp[i,:,:] = fieldline_data['B_Z'][i,:,:]*fieldline_data['B_PHI'][i,:,:]/fieldline_data['raxis'][i]
+    fieldline_data['B_R'] = brtemp
+    fieldline_data['B_Z'] = bztemp
     return fieldline_data
+
+def calc_iota(data):
+    import numpy as np
+    x = np.zeros(data['R_lines'].shape)
+    y = np.zeros(data['Z_lines'].shape)
+    nstep = data['nsteps']
+    for i in range(data['nsteps']):
+        x[i,:] = data['R_lines'][i,:]-data['R_lines'][i,0]
+        y[i,:] = data['Z_lines'][i,:]-data['Z_lines'][i,0]
+    theta  = np.arctan2(y,x)
+    dtheta = np.diff(theta,axis=0)
+    dtheta = np.where(dtheta<-np.pi,dtheta+2*np.pi,dtheta)
+    dtheta = np.where(dtheta>np.pi,dtheta-2*np.pi,dtheta)
+    theta  = np.cumsum(dtheta,axis=0)
+    out         = {}
+    out['rho']  = np.mean(np.sqrt(x*x+y*y),axis=0)
+    out['iota'] = np.zeros(out['rho'].shape)
+    for i in range(data['nlines']):
+        p = np.polyfit(data['PHI_lines'][0:nstep-1,i],theta[:,i],1)
+        out['iota'][i]=p[0]
+    out['iota'][0]=2*out['iota'][1]-out['iota'][2]
+    return out
