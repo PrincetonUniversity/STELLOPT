76261ff1cfdaf328f30cd49165557ea5ab4df609
Samuel Lazerson <lazerson@princeton.edu>
2019-07-06 14:12:31 -0400

    Merge in of ORNL changes, minus routine I need to inspect in detail.

--- VMEC2000/Sources/TimeStep/evolve.f
+++ VMEC2000/Sources/TimeStep/evolve.f
@@ -1,18 +1,14 @@
       SUBROUTINE evolve(time_step, ier_flag, liter_flag, lscreen)
       USE vmec_main
       USE vmec_params, ONLY: bad_jacobian_flag, successful_term_flag,
-     1                       norm_term_flag
+     &                       norm_term_flag
       USE vsvd
       USE xstuff
-#if defined(SKS)
       USE precon2d, ONLY: ictrl_prec2d, l_comp_prec2D, 
-     1                    compute_blocks_par, compute_blocks
+     &                    compute_blocks_par, compute_blocks
       USE parallel_include_module
       USE parallel_vmec_module, ONLY: ZeroLastNType, CopyLastNtype, 
-     1                                SaxpbyLastNtype, CompareEdgeValues
-#else
-      USE precon2d, ONLY: ictrl_prec2d, l_comp_prec2D, compute_blocks
-#endif
+     &                                SaxpbyLastNtype, CompareEdgeValues
       USE timer_sub
       USE vmec_params, ONLY: ntmax
       USE gmres_mod
@@ -30,16 +26,14 @@
 !   L o c a l   V a r i a b l e s
 !-----------------------------------------------
       CHARACTER(LEN=*), PARAMETER :: fcn_message =
-     1 "External calls to FUNCT3D: "
+     &   'External calls to FUNCT3D: '
 !      REAL(dp), PARAMETER :: r0dot_threshold = 5.E-06_dp
       REAL(dp) :: fsq1, dtau, b1, bprec, fac
       LOGICAL :: lfinal_mesh
       INTEGER :: lcount
       INTEGER, SAVE :: iter_on
-#if defined(SKS)      
-      INTEGER :: i, j, k, l, lk
       REAL(dp) :: f3dt1, f3dt2, tevon, tevoff
-#endif
+
 C-----------------------------------------------
 !     IF TROUBLE CONVERGING, TRY TO RECOMPUTE PRECONDITIONER ONCE MORE...
 !      IF (ictrl_prec2d.eq.1 .and. iter2.eq.(iter_on+40)) 
@@ -55,21 +49,20 @@ C-----------------------------------------------
 !      lfinal_mesh = (ns .eq. ns_maxval) .and. (ictrl_prec2d.eq.0)
 !     1              .and. (itype_precon.ne.0)
 
-#if defined (SKS)
-         CALL second0(tevon)
-#endif
+      CALL second0(tevon)
 
-      lfinal_mesh = (ns .EQ. ns_maxval) .and. (ictrl_prec2d.EQ.0)
-     1              .and. (itype_precon.ne.0)
-     2              .and. ((.not. l_v3fit) .or. iter2 - iter1 .ge. 5)
+
+      lfinal_mesh = ns           .EQ. ns_maxval .and.
+     &              ictrl_prec2d .EQ. 0         .and.
+     &              itype_precon .ne. 0         .and.
+     &              (.not.l_v3fit .or. iter2 - iter1 .ge. 5)
 
       IF (iter2 .lt. 10) THEN
          ictrl_prec2d = 0
          lqmr = .false.
          iter_on = -1
       ELSE IF (lfinal_mesh .and. 
-     1        (fsqr+fsqz+fsql).lt.prec2d_threshold) THEN
-!     2                     .and. r0dot.lt.r0dot_threshold) THEN
+     &         fsqr + fsqz + fsql .lt. prec2d_threshold) THEN
          lqmr = (itype_precon .GE. 2)
          lfirst = (lqmr .AND. iter_on.EQ.-1) 
 
@@ -94,13 +87,11 @@ C-----------------------------------------------
          IF (lfirst .OR. l_comp_prec2D) THEN
             IF (l_v3fit) WRITE(*,*) 'VMEC Evolve:compute_blocks'
             IF (PARVMEC) THEN
-#if defined(SKS)
                CALL compute_blocks_par (pxc,pxcdot,pgc)
-#endif
             ELSE
-              CALL compute_blocks (xc,xcdot,gc)
+               CALL compute_blocks (xc,xcdot,gc)
             END IF
-         ENDIF
+         END IF
          IF(l_v3fit) WRITE(*,*) 'VMEC Evolve:prec2d_On iter2 =', iter2
          l_comp_prec2D = .FALSE.
          ictrl_prec2d = 1
@@ -108,13 +99,11 @@ C-----------------------------------------------
          iter1 = iter2-1; fsq = fsqr1 + fsqz1 + fsql1
 
          IF (PARVMEC) THEN
-#if defined(SKS)
             CALL CopyLastNtype(pxstore, pxc)
             CALL ZeroLastNType(pxcdot)
-#endif
          ELSE
-           xc = xstore
-           xcdot = 0
+            xc = xstore
+            xcdot = 0
          END IF
       END IF
 
@@ -123,21 +112,16 @@ C-----------------------------------------------
 !     MUST CALL funct3d EVEN WHEN IN 2D PRECONDITIONING MODE, SINCE
 !     INITIAL RESIDUALS MUST BE KNOWN WHEN CALLING gmres_fun, etc.
 !
-#if defined (SKS)      
       CALL second0(f3dt1)
-      IF (NS_RESLTN > num_grids) NS_RESLTN = num_grids
       f3d_num(NS_RESLTN) = f3d_num(NS_RESLTN)+1
       IF (PARVMEC) THEN
-        CALL funct3d_par (lscreen, ier_flag)
+         CALL funct3d_par(lscreen, ier_flag)
       ELSE
-#endif
-        CALL funct3d (lscreen, ier_flag)
-#if defined (SKS)      
+         CALL funct3d(lscreen, ier_flag)
       END IF
       CALL second0(f3dt2)
-      f3d_time(NS_RESLTN) = f3d_time(NS_RESLTN) + (f3dt2-f3dt1)
-      funct3d_time = funct3d_time + (f3dt2-f3dt1)
-#endif        
+      f3d_time(NS_RESLTN) = f3d_time(NS_RESLTN) + (f3dt2 - f3dt1)
+      funct3d_time = funct3d_time + (f3dt2 - f3dt1)
 
 !
 !     COMPUTE ABSOLUTE STOPPING CRITERION
@@ -148,20 +132,15 @@ C-----------------------------------------------
 !    is running, then have to iterate at least 2 * nvacskip steps
 !    (2 picked out of a hat) (nvacskip - to make sure vacuum gets updated)
 !    before returning.
-!      ELSE IF (fsqr.le.ftolv .and. fsqz.le.ftolv .and.
-!     1         fsql.le.ftolv) THEN
-      ELSE IF (fsqr.le.ftolv .and. fsqz.le.ftolv .and.
-     1         fsql.le.ftolv)
-!     2    .and. ((.not. l_v3fit) .or. iter2 - iter1 .ge. 2 * nvacskip))
-     3         THEN
+
+      ELSE IF (fsqr .le. ftolv .and.
+     &         fsqz .le. ftolv .and.
+     &         fsql .le. ftolv) THEN
          liter_flag = .false.
          ier_flag = successful_term_flag
-         IF (lqmr) THEN
-!            WRITE (nthreed,'(/,2x,a,i5)') fcn_message,nfcn
-!           WRITE (*,'(/,2x,a,i5)') fcn_message,nfcn 
-         END IF
          RETURN
       ENDIF
+
 
 !SPH:042117: MOVE TIME STEP CONTROL HERE (FROM END OF EQSOLVE) TO AVOID
 !STORING A POSSIBLE irst=2 STATE
@@ -169,19 +148,18 @@ C-----------------------------------------------
 
       IF (lqmr) THEN
          IF (PARVMEC) THEN
-#if defined(SKS)
-           CALL gmres_fun_par(ier_flag, itype_precon-1)
-           IF (.NOT.lfreeb) CALL CompareEdgeValues(pxc, pxsave)
-#endif
+            CALL gmres_fun_par(ier_flag, itype_precon - 1, lscreen)
+            IF (.NOT.lfreeb) CALL CompareEdgeValues(pxc, pxsave)
          ELSE
-           CALL gmres_fun(ier_flag, itype_precon-1)
-           IF (.NOT.lfreeb) THEN
-              DO lcount = ns, 2*irzloff, ns
-                 IF (xsave(lcount) .NE. xc(lcount))
-     1           PRINT *,' xsave = ',xsave(lcount),' != xc = ',
-     2           xc(lcount),' for lcount = ',lcount
-              END DO
-           END IF
+            CALL gmres_fun(ier_flag, itype_precon-1)
+            IF (.NOT.lfreeb) THEN
+               DO lcount = ns, 2*irzloff, ns
+                  IF (xsave(lcount) .NE. xc(lcount)) THEN
+                     PRINT *, ' xsave = ',xsave(lcount),' != xc = ',
+     &                        xc(lcount),' for lcount = ',lcount
+                  END IF
+               END DO
+            END IF
          END IF
 
          RETURN
@@ -201,8 +179,10 @@ C-----------------------------------------------
       END IF
 
       dtau = bprec*cp15
-      IF (iter2.GT.iter1 .AND. fsq1*fsq.NE.zero)
-     1   dtau = MIN(ABS(LOG(fsq1/fsq)), dtau)
+      IF (iter2 .GT. iter1 .AND.
+     &    fsq1*fsq .NE. zero) THEN
+         dtau = MIN(ABS(LOG(fsq1/fsq)), dtau)
+      END IF
 
       fsq = fsq1
 
@@ -225,29 +205,26 @@ C-----------------------------------------------
 
 !
 
-#if defined (SKS)
-      IF(PARVMEC) THEN
-        IF (lactive) THEN 
-        CALL SaxpbyLastNtype(fac*time_step, pgc, fac*b1, pxcdot, pxcdot)
-        CALL SaxpbyLastNtype(time_step, pxcdot, one, pxc, pxc)
-        END IF
+      IF (PARVMEC) THEN
+         IF (lactive) THEN
+            CALL SaxpbyLastNtype(fac*time_step, pgc, fac*b1, pxcdot,
+     &                           pxcdot)
+            CALL SaxpbyLastNtype(time_step, pxcdot, one, pxc, pxc)
+         END IF
       ELSE
-#endif
-        xcdot = fac*(b1*xcdot + time_step*gc)
-        xc    = xc + time_step*xcdot
-#if defined (SKS)
-      ENDIF
+         xcdot = fac*(b1*xcdot + time_step*gc)
+         xc    = xc + time_step*xcdot
+      END IF
 
       CALL second0(tevoff)
       evolve_time = evolve_time + (tevoff - tevon)
-#endif
 
       END SUBROUTINE evolve
 
 
       SUBROUTINE TimeStepControl(ier_flag, PARVMEC)
       USE vmec_main, ONLY: res0, res1, fsq, fsqr, fsqz, fsql,
-     1                     irst, iter1, iter2, delt0r, dp
+     &                     irst, iter1, iter2, delt0r, dp
       USE vmec_params, ONLY: ns4
       USE vparams, ONLY: c1pm2
       USE vmec_input, ONLY: nstep
@@ -263,12 +240,8 @@ C-----------------------------------------------
       INTEGER  :: ier_flag
       LOGICAL, INTENT(IN) :: PARVMEC
 
-
       fsq0 = fsqr+fsqz+fsql
       IF (iter2.EQ.iter1 .OR. res0.EQ.-1) THEN
-!         IF (res0.eq.-1 .AND. rank.eq.0) 
-!     1    WRITE (6000, *)' iter1 iter2    res0    ' //
-!     2          '    res1        fsq         fsq0      irst    delt'
          res0 = fsq
          res1 = fsq0
          CALL restart_iter(delt0r)
@@ -291,28 +264,19 @@ C-----------------------------------------------
 ! Residuals are growing in time, reduce time step
          IF (fsq.GT.fact*res0 .OR. fsq0.GT.fact*res1) THEN
             irst = 3
-!         IF (rank .EQ. 0) PRINT *,' FSQ > 1.E4 * FSQ_min AT ITER: ',
-!     1                    iter2
          END IF
       END IF
-!      IF ((iter2-iter1).GT.ns4/2 .AND. iter2.GT.2*ns4
-!     1        .AND. fsqr+fsqz.GT.c1pm2) irst = 3
 
 !     Retrieve previous good state
       IF (irst .NE. 1) THEN
          CALL restart_iter(delt0r)
          iter1 = iter2
-#if defined (SKS)      
          IF (PARVMEC) THEN
-           CALL funct3d_par (.FALSE., ier_flag)
+            CALL funct3d_par(.FALSE., ier_flag)
          ELSE
-#endif
-           CALL funct3d (.FALSE., ier_flag)
-#if defined (SKS)      
+            CALL funct3d(.FALSE., ier_flag)
          END IF
-	IF (irst .NE. 1 .and. irst .NE. 4)
-     1      STOP 'Logic error in TimeStepControl!'
-#endif
+         IF (irst .NE. 1) STOP 'Logic error in TimeStepControl!'
       END IF
 
       END SUBROUTINE TimeStepControl
