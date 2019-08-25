35a7bc6eb6968a743ca06d9ce784ebee36ed4336
Caoxiang Zhu <czhu@pppl.gov>
2019-08-25 12:51:22 -0400

    revise fix_format.py for automatically fixing

--- fix_format.py
+++ fix_format.py
@@ -4,18 +4,15 @@ import sys
 filename = sys.argv[1]
 print(filename)
 
-a =  r'\\ '[:-1]
-b =  r'\ '[:-1]
-
-old_list = ['[math](math)', a, r'\_ '[:-1], r'\^']
-new_list = ['$$', b, r"_ "[:-1], r"^"]
+old_list = ['[math](math)', r'\\ '[:-1], r'\_ '[:-1], r'\^']
+new_list = ['$$', r'\ '[:-1], r"_ "[:-1], r"^"]
 
 
 with open(filename, 'r') as f:
     contents = f.readlines()
     for i, line in enumerate(contents):
-        for old, new in zip(old_list, new_list):
-            contents[i] = line.replace(old, new)
+        for old, new in zip(old_list, new_list):           
+            contents[i] = contents[i].replace(old, new)
 
 with open(filename, 'w') as f:
     f.writelines(contents)
