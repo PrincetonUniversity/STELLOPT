08074e3c684f55f81e8f8ee51441bafff2a01f7c
lazersos <samuel.lazerson@ipp.mpg.de>
2019-09-16 08:25:50 +0200

    Outputs HDF5 Version information in BEAMS3D

--- BEAMS3D/Sources/beams3d_main.f90
+++ BEAMS3D/Sources/beams3d_main.f90
@@ -14,6 +14,9 @@ PROGRAM BEAMS3D
     USE wall_mod, ONLY: wall_free
     USE mpi_params
     USE mpi_inc
+!DEC$ IF DEFINED (LHDF5)
+    USE hdf5
+!DEC$ ENDIF
 
     !-----------------------------------------------------------------------
     !     Local Variables
@@ -25,6 +28,7 @@ PROGRAM BEAMS3D
     !-----------------------------------------------------------------------
     IMPLICIT NONE
     integer :: numargs, i, ier, nshar, vmajor, vminor, liblen
+    integer :: h5major, h5minor, h5rel
     integer, parameter :: arg_len = 256
     character(LEN=MPI_MAX_LIBRARY_VERSION_STRING) :: mpi_lib_name
     character*(arg_len) :: arg1
@@ -47,6 +51,9 @@ PROGRAM BEAMS3D
     CALL MPI_COMM_SIZE(MPI_COMM_SHARMEM, nshar, ierr_mpi) ! MPI
     CALL MPI_GET_VERSION(vmajor,vminor,ier)
     CALL MPI_GET_LIBRARY_VERSION(mpi_lib_name,liblen,ier)
+!DEC$ ENDIF
+!DEC$ IF DEFINED (LHDF5)
+    CALL H5GET_LIBVERSION_F(h5major, h5minor, h5rel, ier)
 !DEC$ ENDIF
     pi = 4.0 * ATAN(1.0)
     pi2 = 8.0 * ATAN(1.0)
@@ -180,6 +187,10 @@ PROGRAM BEAMS3D
         END DO
         DEALLOCATE(args)
         WRITE(6, '(a,f5.2)') 'BEAMS3D Version ', BEAMS3D_VERSION
+!DEC$ IF DEFINED (LHDF5)
+        WRITE(6,'(A)')      '-----  HDF5 Parameters  ----'
+        WRITE(6,'(A,I2,2(A,I2.2))')  '   HDF5_version:  ', h5major,'.',h5minor,' release: ',h5rel
+!DEC$ ENDIF
         WRITE(6,'(A)')      '-----  MPI Parameters  -----'
         WRITE(6,'(A,I2,A,I2.2)')  '   MPI_version:  ', vmajor,'.',vminor
         WRITE(6,'(A,A)')  '   ', TRIM(mpi_lib_name(1:liblen))
