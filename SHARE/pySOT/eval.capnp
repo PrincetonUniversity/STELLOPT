9ecbff5bed8938206d20f48a9cb259d2eaffcc15
Curran D. Muhlberger <curran@muhlbergerweb.com>
2019-09-27 17:10:11 -0400

    Add STELLOPT optimization problem for pySOT
    This subclass of `OptimizationProblem` forwards `eval` calls to an RPC
    server implementing the `eval.capnp` ICD.  Nothing is particularly
    STELLOPT-specific.  Currently, a single instance of this class will
    submit all requests to the same server.

---
+++ SHARE/pySOT/eval.capnp
@@ -0,0 +1,4 @@
+@0xcb6e6711bc51954d;
+interface Eval {
+  eval @0 (params :List(Float64)) -> (objective :Float64);
+}
---
+++ SHARE/pySOT/stellopt.py
@@ -0,0 +1,38 @@
+import numpy as np
+from pySOT.optimization_problems import OptimizationProblem
+import capnp
+import eval_capnp
+
+class StelloptProblem(OptimizationProblem):
+    """STELLOPT weighted objective via RPC
+
+    Connect to a Cap'n Proto RPC server and forward `eval` calls to an RPC
+    `eval` method.  Intended for use with STELLOPT in "rpc-eval" mode.  Ideally
+    each worker thread should connect to a different server, which will be the
+    leader of its own MPI group.
+    """
+    def __init__(self, dim, server_addr):
+        # TODO: Read dim, bounds, etc. from server
+        self.dim = dim
+        # For now, default bounds are [0,1] and may be safely overridden by the
+        # user.
+        self.lb = np.zeros(self.dim)
+        self.ub = np.ones(self.dim)
+
+        # All parameters are continuous.
+        self.int_var = np.array([])
+        self.cont_var = np.arange(0, self.dim)
+
+        # Connect to RPC server and get handle to `eval` method.
+        client = capnp.TwoPartyClient(server_addr)
+        self.evaluator = client.bootstrap().cast_as(eval_capnp.Eval)
+
+    def eval(self, x):
+        """Evaluate objective function via RPC
+
+        Call RPC `eval` method on configured server and wait for and return
+        result.
+        """
+        self.__check_input__(x)
+        # TODO: Handle errors and disconnects
+        return self.evaluator.eval(x.tolist()).wait().objective
