ef2d6ba5a3c1706edd49ac29b50191de06e09725
Samuel Lazerson <lazersos@gmail.com>
2020-03-06 11:00:41 +0100

    Removed GTOVMI, saved in GTOVMI branch

--- GTOVMI/Sources/Modules/precision.f
+++
@@ -1,8 +0,0 @@
-      MODULE precision
-        USE stel_kinds, ONLY: rprec
-!        INTEGER, PARAMETER :: rprec = SELECTED_REAL_KIND(12,100)
-        INTEGER, PARAMETER :: iprec = SELECTED_INT_KIND(8)
-        INTEGER, PARAMETER :: cprec = KIND((1.0_rprec,1.0_rprec))
-        INTEGER, PARAMETER :: dbl = rprec
-      END MODULE precision
-
