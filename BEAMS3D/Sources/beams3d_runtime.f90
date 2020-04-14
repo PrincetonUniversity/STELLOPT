9d128f9d7b6c502e208931d5eef727c9f19d4236
lazersos <samuel.lazerson@ipp.mpg.de>
2020-04-14 15:23:01 +0200

    BEAMS3D: ASCOT5 interface now generates random GID's

--- BEAMS3D/Sources/beams3d_runtime.f90
+++ BEAMS3D/Sources/beams3d_runtime.f90
@@ -124,6 +124,7 @@ MODULE beams3d_runtime
     REAL(rprec), ALLOCATABLE :: R_start(:), phi_start(:), Z_start(:), vll_start(:), v_neut(:,:), mu_start(:), &
                                 & mass(:), charge(:), Zatom(:), t_end(:), weight(:)
     REAL(rprec), ALLOCATABLE :: extcur(:)
+    CHARACTER(LEN=10) ::  qid_str_saved ! For ASCOT5
     CHARACTER(256) :: id_string, mgrid_string, coil_string, &
     vessel_string, int_type, restart_string, bbnbi_string
 
