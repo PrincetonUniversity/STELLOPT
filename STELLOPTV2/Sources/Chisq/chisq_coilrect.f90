ad4f1bb9293d48e8c9cb6211adda3a27dd5b1e56
Joshua Breslau <jbreslau@pppl.gov>
2020-04-28 11:17:10 -0400

    Imported latest coil penalty functions from stellcopt.

--- STELLOPTV2/Sources/Chisq/chisq_coilrect.f90
+++ STELLOPTV2/Sources/Chisq/chisq_coilrect.f90
@@ -1,17 +1,17 @@
 !-----------------------------------------------------------------------
-!     Subroutine:    chisq_coilcrv
+!     Subroutine:    chisq_coilrect
 !     Authors:       J. Breslau (jbreslau@pppl.gov)
-!     Date:          8/24/2017
-!     Description:   Calculates the difference between maximum curvature
-!                    for each coil and its target value.
+!     Date:          6/4/2019
+!     Description:   Calculates penalty based on excursion of coil from
+!                    prescribed v bounds
 !-----------------------------------------------------------------------
-      SUBROUTINE chisq_coilcrv(target,sigma,niter,iflag)
+      SUBROUTINE chisq_coilrect(target,sigma,niter,iflag)
 !-----------------------------------------------------------------------
 !     Libraries
 !-----------------------------------------------------------------------
       USE stellopt_runtime
       USE stellopt_targets
-      USE stellopt_vars, ONLY: lcoil_spline
+      USE stellopt_vars, ONLY: lcoil_spline, lwindsurf
       
 !-----------------------------------------------------------------------
 !     Input/Output Variables
@@ -22,45 +22,44 @@
       REAL(rprec), INTENT(in)    ::  sigma(nigroup)
       INTEGER,     INTENT(in)    ::  niter
       INTEGER,     INTENT(inout) ::  iflag
-
-      INTERFACE
-         SUBROUTINE get_coil_maxcurv(icoil, maxcurv, s_max, u_max, v_max, uout)
-           USE stel_kinds, ONLY: rprec
-           INTEGER, INTENT(IN)           :: icoil
-           REAL(rprec), INTENT(OUT)      :: maxcurv, s_max, u_max, v_max
-           INTEGER, INTENT(IN), OPTIONAL :: uout
-         END SUBROUTINE get_coil_maxcurv
-      END INTERFACE
+      
 !-----------------------------------------------------------------------
 !     Local Variables
 !
 !-----------------------------------------------------------------------
+      REAL(rprec) :: vmin, vmax
       INTEGER     :: ik
-      REAL(rprec) :: maxcurv, smax, umax, vmax
+
 !----------------------------------------------------------------------
 !     BEGIN SUBROUTINE
 !----------------------------------------------------------------------
       IF (iflag < 0) RETURN
+      IF (.NOT.lwindsurf) RETURN  !This routine only makes sense for embedded curves in 2D
       ik   = COUNT(sigma < bigno)
-      IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'MAX. COIL CURVATURE ',ik,7
-      IF (iflag == 1) WRITE(iunit_out,'(A)') 'TARGET  SIGMA  VAL  LOC  U  V'
+      IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'VBOUNDS ',ik,5
+      IF (iflag == 1) WRITE(iunit_out,'(A)') 'TARGET  SIGMA  VMIN  VMAX'
       IF (niter >= 0) THEN
          DO ik = 1, nigroup
             IF ((sigma(ik) < bigno) .AND. ANY(lcoil_spline(ik,:))) THEN
+               CALL get_coil_vbounds(ik, coilrectduu(ik), coilrectdul(ik), vmin, vmax)
                mtargets = mtargets + 1
-               CALL get_coil_maxcurv(ik, maxcurv, smax, umax, vmax)
                targets(mtargets) = target(ik)
                sigmas(mtargets)  = sigma(ik)
-               vals(mtargets)    = MAX(maxcurv, target(ik))  ! One-sided barrier
-               IF (iflag == 1) WRITE(iunit_out,'(I5,6ES22.12E3)') &
-                    ik,target(ik),sigma(ik),maxcurv,smax,umax,vmax
+               vals(mtargets)    = (MIN(vmin - coilrectvmin(ik), 0.0)/coilrectpfw)**2
+               mtargets = mtargets + 1
+               targets(mtargets) = target(ik)
+               sigmas(mtargets)  = sigma(ik)
+               vals(mtargets)    = (MAX(vmax - coilrectvmax(ik), 0.0)/coilrectpfw)**2
+               IF (iflag == 1) WRITE(iunit_out,'(I5,4ES22.12E3)') ik,target(ik),sigma(ik),vmin,vmax
             END IF
          END DO
       ELSE
          DO ik = 1, nigroup
             IF ((sigma(ik) < bigno) .AND. ANY(lcoil_spline(ik,:))) THEN
                mtargets = mtargets + 1
-               IF (niter == -2) target_dex(mtargets)=jtarget_coilcrv
+               IF (niter == -2) target_dex(mtargets)=jtarget_coilrect
+               mtargets = mtargets + 1
+               IF (niter == -2) target_dex(mtargets)=jtarget_coilrect
             END IF
          END DO
       END IF
@@ -68,4 +67,4 @@
 !----------------------------------------------------------------------
 !     END SUBROUTINE
 !----------------------------------------------------------------------
-      END SUBROUTINE chisq_coilcrv
+      END SUBROUTINE chisq_coilrect
