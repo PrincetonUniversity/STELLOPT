630e3d5e99a04b0140b4904e07b10ec6874d6fbd
Joshua Breslau <jbreslau@pppl.gov>
2020-04-28 11:17:10 -0400

    Imported latest coil penalty functions from stellcopt.

--- STELLOPTV2/Sources/Chisq/chisq_coillen.f90
+++ STELLOPTV2/Sources/Chisq/chisq_coillen.f90
@@ -5,53 +5,69 @@
 !     Description:   Calculates the difference between the length of each 
 !                    coil and its target value.
 !-----------------------------------------------------------------------
-      SUBROUTINE chisq_coillen(target,sigma,niter,iflag)
+      SUBROUTINE chisq_coillen(target_l,sigma_l,target_v,sigma_v,niter,iflag)
 !-----------------------------------------------------------------------
 !     Libraries
 !-----------------------------------------------------------------------
       USE stellopt_runtime
       USE stellopt_targets
       USE stellopt_vars, ONLY: lcoil_spline
-      
+
 !-----------------------------------------------------------------------
 !     Input/Output Variables
 !
 !-----------------------------------------------------------------------
       IMPLICIT NONE
-      REAL(rprec), INTENT(in)    ::  target(nigroup)
-      REAL(rprec), INTENT(in)    ::  sigma(nigroup)
+      REAL(rprec), INTENT(in)    ::  target_l(nigroup), target_v(nigroup)
+      REAL(rprec), INTENT(in)    ::  sigma_l(nigroup), sigma_v(nigroup)
       INTEGER,     INTENT(in)    ::  niter
       INTEGER,     INTENT(inout) ::  iflag
-      
+
 !-----------------------------------------------------------------------
 !     Local Variables
 !
 !-----------------------------------------------------------------------
       INTEGER     :: ik
-      REAL(rprec) :: local_len
+      REAL(rprec) :: local_len, rmssegvar
 !----------------------------------------------------------------------
 !     BEGIN SUBROUTINE
 !----------------------------------------------------------------------
       IF (iflag < 0) RETURN
-      ik   = COUNT(sigma < bigno)
-      IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'LEN ',ik,4
-      IF (iflag == 1) WRITE(iunit_out,'(A)') 'TARGET  SIGMA  VAL'
+      ik   = COUNT((sigma_l < bigno).OR.(sigma_v < bigno))
+      IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'LEN+SEG.SD ',ik, 7
+      IF (iflag == 1) WRITE(iunit_out,'(A)') 'TARGET  SIGMA  VAL  TARGET  SIGMA  VAL'
       IF (niter >= 0) THEN
          DO ik = 1, nigroup
-            IF ((sigma(ik) < bigno) .AND. ANY(lcoil_spline(ik,:))) THEN
-               mtargets = mtargets + 1
-               CALL get_coil_length(ik, local_len)
-               targets(mtargets) = target(ik)
-               sigmas(mtargets)  = sigma(ik)
-               vals(mtargets)    = local_len
-               IF (iflag == 1) WRITE(iunit_out,'(I5,3ES22.12E3)') ik,target(ik),sigma(ik),local_len
+            IF (((sigma_l(ik) < bigno).OR.(sigma_v(ik) < bigno)) .AND. &
+                 ANY(lcoil_spline(ik,:))) THEN
+               CALL get_coil_length(ik, local_len, rmssegvar)
+               IF (sigma_l(ik) < bigno) THEN
+                  mtargets = mtargets + 1
+                  targets(mtargets) = target_l(ik)
+                  sigmas(mtargets)  = sigma_l(ik)
+                  vals(mtargets)    = local_len
+               END IF
+               IF (sigma_v(ik) < bigno) THEN
+                  mtargets = mtargets + 1
+                  targets(mtargets) = target_v(ik)
+                  sigmas(mtargets)  = sigma_v(ik)
+                  vals(mtargets)    = rmssegvar
+               END IF
+               IF (iflag == 1) WRITE(iunit_out,'(I5,6ES22.12E3)') ik,&
+                    target_l(ik), sigma_l(ik), local_len, target_v(ik), sigma_v(ik), rmssegvar
             END IF
          END DO
       ELSE
          DO ik = 1, nigroup
-            IF ((sigma(ik) < bigno) .AND. ANY(lcoil_spline(ik,:))) THEN
-               mtargets = mtargets + 1
-               IF (niter == -2) target_dex(mtargets)=jtarget_coillen
+            IF (ANY(lcoil_spline(ik,:))) THEN
+               IF (sigma_l(ik) < bigno) THEN
+                  mtargets = mtargets + 1
+                  IF (niter == -2) target_dex(mtargets)=jtarget_coillen
+               END IF
+               IF (sigma_v(ik) < bigno) THEN
+                  mtargets = mtargets + 1
+                  IF (niter == -2) target_dex(mtargets)=jtarget_coilsegvar
+               END IF
             END IF
          END DO
       END IF
