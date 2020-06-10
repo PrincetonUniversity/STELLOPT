b7185bbda7a50bd490a51b7b6a69fbe1cac21843
lazersos <samuel.lazerson@ipp.mpg.de>
2020-06-10 20:23:14 +0200

    STELLOPT: Fixes to parallelization

--- LIBSTELL/Sources/Miscel/common_factor.f
+++ LIBSTELL/Sources/Miscel/common_factor.f
@@ -3,14 +3,13 @@
       INTEGER, INTENT(IN) :: N
       INTEGER, INTENT(IN) :: m
       INTEGER, INTENT(IN) :: ifact
-      INTEGER :: val
+      INTEGER:: val
 
-      
+      val = 0
       IF (mod(N,m)==0) THEN
          val = m
       ELSE
          val = COMMON_FACTOR(N,m+ifact,ifact)
       END IF
 
-      RETURN
       END FUNCTION COMMON_FACTOR
