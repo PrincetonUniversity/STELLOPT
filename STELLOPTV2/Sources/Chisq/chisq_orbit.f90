370cee0d3271f5e169cad685c19c2e0d6abf8736
lazersos <samuel.lazerson@ipp.mpg.de>
2020-06-18 17:39:58 +0200

    STELLOPT: Fixed BEAMS3D benchmark

--- STELLOPTV2/Sources/Chisq/chisq_orbit.f90
+++ STELLOPTV2/Sources/Chisq/chisq_orbit.f90
@@ -13,7 +13,7 @@
       USE stellopt_targets
       USE equil_vals
 !DEC$ IF DEFINED (BEAMS3D_OPT)
-      USE beams3d_input_mod, ONLY: read_beams3d_input, r_start_in
+      USE beams3d_input_mod, ONLY: read_beams3d_input, lbeam, lbbnbi
 !DEC$ ENDIF
       
       
@@ -63,7 +63,8 @@
          END DO
          iflag=0
 !DEC$ IF DEFINED (BEAMS3D_OPT)
-         r_start_in(1) = 1.0 ! So that the beams aren't read.
+         lbeam = .false.
+         lbbnbi = .false.
          CALL read_beams3d_input('input.'//TRIM(id_string), iflag)
 !DEC$ ENDIF
       END IF
