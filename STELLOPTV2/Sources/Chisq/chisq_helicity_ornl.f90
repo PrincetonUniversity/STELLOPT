56d4be00e30e012d251754ff8f1d85726c8840af
Matt Landreman <mattland@umd.edu>
2020-06-24 12:02:32 -0400

    STELLOPTV2: Removed unnecessary print statement

--- STELLOPTV2/Sources/Chisq/chisq_helicity_ornl.f90
+++ STELLOPTV2/Sources/Chisq/chisq_helicity_ornl.f90
@@ -65,7 +65,6 @@
          END IF
          nlmask = .TRUE.
          WHERE(lmask) nlmask = .FALSE.
-         print "(a,i6,a,i7,a,l1)","chisq_helicity_ornl mnboz_b=",mnboz_b," nsd=",nsd," allocated(bmnc_b)=",allocated(bmnc_b)
          DO ik = 1, nsd
             IF (sigma(ik) >= bigno) CYCLE
             bmax  = MAXVAL(ABS(bmnc_b(1:mnboz_b,ik)))
