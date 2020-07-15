a492ae072f51c2ef57be9cfba5312fd0345580e9
lazersos <samuel.lazerson@ipp.mpg.de>
2020-07-15 16:18:09 +0200

    BEAMS3D: Fixed Suzuki interface WRT plasma mass and Z

--- BEAMS3D/Sources/beams3d_physics_mod.f90
+++ BEAMS3D/Sources/beams3d_physics_mod.f90
@@ -500,13 +500,14 @@ MODULE beams3d_physics_mod
             !     Note: 10^18<ne<10^21
             !           E(keV/amu)/100 < Te < E(keV/amu)/2
             !--------------------------------------------------------------
-            Z_in(1)  = NINT(mycharge/e_charge)
-            A_in(1)  = NINT(mymass*inv_dalton)
+            !Z_in(1)  = NINT(mycharge/e_charge)
+            A_in(1)  = NINT(plasma_mass*inv_dalton)
             energy   = energy/(e_charge*A_in(1)) ! keV/amu
             DO l = 1, num_depo
                nelocal(l)  = MAX(MIN(nelocal(l),1E21),1E18)
                telocal(l)  = MAX(MIN(telocal(l),energy(l)/2),1.0E-3)
                ni_in(1) = nelocal(l)/zefflocal(l)
+               Z_in(1)  = NINT(zefflocal(l))
                CALL suzuki_sigma(1,energy(l),nelocal(l),telocal(l),ni_in,A_in,Z_in,tau_inv(l))
             END DO
             tau_inv = tau_inv*nelocal*ABS(q(4))*1E-4
