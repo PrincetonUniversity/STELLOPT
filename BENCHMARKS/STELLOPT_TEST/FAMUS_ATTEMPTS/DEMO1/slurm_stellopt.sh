eab6dfc35a6e18aa0ac54987bb4d9860165c6577
J.C. Schmitt <jcs0114@auburn.edu>
2020-09-02 11:43:34 -0400

    added input files for stellopt/famus test case

---
+++ BENCHMARKS/STELLOPT_TEST/FAMUS_ATTEMPTS/DEMO1/slurm_stellopt.sh
@@ -0,0 +1,16 @@
+#!/bin/tcsh
+#This file is called submit-script.sh
+#SBATCH --partition=centos7
+#SBATCH --time=24:00:00   # run time in days-hh:mm:ss
+#SBATCH --ntasks=32          # default 16 if this line not specified
+#SBATCH --mem=128gb
+#SBATCH -o out.slurm-%j.%N #
+#SBATCH -e err.slurm-%j.%N #
+#Make sure to change the above two lines to reflect your appropriate
+# file locations for standard error and output
+
+source /p/stellopt/ANALYSIS/jschmitt/src/module_fotd_famus_vc7
+mpirun -n 32 --mca mtl ^psm --mca btl ^openib  /p/stellopt/ANALYSIS/jschmitt/src/STELLOPT_FAMUS_V1/STELLOPTV2/Release/xstelloptv2 input.QAS >& log.stellopt
+
+
+
