36bf75527948cdcf65d08cf1fdb0362b1104eaeb
John Schmitt <jschmitt@vulcan.edu>
2021-10-15 10:26:49 -0500

    added a deallocation fix

--- STELLOPTV2/Sources/Chisq/chisq_gamma_c_v2_mod.f90
+++ STELLOPTV2/Sources/Chisq/chisq_gamma_c_v2_mod.f90
@@ -1328,6 +1328,7 @@ contains
         deallocate ( Bsupu )
         deallocate ( Bsupv )
         deallocate ( binormal )
+        deallocate ( myCoords )
         deallocate ( well_start )
         deallocate ( well_stop )
         deallocate ( bp )
