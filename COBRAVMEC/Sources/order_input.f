99b3b65eb9fe738d5439e3fcb31192b4786242ed
lazersos <samuel.lazerson@ipp.mpg.de>
2022-03-12 21:10:53 +0100

    COBRAVMEC: Fix to support nyquist sized arrays

--- COBRAVMEC/Sources/order_input.f
+++ COBRAVMEC/Sources/order_input.f
@@ -25,6 +25,9 @@
       INTEGER, DIMENSION(:), ALLOCATABLE :: plist
       REAL(rprec) :: aspect_v, r0max_v, r0min_v, betaxis_v
       LOGICAL, ALLOCATABLE, DIMENSION(:):: lballoon
+      REAL(rprec), ALLOCATABLE, DIMENSION(:,:) :: 
+     1               rmnc_nyq, zmns_nyq, lmns_nyq,
+     2               rmns_nyq, zmnc_nyq, lmnc_nyq
 !-----------------------------------------------
 !
 !      Variables allocated in this routine (deallocated elsewhere)
@@ -64,8 +67,31 @@
       r0max_v = rmax_surf
       r0min_v = rmin_surf
       betaxis_v = betaxis
-      mnmax_v = mnmax
+      mnmax_v = mnmax_nyq
       lasym_v = lasym                                                         ! 111009: RS, Logical for Asymmetric input
+
+!...   Fix to get the R/Z/L on the nyquist grid
+       ALLOCATE(rmnc_nyq(mnmax_nyq,ns), zmns_nyq(mnmax_nyq,ns),
+     1          lmns_nyq(mnmax_nyq,ns))
+       rmnc=0; zmns=0; lmns=0
+       IF (lasym) ALLOCATE(rmns_nyq(mnmax_nyq,ns), 
+     1          zmnc_nyq(mnmax_nyq,ns),lmnc_nyq(mnmax_nyq,ns))
+       DO j = 1, mnmax_nyq
+          DO i = 1, mnmax
+             IF ((xm(i) .eq. xm_nyq(j)) .and.
+     1           (xn(i) .eq. xn_nyq(j))) THEN
+                PRINT *,xm(i),xm_nyq(j),xn(i),xn_nyq(j)
+                rmnc_nyq(j,:) = rmnc(i,:)
+                zmns_nyq(j,:) = zmns(i,:)
+                lmns_nyq(j,:) = lmns(i,:)
+                IF (lasym) THEN
+                   rmns_nyq(j,:) = rmns(i,:)
+                   zmnc_nyq(j,:) = zmnc(i,:)
+                   lmnc_nyq(j,:) = lmnc(i,:)
+                END IF
+             END IF
+          END DO
+       END DO
 
 !...   IOTA, PRES, PHIP are ALL on HALF-MESH!!!
 
@@ -110,8 +136,8 @@
        if (k .ne. 0) stop 'Allocation error 2 in cobra order_input'
 !       allocate (xm_v(mnmax_v), xn_v(mnmax_v), plist(ns_cob), stat=k)
 !       if (k .ne. 0) stop 'Allocation error 2 in cobra order_input'
-      xm_v = xm(1:mnmax_v)
-      xn_v = xn(1:mnmax_v)
+      xm_v = xm_nyq(1:mnmax_v)
+      xn_v = xn_nyq(1:mnmax_v)
 
       mndim = ns_cob*mnmax_v
 
@@ -162,22 +188,29 @@
 
       DO k=1, ns_cob
          lk = (k-1)*mnmax_v
-         rmncf(lk+1:lk+mnmax_v) = rmnc(1:mnmax_v,k)
-         zmnsf(lk+1:lk+mnmax_v) = zmns(1:mnmax_v,k)
+         rmncf(lk+1:lk+mnmax_v) = rmnc_nyq(1:mnmax_v,k)
+         zmnsf(lk+1:lk+mnmax_v) = zmns_nyq(1:mnmax_v,k)
          bmnch(lk+1:lk+mnmax_v) = bmnc(1:mnmax_v,k)
-         lmnsh(lk+1:lk+mnmax_v) = lmns(1:mnmax_v,k)
+         lmnsh(lk+1:lk+mnmax_v) = lmns_nyq(1:mnmax_v,k)
          bsupvmnch(lk+1:lk+mnmax_v) = bsupvmnc(1:mnmax_v,k)
          bsupumnch(lk+1:lk+mnmax_v) = bsupumnc(1:mnmax_v,k)
          IF (lasym_v) THEN                                                   !  110909 RS: For asymmetric input
-           rmnsf(lk+1:lk+mnmax_v) = rmns(1:mnmax_v,k)
-           zmncf(lk+1:lk+mnmax_v) = zmnc(1:mnmax_v,k)
+           rmnsf(lk+1:lk+mnmax_v) = rmns_nyq(1:mnmax_v,k)
+           zmncf(lk+1:lk+mnmax_v) = zmnc_nyq(1:mnmax_v,k)
            bmnsh(lk+1:lk+mnmax_v) = bmns(1:mnmax_v,k)
-           lmnch(lk+1:lk+mnmax_v) = lmnc(1:mnmax_v,k)
+           lmnch(lk+1:lk+mnmax_v) = lmnc_nyq(1:mnmax_v,k)
            bsupvmnsh(lk+1:lk+mnmax_v) = bsupvmns(1:mnmax_v,k)
            bsupumnsh(lk+1:lk+mnmax_v) = bsupumns(1:mnmax_v,k)
          ENDIF
 
       ENDDO
+
+      IF (ALLOCATED(rmnc_nyq)) DEALLOCATE(rmnc_nyq)
+      IF (ALLOCATED(zmns_nyq)) DEALLOCATE(zmns_nyq)
+      IF (ALLOCATED(lmns_nyq)) DEALLOCATE(lmns_nyq)
+      IF (ALLOCATED(rmns_nyq)) DEALLOCATE(rmns_nyq)
+      IF (ALLOCATED(zmnc_nyq)) DEALLOCATE(zmnc_nyq)
+      IF (ALLOCATED(lmnc_nyq)) DEALLOCATE(lmnc_nyq)
 
 !...   identify wanted surfaces
 
