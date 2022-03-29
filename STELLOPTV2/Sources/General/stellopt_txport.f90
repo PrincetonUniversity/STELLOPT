edd0388d507585e62f834b83746c653ae4e60705
lazersos <samuel.lazerson@ipp.mpg.de>
2022-03-29 08:41:15 +0200

    STELLOPT: Added Available Energy Turbulence Metric

--- STELLOPTV2/Sources/General/stellopt_txport.f90
+++ STELLOPTV2/Sources/General/stellopt_txport.f90
@@ -33,6 +33,9 @@
 !      USE read_boozer_mod
       USE EZspline_obj
       USE EZspline
+!DEC$ IF DEFINED (AEOPT)
+      USE trapped_avail_energy_mod
+!DEC$ ENDIF
 !DEC$ IF DEFINED (GENE)
       USE gene_subroutine, ONLY: rungene
       USE par_in, ONLY: diagdir,file_extension
@@ -530,6 +533,14 @@
                         CALL EZspline_free(Bhat_spl,ier)
                         CALL EZspline_free(L2_spl,ier)
                      END DO
+                  CASE('AvailEnergy')
+!DEC$ IF DEFINED (AEOPT)
+                      CALL compute_AE(maxPnt, g11(ialpha,:), g12(ialpha,:), g22(ialpha,:), Bhat(ialpha,:), &
+                                             abs_jac(ialpha,:), L1(ialpha,:), L2(ialpha,:), dBdt(ialpha,:), &
+                                      1, dpdx, q, shat, 1.0D0, 0.0D0, &
+                                      1D-4, 4.0D+1, 10000, 10000, result)
+                      vqqprox(ialpha,:) = result/maxPnt
+!DEC$ ENDIF
                   CASE('all')
                      ! prox1
                      qqfac=(one+one/(rhsrkp*(one+(tau_s*slocav)**2)))
