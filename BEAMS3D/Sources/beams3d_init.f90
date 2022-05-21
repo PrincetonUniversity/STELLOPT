d04105c0bc62decb40f00b4533d317d2c8e53b96
kudav <david.kulla@ipp.mpg.de>
2022-05-21 15:00:51 +0200

    BEAMS3D: fixed fidasim axis declaration

--- BEAMS3D/Sources/beams3d_init.f90
+++ BEAMS3D/Sources/beams3d_init.f90
@@ -316,10 +316,10 @@
                ALLOCATE(raxis_fida(nr_fida))
                ALLOCATE(zaxis_fida(nz_fida))
                ALLOCATE(phiaxis_fida(nphi_fida))
+               FORALL(i = 1:nr_fida) raxis_fida(i) = (i-1)*(rmax_fida-rmin_fida)/(nr_fida-1) + rmin_fida
+               FORALL(i = 1:nz_fida) zaxis_fida(i) = (i-1)*(zmax_fida-zmin_fida)/(nz_fida-1) + zmin_fida
+               FORALL(i = 1:nphi_fida) phiaxis_fida(i) = (i-1)*(phimax_fida-phimin_fida)/(nphi_fida-1) + phimin_fida
             END IF
-            FORALL(i = 1:nr_fida) raxis_fida(i) = (i-1)*(rmax_fida-rmin_fida)/(nr_fida-1) + rmin_fida
-            FORALL(i = 1:nz_fida) zaxis_fida(i) = (i-1)*(zmax_fida-zmin_fida)/(nz_fida-1) + zmin_fida
-            FORALL(i = 1:nphi_fida) phiaxis_fida(i) = (i-1)*(phimax_fida-phimin_fida)/(nphi_fida-1) + phimin_fida
          END IF
          CALL MPI_BARRIER(MPI_COMM_SHARMEM, ier)
          ! Put the vacuum field on the background grid
