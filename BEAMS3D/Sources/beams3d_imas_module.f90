ee18621b7767178ac206dcb72ed35a62d80970c1
lazersos <samuel.lazerson@ipp.mpg.de>
2022-09-12 11:33:24 +0200

    BEAMS3D: Interim commit of IMAS interface

--- BEAMS3D/Sources/beams3d_imas_module.f90
+++ BEAMS3D/Sources/beams3d_imas_module.f90
@@ -7,179 +7,166 @@
 !-----------------------------------------------------------------------
 MODULE BEAMS3D_IMAS_MODULE
 #if defined(IMAS)
-  !---------------------------------------------------------------------
-  !     Libraries
-  !---------------------------------------------------------------------
-  USE ids_schemas
 
-  !---------------------------------------------------------------------
-  !     MODULE VARIABLES
-  !        IMAS_EQUI : Equilibrium input
-  !        IMAS_MACH : Machine input
-  !        IMAS_PROF : Profile input
-  !---------------------------------------------------------------------
-  TYPE(ids_equilibrium) :: IMAS_EQUI
-  TYPE(ids_equilibrium) :: IMAS_MACH
-  TYPE(ids_equilibrium) :: IMAS_PROF
-   
+CONTAINS
 !-----------------------------------------------------------------------
-!     SUBROUTINE:        VMEC_IMAS
-!     PURPOSE:           RUNS VMEC ASSUMING XML DEFINES RUN LIKE THE
-!                        NAMELIST
+!     SUBROUTINE:        BEAMS3D_IMAS_INIT
+!     PURPOSE:           Handles Initialization of BEAMS3D
 !-----------------------------------------------------------------------
-SUBROUTINE BEAMS3D_IMAS_NBI(EQ_IN, MACHINE_IN, PROFILES_IN, INDATA, &
-                        status_code, status_message)
+SUBROUTINE BEAMS3D_IMAS_INIT(INDATA_XML, status_code, status_message)
   !---------------------------------------------------------------------
   !     Libraries
   !---------------------------------------------------------------------
   USE ids_schemas
-  USE beams3d_runtime
-  USE wall_mod, ONLY: wall_free
-  USE mpi_params
-  USE mpi_inc
-#if defined(LHDF5)
-  USE hdf5
-#endif
+  USE beams3d_input_mod, ONLY: init_beams3d_input, read_beams3d_input
 
   !---------------------------------------------------------------------
   !     INPUT/OUTPUT VARIABLES
-  !        EQ_IN : Equilibrium input
-  !        MACHINE_IN : Machine input
-  !        PROFILES_IN : Profile input
+  !        INDATA_XML : VMEC INDATA namelist AS XML
   !        STATUS_CODE : STATUS Flag
   !        STATUS_MESSAGE : Text Message
   !---------------------------------------------------------------------
   IMPLICIT NONE
-  TYPE(ids_equilibrium), INTENT(IN) :: EQ_IN
-  TYPE(ids_equilibrium), INTENT(IN) :: MACHINE_IN
-  TYPE(ids_equilibrium), INTENT(IN) :: PROFILES_IN
-  TYPE(ids_parameters_input), INTENT(IN) :: INDATA
+  TYPE(ids_parameters_input), INTENT(IN) :: INDATA_XML
+  INTEGER, INTENT(OUT) :: status_code
+  CHARACTER(LEN=:), POINTER, INTENT(OUT) :: status_message
+
+  !---------------------------------------------------------------------
+  !     BEGIN EXECUTION
+  !---------------------------------------------------------------------
+
+  status_code = 0
+  !status_message = 'OK'
+
+  !----  Set defaults ----
+  CALL init_beams3d_input
+
+  !----  Read XML ----
+  CALL beams3d_input_imas(INDATA_XML,status_code,status_message)
+
+  !----  Read XML ----
+  CALL read_beams3d_input('IMAS',status_code)
+
+  RETURN
+END SUBROUTINE BEAMS3D_IMAS_INIT
+
+!-----------------------------------------------------------------------
+!     SUBROUTINE:        BEAMS3D_INPUT_IMAS
+!     PURPOSE:           Handles setting up the input variables
+!-----------------------------------------------------------------------
+SUBROUTINE BEAMS3D_INPUT_IMAS(INDATA_XML, status_code, status_message)
+  !---------------------------------------------------------------------
+  !     Libraries
+  !---------------------------------------------------------------------
+  USE ids_schemas
+  USE xml2eg_mdl, only: xml2eg_parse_memory, xml2eg_get, type_xml2eg_document, xml2eg_free_doc, set_verbose
+  USE beams3d_input_mod
+
+  !---------------------------------------------------------------------
+  !     INPUT/OUTPUT VARIABLES
+  !        INDATA_XML : VMEC INDATA namelist AS XML
+  !        STATUS_CODE : STATUS Flag
+  !        STATUS_MESSAGE : Text Message
+  !---------------------------------------------------------------------
+  IMPLICIT NONE
+  TYPE(ids_parameters_input), INTENT(IN) :: INDATA_XML
   INTEGER, INTENT(OUT) :: status_code
   CHARACTER(LEN=:), POINTER, INTENT(OUT) :: status_message
 
   !---------------------------------------------------------------------
   !     SUBROUTINE VARIABLES
   !---------------------------------------------------------------------
-  LOGICAL :: lmpi_flag
-  INTEGER :: impi_flag
+  LOGICAL :: lmpi_flag, lscreen
+  INTEGER :: impi_flag, ivmec_flag, iunit
+  INTEGER :: ictrl(5)
+  CHARACTER(len = 128)    :: reset_string
+  REAL(rprec) :: Xmn(0:mpol1d)
+
+  TYPE(type_xml2eg_document) :: doc
 
   !---------------------------------------------------------------------
   !     BEGIN EXECUTION
   !---------------------------------------------------------------------
 
-  !----  MPI initialisation ----
-#if defined(MPI_OPT)
-  CALL MPI_initialized(lmpi_flag, impi_flag)
-  if (.not. lmpi_flag)   call MPI_INIT(impi_flag)
-#endif
-
-  !----  Handle the MPI Communicators ----
-#if defined(MPI_OPT)
-  CALL MPI_COMM_DUP( MPI_COMM_WORLD, MPI_COMM_BEAMS, ierr_mpi)
-  CALL MPI_COMM_RANK(MPI_COMM_BEAMS, myworkid, ierr_mpi) ! MPI
-  CALL MPI_COMM_SIZE(MPI_COMM_BEAMS, nprocs_beams, ierr_mpi) ! MPI
-  CALL MPI_COMM_SPLIT_TYPE(MPI_COMM_BEAMS, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, MPI_COMM_SHARMEM, ierr_mpi)
-  CALL MPI_COMM_RANK(MPI_COMM_SHARMEM, myid_sharmem, ierr_mpi)
-  CALL MPI_COMM_SIZE(MPI_COMM_SHARMEM, nshar, ierr_mpi) ! MPI
-  CALL MPI_GET_VERSION(vmajor,vminor,ierr_mpi)
-  CALL MPI_GET_LIBRARY_VERSION(mpi_lib_name,liblen,ierr_mpi)
-  ! Now we set some info
-  CALL MPI_INFO_CREATE(mpi_info_beams3d, ierr_mpi)
-  CALL MPI_INFO_SET(mpi_info_beams3d, "IBM_largeblock_io", "true",    ierr_mpi)
-  CALL MPI_INFO_SET(mpi_info_beams3d, "stripping_unit",    "1048576", ierr_mpi)
-  CALL MPI_INFO_SET(mpi_info_beams3d, "romio_ds_read",     "disable", ierr_mpi)
-  CALL MPI_INFO_SET(mpi_info_beams3d, "romio_ds_write",    "disable", ierr_mpi)
-#endif
-
-  !----  HDF5  ----
-#if defined(LHDF5)
-  CALL H5GET_LIBVERSION_F(h5major, h5minor, h5rel, ier)
-  h5par = 0
-#endif
-#if defined(HDF5_PAR)
-  h5par = 1
-#endif
-
-  !----  Constants  ----
-  pi = 4.0 * ATAN(1.0)
-  pi2 = 8.0 * ATAN(1.0)
-  invpi2 = 1./pi2
-  mu0 = (16.0E-7) * ATAN(1.0)
-  to3 = REAL(2)/REAL(3)
-
-  !----  Defaults  ----
-  lverb        = .FALSE.
-  lvmec        = .FALSE.
-  lpies        = .FALSE.
-  lspec        = .FALSE.
-  leqdsk       = .FALSE.
-  lcoil        = .FALSE.
-  lmgrid       = .FALSE.
-  lascot       = .FALSE.
-  lascotfl     = .FALSE.
-  lascot4      = .FALSE.
-  lbbnbi       = .FALSE.
-  lraw         = .FALSE.
-  lvessel      = .FALSE.
-  lvac         = .FALSE.
-  lrestart_grid     = .FALSE.
-  lrestart_particles     = .FALSE.
-  lbeam_simple = .FALSE.
-  lhitonly           = .FALSE. 
-  lplasma_only = .FALSE.
-  lbeam        = .FALSE.
-  lread_input  = .FALSE.
-  lcollision   = .FALSE.
-  lw7x   = .FALSE.
-  lrandomize = .FALSE.
-  lsuzuki = .FALSE.
-  id_string    = TRIM(file_str)
-  coil_string  = ''
-  mgrid_string = ''
-  vessel_string = ''
-  restart_string = ''
-  eqdsk_string = ''
-
-  !----  Initialize the code  ----
-  CALL beams3d_init
-
-  !----  Follow particles  ----
-  CALL beams3d_follow
-
-  !----  Write ouput  ----
-  CALL beams3d_write('TRAJECTORY_PARTIAL')
-  IF (lascot) THEN
-     IF (lascotfl) THEN
-        CALL beams3d_write_ascoth5('FIELDLINES')
-     ELSE
-        CALL beams3d_write_ascoth5('MARKER')
-     END IF
-  END IF
-  IF (lascot4) CALL beams3d_write_ascoth4('MARKER')
-
-  !----  Diagnostics ----
-  CALL beams3d_diagnostics
-
-  !----  Clean up memory ----
-  CALL beams3d_free(MPI_COMM_SHARMEM)
-  CALL wall_free(ier,MPI_COMM_SHARMEM)
-              
-  !----  Free local communicators ----
-  CALL MPI_COMM_FREE(MPI_COMM_SHARMEM,ierr_mpi)
-  CALL MPI_COMM_FREE(MPI_COMM_BEAMS,ierr_mpi)
-  CALL MPI_INFO_FREE(mpi_info_beams3d, ierr_mpi)
-
-  !---------------------------------------------------------------------
-  !     END EXECUTION - Don't touch below here
-  !---------------------------------------------------------------------
-
-  !----  MPI Finalisation ----
-#if defined(MPI_OPT)
-  call MPI_finalized(lmpi_flag, impi_flag)
-  IF (.NOT. lmpi_flag)   CALL MPI_Finalize(impi_flag)
-#endif
+  !----  Intitailizations (This is a hack to set defaults)
+  CALL vsetup(0)
+  iunit = -327
+  CALL read_indata_namelist(iunit,status_code)
   
-END SUBROUTINE BEAMS3D_IMAS
+  !----  Now setup the run based on the xml
+  CALL xml2eg_parse_memory(INPUT_XML%parameters_value,doc)
+  CALL SET_VERBOSE(.TRUE.) ! Only needed if you want to see what's going on in the parsing
+  CALL xml2eg_get(doc,'NR',nr)
+  CALL xml2eg_get(doc,'NPHI',nphi)
+  CALL xml2eg_get(doc,'NZ',nz)
+  CALL xml2eg_get(doc,'RMIN',rmin)
+  CALL xml2eg_get(doc,'RMAX',rmax)
+  CALL xml2eg_get(doc,'ZMIN',zmin)
+  CALL xml2eg_get(doc,'ZMAX',zmax)
+  CALL xml2eg_get(doc,'PHIMIN',phimin)
+  CALL xml2eg_get(doc,'PHIMAX',phimax)
+  CALL xml2eg_get(doc,'NPARTICLES_START',nparticles_start)
+  CALL xml2eg_get(doc,'R_START_IN',r_start_in)
+  CALL xml2eg_get(doc,'PHI_START_IN',phi_start_in)
+  CALL xml2eg_get(doc,'Z_START_IN',z_start_in)
+  CALL xml2eg_get(doc,'VLL_START_IN',vll_start_in)
+  CALL xml2eg_get(doc,'MU_START_IN',mu_start_in)
+  CALL xml2eg_get(doc,'T_END_IN',t_end_in)
+  CALL xml2eg_get(doc,'CHARGE_IN',charge_in)
+  CALL xml2eg_get(doc,'MASS_IN',mass_in)
+  CALL xml2eg_get(doc,'ZATOM_IN',zatom_in)
+  CALL xml2eg_get(doc,'NPOINC',npoinc)
+  CALL xml2eg_get(doc,'FOLLOW_TOL',follow_tol)
+  CALL xml2eg_get(doc,'INT_TYPE',int_type)
+  CALL xml2eg_get(doc,'VC_ADAPT_TOL',vc_adapt_tol)
+  CALL xml2eg_get(doc,'ADIST_BEAMS',adist_beams)
+  CALL xml2eg_get(doc,'ASIZE_BEAMS',asize_beams)
+  CALL xml2eg_get(doc,'DIV_BEAMS',div_beams)
+  CALL xml2eg_get(doc,'E_BEAMS',E_beams)
+  CALL xml2eg_get(doc,'P_BEAMS',P_beams)
+  CALL xml2eg_get(doc,'DEX_BEAMS',dex_beams)
+  CALL xml2eg_get(doc,'MASS_BEAMS',mass_beams)
+  CALL xml2eg_get(doc,'CHARGE_BEAMS',charge_beams)
+  CALL xml2eg_get(doc,'ZATOM_BEAMS',Zatom_beams)
+  CALL xml2eg_get(doc,'R0_BEAMS',r_beams_in(:,1))
+  CALL xml2eg_get(doc,'R1_BEAMS',r_beams_in(:,2))
+  CALL xml2eg_get(doc,'PHI0_BEAMS',phi_beams_in(:,1))
+  CALL xml2eg_get(doc,'PHI1_BEAMS',phi_beams_in(:,2))
+  CALL xml2eg_get(doc,'Z0_BEAMS',z_beams_in(:,1))
+  CALL xml2eg_get(doc,'Z1_BEAMS',z_beams_in(:,2))
+  CALL xml2eg_get(doc,'TE_AUX_S',TE_AUX_S)
+  CALL xml2eg_get(doc,'TE_AUX_F',TE_AUX_F)
+  CALL xml2eg_get(doc,'NE_AUX_S',NE_AUX_S)
+  CALL xml2eg_get(doc,'NE_AUX_F',NE_AUX_F)
+  CALL xml2eg_get(doc,'TI_AUX_S',TI_AUX_S)
+  CALL xml2eg_get(doc,'TI_AUX_F',TI_AUX_F)
+  CALL xml2eg_get(doc,'POT_AUX_S',POT_AUX_S)
+  CALL xml2eg_get(doc,'POT_AUX_F',POT_AUX_F)
+  CALL xml2eg_get(doc,'ZEFF_AUX_S',ZEFF_AUX_S)
+  CALL xml2eg_get(doc,'ZEFF_AUX_F',ZEFF_AUX_F)
+  CALL xml2eg_get(doc,'NI_AUX_Z',NI_AUX_Z)
+  CALL xml2eg_get(doc,'NI_AUX_M',NI_AUX_M)
+  CALL xml2eg_get(doc,'NI_AUX_S',NI_AUX_S)
+  CALL xml2eg_get(doc,'NI01_AUX_F',NI_AUX_F(1,:))
+  CALL xml2eg_get(doc,'NI02_AUX_F',NI_AUX_F(2,:))
+  CALL xml2eg_get(doc,'NI03_AUX_F',NI_AUX_F(3,:))
+  CALL xml2eg_get(doc,'NI04_AUX_F',NI_AUX_F(4,:))
+  CALL xml2eg_get(doc,'NE_SCALE',NE_SCALE)
+  CALL xml2eg_get(doc,'TE_SCALE',TE_SCALE)
+  CALL xml2eg_get(doc,'TI_SCALE',TI_SCALE)
+  CALL xml2eg_get(doc,'ZEFF_SCALE',ZEFF_SCALE)
+  CALL xml2eg_get(doc,'PLASMA_MASS',plasma_mass)
+  CALL xml2eg_get(doc,'PLASMA_ZMEAN',plasma_zmean)
+  CALL xml2eg_get(doc,'THERM_FACTOR',therm_factor)
+  CALL xml2eg_get(doc,'FUSION_SCALE',fusion_scale)
+
+  ! Make sure to clean up after you!!
+  ! When calling "xml2eg_parse_memory" memory was allocated in the "doc" object.
+  ! This memory is freed by "xml2eg_free_doc(doc)"
+  CALL xml2eg_free_doc(doc)
+
+  RETURN
+END SUBROUTINE BEAMS3D_INPUT_IMAS
 
 #endif
 END MODULE BEAMS3D_IMAS_MODULE
