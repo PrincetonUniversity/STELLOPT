3839977e79443099fbcf6280a045847bfb0fd30e
lazersos <samuel.lazerson@ipp.mpg.de>
2022-10-08 09:54:25 +0200

    LIBSTELL: Git version now has default values if no git

--- LIBSTELL/Sources/Modules/git_version_mod.f90
+++ LIBSTELL/Sources/Modules/git_version_mod.f90
@@ -6,5 +6,11 @@ MODULE GIT_VERSION_MOD
    CHARACTER(40), PARAMETER :: git_hash = GIT_HASH_EXT
    CHARACTER(32), PARAMETER :: git_branch = GIT_BRANCH_EXT
    CHARACTER(19), PARAMETER :: built_on = BUILT_ON_EXT
+#else
+   CHARACTER(64), PARAMETER :: git_repository = "not from a git repo"
+   CHARACTER(32), PARAMETER :: git_version = ""
+   CHARACTER(40), PARAMETER :: git_hash = ""
+   CHARACTER(32), PARAMETER :: git_branch = ""
+   CHARACTER(19), PARAMETER :: built_on = ""
 #endif
 END MODULE GIT_VERSION_MOD
