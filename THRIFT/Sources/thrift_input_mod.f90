9121eab2238807dc9b3e18efda037f50a340bd29
lazersos <samuel.lazerson@ipp.mpg.de>
2022-10-31 14:28:45 +0100

    THRIFT: Added thrift_input_mod

---
+++ THRIFT/Sources/thrift_input_mod.f90
@@ -0,0 +1,96 @@
+!-----------------------------------------------------------------------
+!     Module:        beams3d_input_mod
+!     Authors:       C. van Ham
+!     Date:          11/XX/2022
+!     Description:   This module contains the THRIFT input namelist and
+!                    subroutine which initializes and reads the
+!                    THRIFT input namelist.
+!-----------------------------------------------------------------------
+      MODULE thrift_input_mod
+!-----------------------------------------------------------------------
+!     Libraries
+!-----------------------------------------------------------------------
+      USE stel_kinds, ONLY: rprec
+      USE thrift_runtime
+
+!-----------------------------------------------------------------------
+!     Module Variables
+!
+!-----------------------------------------------------------------------
+      IMPLICIT NONE
+!-----------------------------------------------------------------------
+!     Input Namelists
+!         &thrift_input
+!            nr             Number of radial gridpoints
+!-----------------------------------------------------------------------
+      NAMELIST /thrift_input/ nparallel_runs,bootstrap_type
+      
+!-----------------------------------------------------------------------
+!     Subroutines
+!         init_thrift_input:   Initializes the namelist
+!         read_thrift_input:   Reads the namelist
+!-----------------------------------------------------------------------
+      CONTAINS
+
+      SUBROUTINE init_thrift_input
+      IMPLICIT NONE
+      nparallel_runs     = 1
+      mboz               = 32
+      nboz               = 16
+      RETURN
+      END SUBROUTINE init_thrift_input
+      
+      SUBROUTINE read_thrift_input(filename, istat)
+      IMPLICIT NONE
+      CHARACTER(*), INTENT(in) :: filename
+      INTEGER, INTENT(out) :: istat
+      LOGICAL :: lexist
+      INTEGER :: iunit
+      CHARACTER(LEN=1000) :: line
+
+      ! Read namelist
+      IF (filename /= 'IMAS') THEN
+         istat=0
+         iunit=12
+         INQUIRE(FILE=TRIM(filename),EXIST=lexist)
+         IF (.not.lexist) stop 'Could not find input file'
+         CALL safe_open(iunit,istat,TRIM(filename),'old','formatted')
+         IF (istat /= 0) CALL handle_err(NAMELIST_READ_ERR,'thrift_input in: '//TRIM(filename),istat)
+         READ(iunit,NML=thrift_input,IOSTAT=istat)
+         IF (istat /= 0) THEN
+            backspace(iunit)
+            read(iunit,fmt='(A)') line
+            write(6,'(A)') 'Invalid line in namelist: '//TRIM(line)
+            CALL handle_err(NAMELIST_READ_ERR,'thrift_input in: '//TRIM(filename),istat)
+         END IF
+         CLOSE(iunit)
+      END IF
+      RETURN
+      END SUBROUTINE read_thrift_input
+
+      SUBROUTINE write_thrift_namelist(iunit_out, istat)
+      INTEGER, INTENT(in) :: iunit_out
+      INTEGER, INTENT(out) :: istat
+      INTEGER :: ik, n
+      CHARACTER(LEN=*), PARAMETER :: outboo  = "(2X,A,1X,'=',1X,L1)"
+      CHARACTER(LEN=*), PARAMETER :: outint  = "(2X,A,1X,'=',1X,I0)"
+      CHARACTER(LEN=*), PARAMETER :: outflt  = "(2X,A,1X,'=',1X,ES22.12E3)"
+      CHARACTER(LEN=*), PARAMETER :: outexp  = "(2X,A,1X,'=',1X,ES22.12E3)"
+      CHARACTER(LEN=*), PARAMETER :: outcmp  = "(2x,A,1X,'=','(',i3,',',i3,')')"
+      CHARACTER(LEN=*), PARAMETER :: outstr  = "(2X,A,1X,'=',1X,'''',A,'''')"
+      CHARACTER(LEN=*), PARAMETER :: onevar  = "(2X,A,1X,'=',1X,L1,2(2X,A,1X,'=',1X,ES22.12E3))"
+      CHARACTER(LEN=*), PARAMETER :: vecvar  = "(2X,A,'(',I3.3,')',1X,'=',1X,ES22.12E3)"
+      CHARACTER(LEN=*), PARAMETER :: vecvar2  = "(2X,A,'(',I3.3,',',I3.3,')',1X,'=',1X,ES22.12E3)"
+      istat = 0
+      WRITE(iunit_out,'(A)') '&THRIFT_INPUT'
+      WRITE(iunit_out,'(A)') '!---------- GENERAL PARAMETERS ------------'
+      WRITE(iunit_out,outint) 'NPARALLEL_RUNS',nparallel_runs
+      WRITE(iunit_out,outstr) 'BOOTSTRAP_TYPE',bootstrap_type
+      WRITE(iunit_out,'(A)') '!---------- BOOZER TRANSFORMATION ------------'
+      WRITE(iunit_out,outint) 'MBOZ',mboz
+      WRITE(iunit_out,outint) 'MBOZ',nboz
+      WRITE(iunit_out,'(A)') '/'
+      RETURN
+      END SUBROUTINE write_thrift_namelist
+
+      END MODULE THRIFT_INPUT_MOD
