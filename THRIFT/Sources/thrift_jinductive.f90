51a5f64ae29c7b80611cd16e552f0ea7ab3c16bd
lvanham <lucas.vanham@gmail.com>
2023-02-03 16:20:11 +0100

    THRIFT: Trying to get Jinductive to work

--- THRIFT/Sources/thrift_jinductive.f90
+++ THRIFT/Sources/thrift_jinductive.f90
@@ -1,7 +1,7 @@
 !-----------------------------------------------------------------------
 !     Subroutine:    thrift_jinductive
 !     Authors:       L. van Ham
-!     Date:          11/14/2022
+!     Date:          01/23/2023
 !     Description:   This subroutine updates the plasma response to
 !                    source currents.  
 !-----------------------------------------------------------------------
@@ -20,9 +20,10 @@
 !        ier         Error flag
 !-----------------------------------------------------------------------
       IMPLICIT NONE
+      LOGICAL :: lverbose
       INTEGER :: i, ier
-      REAL(rprec) :: rho,s,s11,s12,s21,s22,iota,phip, etapara
-      REAL(rprec), DIMENSION(:), ALLOCATABLE :: f1, f2, f3
+      REAL(rprec) :: rho,s,s11,s12,Bav,Bsqav,iota,phip,etapara
+      REAL(rprec), DIMENSION(:), ALLOCATABLE :: f1, f2, f3, f4
       TYPE(EZspline1_r8) :: f_spl
 !----------------------------------------------------------------------
 !     BEGIN SUBROUTINE
@@ -30,52 +31,75 @@
 !     The general idea here is to use equation (22) in form to update
 !     THRIFT_JPLASMA.  Where THRIFT_JSOURCE is the J in <J_s.B>.
 
+      lverbose = .true.
       ! Allocations
-      ALLOCATE(f1(nrho),f2(nrho),f3(nrho))
-      f1=0; f2=0; f3=0;
+      ALLOCATE(f1(nrho),f2(nrho),f3(nrho),f4(nrho))
+      f1=0; f2=0; f3=0; f4=0;
 
-      ! First we calculate helpers
       DO i = 1, nrho
          rho = THRIFT_RHO(i)
          s   = rho*rho
          ier = 0
-         CALL get_equil_sus(s,s11,s12,s21,s22,ier)
-         CALL EZspline_interp(iota_spl,rho,iota,ier)
-         f1(i) = (s11*iota+s12)
-         f2(i) = (s21*iota+s22)
+         CALL get_equil_sus(s,s11,s12,Bav,Bsqav,ier) 
+         CALL EZspline_interp(iota_spl,rho,iota,ier)   
+         CALL EZspline_interp(phip_spl,rho,phip,ier)
+         f1(i) = invmu0*phip*(s11*iota+s12) ! I(rho)
+         f2(i) = f1(i)/phip ! I/Phi'     
       END DO
 
-      ! Now we need to allocate a helper Sup/Sdn and take derivaives
       bcs1=(/ 0, 0/)
       CALL EZspline_init(f_spl,nrho,bcs1,ier)
       f_spl%isHermite   = 0
-      CALL EZspline_setup(f_spl,f1/f2,ier,EXACT_DIM=.true.)
+      CALL EZspline_setup(f_spl,f2,ier,EXACT_DIM=.true.)
+     
       DO i = 1, nrho
-         CALL EZspline_derivative(f_spl,1,THRIFT_RHO(i),f3(i),ier)
-         CALL EZspline_interp(phip_spl,THRIFT_RHO(i),phip,ier)
-         f3(i)=f3(i)*phip
-         CALL get_equil_Bav(s,s11,s22,ier)
-         f1(i) = s11 ! Bsq <B>
+         rho = THRIFT_RHO(i)
+         s   = rho*rho
+         ier = 0
+
+         CALL EZspline_derivative(f_spl,1,rho,f3(i),ier)
+         CALL get_equil_Bav(s,Bav,Bsqav,ier) ! Bav =<B>;Bsqav=<B^2>
+         CALL get_prof_pprime(rho,THRIFT_T(mytimestep),s12) !s12 = p'
+         CALL get_prof_etapara(rho,THRIFT_T(mytimestep),etapara) !eta = eta
+         CALL EZspline_interp(vp_spl,rho,phip,ier) ! phip = V'
+         f4(i)= etapara*phip*( mu0*s12*f2(i) + 0.5*Bsqav/rho*f3(i) &
+               - THRIFT_JSOURCE(i,mytimestep)*Bav)      
       END DO
+
       CALL EZspline_free(f_spl,ier)
-      f3 = f3*f2*f2
-      f1 = f1*2*mu0
 
-      ! Now we subtrac the driven current
-      !   THRIFT_JSOURCE is dI/ds  but we need <j.B>
-      !    <j.B> = a * rho * <B> * dI/ds / dV/ds [A*T/m^2]
+      bcs1=(/ 0, 0/)
+      CALL EZspline_init(f_spl,nrho,bcs1,ier)
+      f_spl%isHermite   = 0
+      CALL EZspline_setup(f_spl,f4,ier,EXACT_DIM=.true.)
+      !f4 = THRIFT_T(mytimestep+1)-THRIFT_T(mytimestep)
+
       DO i = 1, nrho
-         CALL get_prof_etapara(THRIFT_RHO(i),THRIFT_T(mytimestep),etapara)
-         CALL EZspline_interp(vp_spl,THRIFT_RHO(i),s11,ier)
-         f3(i) = (f3(i)/mu0 - f1(i)*THRIFT_JSOURCE(i,mytimestep)*s11)*etapara
+         rho = THRIFT_RHO(i)
+         s   = rho*rho
+         ier = 0
+         CALL get_equil_sus(s,s11,s12,Bav,Bsqav,ier) 
+         s12 = THRIFT_T(mytimestep+1)-THRIFT_T(mytimestep) !timestep
+         CALL EZspline_derivative(f_spl,1,rho,f3(i),ier)
+         CALL Ezspline_interp(phip_spl,rho,phip,ier)
+         f2(i) = f1(i) + 0.5*invmu0*s12*phip*s11*f3(i)/(rho*eq_phiedge**2) !I next
       END DO
 
-      ! Now we take the derivative d/dPhi = 1/(dPhi/drho) dShelp/drho
+      CALL EZspline_free(f_spl,ier)
+      bcs1=(/ 0, 0/)
+      CALL EZspline_init(f_spl,nrho,bcs1,ier)
+      f_spl%isHermite   = 0
+      CALL EZspline_setup(f_spl,f2,ier,EXACT_DIM=.true.)
+      DO i = 1, nrho
+         rho = THRIFT_RHO(i)
+         CALL EZspline_derivative(f_spl,1,rho,f1(i),ier)
+         CALL Ezspline_interp(vp_spl,rho,phip,ier)
+         THRIFT_JPLASMA(i,mytimestep) = 2*pi*eq_Rmajor*f1(i)/phip
+      END DO     
+      CALL EZspline_free(f_spl,ier)
 
-
-      
       ! Deallocate
-      DEALLOCATE(f1,f2)
+      DEALLOCATE(f1,f2,f3,f4)
 
       RETURN
 !----------------------------------------------------------------------
