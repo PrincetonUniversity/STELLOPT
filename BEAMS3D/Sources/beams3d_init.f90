0e6d14cb0c48188d4725aea31063e28918fd6817
kudav <david.kulla@ipp.mpg.de>
2023-02-21 16:51:56 +0100

    BEAMS3D: fidasim2 dist red. by 1dim to save mem.

--- BEAMS3D/Sources/beams3d_init.f90
+++ BEAMS3D/Sources/beams3d_init.f90
@@ -113,6 +113,11 @@
         lbeam = .false.
       END IF
 
+      IF (ldepo) THEN
+         lfidasim = .false.
+         lfidasim2 = .false.
+      END IF
+
       ! Handle existence of ADAS for NBI
       IF (lbeam .and. .not.lsuzuki .and. myid_sharmem==master) THEN
          lsuzuki = .not.adas_tables_avail()
@@ -692,7 +697,7 @@
                ndot_prof(nbeams,ns_prof1))
       ipower_prof=0; epower_prof=0; ndot_prof=0
       CALL mpialloc(dist5d_prof, nbeams, ns_prof1, ns_prof2, ns_prof3, ns_prof4, ns_prof5, myid_sharmem, 0, MPI_COMM_SHARMEM, win_dist5d)
-      IF (lfidasim2)       CALL mpialloc(dist5d_fida, nbeams, nr_fida, nz_fida, nphi_fida, nenergy_fida, npitch_fida, myid_sharmem, 0, MPI_COMM_SHARMEM, win_dist5d_fida)
+      IF (lfidasim2)       CALL mpialloc(dist5d_fida, nr_fida, nz_fida, nphi_fida, nenergy_fida, npitch_fida, myid_sharmem, 0, MPI_COMM_SHARMEM, win_dist5d_fida)
       IF (myid_sharmem == master) THEN
          dist5d_prof = 0
          IF (lfidasim2) dist5d_fida = 0
