cc443ec134f9140f4ea6f32232c111914bffc27c
Lucas van Ham <lucas.vanham@gmail.com>
2023-02-22 15:40:17 +0100

    THRIFT: Change print settings

--- THRIFT/Sources/thrift_evolve.f90
+++ THRIFT/Sources/thrift_evolve.f90
@@ -123,7 +123,7 @@
             END IF
 
             ! Print progress
-            IF (lverb) WRITE(6,'(1X,F5.2,1X,I2,1X,F5.2,5(1X,ES8.2),1X,ES8.2)') &
+            IF (lverb) WRITE(6,'(1X,F5.2,1X,I2,1X,F5.2,5(1X,ES10.2),1X,ES10.2)') &
                 THRIFT_T(mytimestep),nsubsteps,eq_beta*100,&
                 eq_volume*SUM(THRIFT_J(:,mytimestep))/(pi2*eq_Rmajor*nrho),&
                 eq_volume*SUM(THRIFT_JPLASMA(:,mytimestep))/(pi2*eq_RMajor*nrho),&
