c25e58018d718dddc6b2cd4b34ea084d21dc4af1
Lucas van Ham <lucas.vanham@gmail.com>
2023-02-23 16:43:11 +0100

    THRIFT: Add edge case when tstart = 0

--- THRIFT/Sources/thrift_jinductive.f90
+++ THRIFT/Sources/thrift_jinductive.f90
@@ -40,15 +40,18 @@
       lverbmat    = .false.
       lverbpost   = .true.
 
+      THRIFT_I        = 0; THRIFT_IBOOT    = 0; THRIFT_IPLASMA  = 0
+      THRIFT_IECCD    = 0; THRIFT_INBCD    = 0; THRIFT_IOHMIC   = 0
 
       ! Check to make sure we're not zero beta
       IF (eq_beta == 0) THEN
-         IF (mytimestep == 1) THEN 
-            THRIFT_JPLASMA(:,mytimestep) = 0
-         ELSE 
-            THRIFT_JPLASMA(:,mytimestep) = THRIFT_JPLASMA(:,mytimestep-1)
-         END IF
-         RETURN
+         IF (mytimestep /= 1) THRIFT_JPLASMA(:,mytimestep) = THRIFT_JPLASMA(:,mytimestep-1)
+         GOTO 1000
+      END IF
+      ! Check to make sure delta t != 0
+      IF (tstart==0 .and. mytimestep==1) THEN
+         THRIFT_JPLASMA(:,mytimestep)=-THRIFT_JSOURCE(:,mytimestep)
+         GOTO 1000
       END IF
 
       ! Allocations
@@ -136,9 +139,12 @@
       END IF
 
       ! Timesteps and grid spacing 
-      k = THRIFT_T(2)-THRIFT_T(1)      ! k <- dt
+      IF (mytimestep == 1) THEN
+         k = tstart
+      ELSE
+         k = THRIFT_T(mytimestep)-THRIFT_T(mytimestep-1)
+      END IF
       h = THRIFT_RHO(2)-THRIFT_RHO(1)  ! h <- drho
-      ! NB: If grids are changed to be non-uniform, adjust this accordingly
 
       ! Visualisation of different grids
       !j=1 2    3    4    5    6    7
@@ -403,73 +409,6 @@
          WRITE(6,*)''
       END IF    
 
-      ! J_plasma = dI_plasma/dA = dI/ds ds/dA =1/2rho dI/drho 2pi R/V' 
-      !          = pi R/(rho v') dI/drho = pi R / (rho phi_a dV/dphi) dI_plasma/drho
-      !DO i = 1, nrho
-      !   ! Calculate derivative, store in temp1
-      !   IF (i == 1) THEN ! Symmetry BC
-      !      temp1 = (B(2)-B(1))/(2*h)
-      !   ELSE IF (i == nrho) THEN ! 
-      !      temp1 = B(nrho) ! Set current at nrho = nrho-1 for now
-      !      temp1 = (4*temp1-3*B(nrho)-B(nrho-1))/(3*h) 
-      !   ELSE
-      !      temp1 = (B(i+1)-B(i-1))/(2*h)
-      !   END IF
-      !   rho = THRIFT_RHO(i)
-      !   s = rho*rho
-      !   ier = 0
-      !   CALL EZspline_interp(vp_spl,rho,vp,ier)
-      !   CALL get_equil_Rmajor(s,Rmajor,s11,Aminor,ier)
-      !   THRIFT_JPLASMA(i,mytimestep) = pi*Rmajor/(eq_phiedge*rho*vp)*temp1
-      !END DO
-
-      ! J_plasma = dI_plasma/dA = dI_plasma/drho / dA/drho
-      IF (.false.) THEN
-         DO i = 1, nrho
-            ! Calculate derivatives
-            IF (i == 1) THEN ! Symmetry BC
-               rho = THRIFT_RHO(1)
-               s = rho*rho
-               ier = 0
-               CALL get_equil_Rmajor(s,h,h,Aminor,ier) 
-               rho = THRIFT_RHO(2)
-               s = rho*rho
-               ier = 0
-               CALL get_equil_Rmajor(s,h,h,temp2,ier)
-               temp2 = (temp2 - Aminor)/(2*h) ! dA/drho
-               temp1 = (B(2)-B(1))/(2*h) ! dI/drho
-            ELSE IF (i == nrho) THEN ! 
-               rho = 1
-               s = rho*rho
-               ier = 0
-               CALL get_equil_Rmajor(s,h,h,Aminor,ier)
-               rho = THRIFT_RHO(nrho)
-               s = rho*rho
-               ier = 0
-               CALL get_equil_Rmajor(s,h,h,temp1,ier)
-               rho = THRIFT_RHO(nrho-1)
-               s = rho*rho
-               ier = 0
-               CALL get_equil_Rmajor(s,h,h,temp2,ier)
-               temp2 = (4*Aminor-3*temp1-temp2)/(3*h) ! dA/drho = (4*A(nrho+1)-3*A(nrho)-A(nrho-1))/3h
-               temp1 = B(nrho) ! Set current at nrho = nrho-1 for now
-               temp1 = (4*temp1-3*B(nrho)-B(nrho-1))/(3*h) 
-            ELSE
-               temp1 = (B(i+1)-B(i-1))/(2*h)
-               rho = THRIFT_RHO(i-1)
-               s = rho*rho
-               ier = 0
-               CALL get_equil_Rmajor(s,h,h,Aminor,ier)
-               rho = THRIFT_RHO(i+1)
-               s = rho*rho
-               ier = 0
-               CALL get_equil_Rmajor(s,h,h,temp2,ier)
-               temp2 = (temp2 - Aminor)/(2*h) ! dA/drho
-            END IF
-            THRIFT_JPLASMA(i,mytimestep) = temp1/temp2 ! dI/drho / dA/drho
-         END DO
-      END IF
-
       ! JPLASMA(i) = (IPLASMA(i)-IPLASMA(i-1))/(pi*(a(i)**2-a(i-1)**2))
       temp1 = 0; temp2 = 0;
       DO i = 1, nrho
@@ -504,11 +443,15 @@
          WRITE(6,*)' JINDUCTIVE DONE'
          WRITE(6,*)'==============================================================================='
       END IF    
+      DEALLOCATE(A_temp, B_temp, C_temp, D_temp, &
+               B_der, C_der, D_der, &
+               a1, a2, a3, a4, &
+               DU, D, DL, B)
+      RETURN
 
+1000  CONTINUE
       ! Calculate enclosed currents for progress
       temp1 = 0
-      THRIFT_I        = 0; THRIFT_IBOOT    = 0; THRIFT_IPLASMA  = 0
-      THRIFT_IECCD    = 0; THRIFT_INBCD    = 0; THRIFT_IOHMIC   = 0
       DO i = 1, nrho
          rho = THRIFT_RHO(i)
          s = rho * rho
@@ -525,135 +468,8 @@
       END DO
       THRIFT_I = THRIFT_IPLASMA + THRIFT_IBOOT + THRIFT_IECCD + THRIFT_INBCD + THRIFT_IOHMIC
 
-      DEALLOCATE(A_temp, B_temp, C_temp, D_temp, &
-               B_der, C_der, D_der, &
-               a1, a2, a3, a4, &
-               DU, D, DL, B)
-      RETURN
+
 !----------------------------------------------------------------------
 !     END SUBROUTINE
 !----------------------------------------------------------------------
       END SUBROUTINE thrift_jinductive
-
-       
-
-!---------------------------------------------
-!     OLD NON-FUNCTIONING CODE
-!---------------------------------------------------------
-      ! temp1 rho grid extended beyond boundaries (nrho+4)
-      ! To get on nrho+2 grid, shift index by +1
-      ! To get on nrho grid, shift index by +2
-      !rho_temp(1) = -THRIFT_RHO(1)
-      !rho_temp(2) = 0
-      !rho_temp(3:nrho+2) = THRIFT_RHO
-      !rho_temp(nrho+3) = 1
-      !rho_temp(nrho+4) = 2*rho_temp(nrho+3) - rho_temp(nrho+2)
-
-      ! MU0 I / PHIP   (nrho+4)
-      !WRITE(6,*) "  rho    s11    s12     iota     mu0 I/phip"
-      !WRITE(6,*) "------------------------------------------"
-
-      !I_temp(1) = 0 ! rho = -rho(1), will be overwritten
-      !I_temp(2) = 0 ! rho = 0
-      !WRITE(6,'(2X,F5.3,4(3X,E9.2,A)') rho_temp(2),I_temp(2),';'
-      !DO i = 1, nrho+1
-      !      rho = rho_temp(i+2)
-      !      s   = rho*rho
-      !      ier = 0
-      !      CALL get_equil_sus(s,s11,s12,Bav,Bsqav,ier) 
-      !      CALL EZspline_interp(iota_spl,rho,iota,ier)   
-      !      I_temp(i+2) = 2*rho*(s11*iota+s12) ! mu0 I/phip|(rho)
-      !      WRITE(6,'(2X,F5.3,4(3X,E9.2),A)') rho_temp(i+2),s11,s12,iota,I_temp(i+2),';'
-      !END DO
-      !I_temp(nrho+4)=I_temp(nrho+3) ! no current outside rho=1
-      !I_temp(1) = I_temp(3) ! rho = -rho(1)
-      !WRITE(6,'(2X,F5.3,3X,E8.1,A)') rho_temp(nrho+4),I_temp(nrho+4),';'
-      !WRITE(6,'(2X,F5.3,3X,E8.1,A)') rho_temp(1),I_temp(1),';'
-
-      ! D/DS(U)   (nrho+2)
-      !WRITE(6,*) ""
-      !WRITE(6,*) "  rho   1/(2rho)*d/drho(mu0 I/phip)"
-      !WRITE(6,*) "-----------------------------------"
-
-      !f2(1) = 0 ! rho = 0
-      !WRITE(6,'(2X,F5.3,6X,E8.1,A)') rho_temp(2),f2(1),';'
-      !DO i = 2, nrho+2
-      !   f2(i) = 0.5/rho_temp(i+1) * & ! 1/(2 rho)
-      !            0.5*(I_temp(i+2)-I_temp(i))/(rho_temp(i+2)-rho_temp(i)) ! du/drho
-      !   WRITE(6,'(2X,F5.3,6X,E8.1,A)') rho_temp(i+1),f2(i),';'
-      !END DO
-
-      ! Temporary Jsource grid with boundaries (nrho+2)
-      !j_temp(1)          = THRIFT_JSOURCE(1,mytimestep)
-      !j_temp(2:nrho+1)   = THRIFT_JSOURCE(:,mytimestep)
-      !j_temp(nrho+2)     = 0.0
-
-      !WRITE(6,*) ""
-      !WRITE(6,*) "  rho      f"
-      !WRITE(6,*) "---------------------"
-
-      ! F in DF/DX     (nrho+2)
-      !DO i = 1, nrho+2
-      !   rho = rho_temp(i+1)
-      !   s   = rho*rho
-      !   ier = 0
-      !   CALL get_prof_etapara(rho,THRIFT_T(mytimestep),etapara) !eta = eta
-      !   CALL EZspline_interp(vp_spl,rho,s11,ier)           !s11 = V'
-      !   CALL get_prof_pprime(rho,THRIFT_T(mytimestep),s12) !s12 = p'
-      !   CALL get_equil_Bav(s,Bav,Bsqav,ier)                !Bav = <B>; Bsqav = <B^2>
-
-         ! I on nrho+4 grid, f on nrho+2, so I_temp is shifted by +1
-      !   f2(i) = etapara*s11*(s12*I_temp(i+1)  &   ! eta*V'(p'u  
-      !                  + Bsqav/mu0*f2(i)       &   !     + <B^2>/mu0*du/dx 
-      !                  - j_temp(i)*Bav)            !         - <J.B>)
-      !   WRITE(6,'(F5.3,3X,E8.1,A)') rho, f2(i),';'
-
-      !END DO
-
-      ! I_temp (nrho+4), j_temp(nrho+2) freed up
-      !I_temp = 0; j_temp = 0;
-      
-      ! Construct du/dt (nrho)
-      !DO i = 1, nrho
-      !   rho = THRIFT_RHO(i)
-      !   s   = rho*rho
-      !   ier = 0
-      !   CALL get_equil_sus(s,s11,s12,Bav,Bsqav,ier) 
-      !   j_temp(i) = 0.5/rho * & ! 1/(2 rho)
-      !            0.5*(f2(i+2)-f2(i))/(rho_temp(i+3)-rho_temp(i+1)) ! df/drho
-      !   j_temp(i) = 0.5*phip/rho * & 
-      !            mu0*s11/(eq_phiedge**2)*j_temp(i) ! j_temp= dI/dt, assuming d/dt(phip)=0
-      !END DO
-
-      ! Need dj/dt though, not dI/dt
-      ! dj/dt = pi R/(rho V') d/drho(dI/dt)
-      ! Once again, need to evaluate d/drho using CD scheme (nrho+2)
-      ! BCs: dI/dt(0) = 0, dI/dt(rho) = dI/dt(prev rho)
-      !I_temp(1) = 0 
-      !I_temp(2:nrho) = j_temp
-      !I_temp(nrho+1) = I_temp(nrho)
-      ! Note that I_temp(nrho+3)=I_temp(nrho+4)=0 are never accessed
-      
-      ! j_temp freed up again
-      !j_temp = 0
-
-      ! Calculate dj/dt (nrho)
-      !DO i = 1, nrho
-      !   rho = THRIFT_RHO(i)
-      !   CALL EZspline_interp(vp_spl,rho,phip,ier)
-      !   j_temp(i) = 0.5/rho * & ! 1/(2 rho)
-      !               0.5*(I_temp(i+2)-I_temp(i))/(rho_temp(i+3)-rho_temp(i+1)) ! d/rho(dI/dt)
-      !   j_temp(i) = j_temp(i)*pi*eq_Rmajor/(rho*phip) ! dj/dt
-      !END DO     
-
-      ! update plasma current
-      ! s11 = dt
-      !IF (mytimestep==1) THEN 
-      !   s11 = THRIFT_T(2)-THRIFT_T(1)
-      !   THRIFT_JPLASMA(:,mytimestep) = j_temp*s11
-      !ELSE 
-      !   s11 = THRIFT_T(mytimestep)-THRIFT_T(mytimestep-1)
-      !   THRIFT_JPLASMA(:,mytimestep) = THRIFT_JPLASMA(:,mytimestep-1)+j_temp*s11
-      !END IF
-
-      ! Deallocate
