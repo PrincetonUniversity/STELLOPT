c337232fd9da92fdba872b8c8faae234763ceb07
Lucas van Ham <lucas.vanham@gmail.com>
2023-03-01 22:07:58 +0100

    THRIFT: Add printer for simple du/dt

--- THRIFT/Sources/thrift_jinductive.f90
+++ THRIFT/Sources/thrift_jinductive.f90
@@ -22,7 +22,7 @@
 !-----------------------------------------------------------------------
       IMPLICIT NONE
       INTEGER :: i,itime, ier
-      REAL(rprec) :: rho,s,h,k,t,s11,s12,etapara,pprime,&
+      REAL(rprec) :: rho,s,drho,dt,t,s11,s12,etapara,pprime,&
                      temp1,temp2,source_axis,source_edge,Aminor,Rmajor
       REAL(rprec), DIMENSION(:), ALLOCATABLE :: A_temp,B_temp,C_temp,D_temp,&
                                                 B_der, C_der, D_der, &
@@ -32,7 +32,7 @@
 !     BEGIN SUBROUTINE
 !----------------------------------------------------------------------
 
-      ! If at zero beta, copy previous value of JPLASMA
+      ! If at zero beta, copy previous value of JPLASMA onto this timestep
       IF (eq_beta == 0) THEN
          IF (mytimestep /= 1) THRIFT_JPLASMA(:,mytimestep) = THRIFT_JPLASMA(:,mytimestep-1)
          GOTO 1000 
@@ -64,68 +64,73 @@
       THRIFT_ISOURCE(:,mytimestep) = THRIFT_IBOOT(:,mytimestep)+THRIFT_IECCD(:,mytimestep)&
          +THRIFT_INBCD(:,mytimestep)+THRIFT_IOHMIC(:,mytimestep)
 
-      ! If first timestep, declare JPLASMA = JSOURCE (total current zero)
+      ! If mytimestep = 1 & tstart = 0, ITOT=0 and continue to next iteration
+      ! If mytimestep = 1 & tstart > 0, ITOT=0 and calculate change in IPLASMA between tstart&t=0
       IF (mytimestep==1) THEN
          THRIFT_JPLASMA(:,mytimestep)=-THRIFT_JSOURCE(:,mytimestep) 
          CALL curden_to_curtot(THRIFT_JPLASMA,THRIFT_AMINOR,THRIFT_IPLASMA,mytimestep)
-         IF (tstart==0) GOTO 1000 ! If t=0 as well, go to next iteration
+         IF (tstart==0) GOTO 1000
       END IF
 
-      ! k = Delta t
+      ! t = current simulation time
+      ! dt = delta t between this and previous iteration (on first iter, dt=tstart)
+      ! itime = previous timestep (on first iter, itime=mytimestep)
+      t = THRIFT_T(mytimestep) ! t = current sim time
       IF (mytimestep==1) THEN
          itime = mytimestep
-         k = tstart
+         dt = tstart
       ELSE
          itime = mytimestep-1
-         k = THRIFT_T(mytimestep)-THRIFT_T(itime)
+         dt = THRIFT_T(mytimestep)-THRIFT_T(itime)
       END IF
-      t = THRIFT_T(mytimestep) ! t = current sim time
-      IF (t>tmax.and.THRIFT_T(mytimestep-1)<=tmax) WRITE(6,*) &
+      IF (t>tmax.and.THRIFT_T(mytimestep-1)<=tmax.and.(nsubsteps==1)) WRITE(6,*) &
          '! THRIFT has exceeded end time of profiles file. Proceeding with profiles at t=tmax !' 
 
       ! Allocations
       ALLOCATE(A_temp(nrho+2), B_temp(nrho+2), C_temp(nrho+2), D_temp(nrho+2), &
-               B_der(nrho), C_der(nrho), D_der(nrho), &
-               a1(nrho), a2(nrho), a3(nrho), a4(nrho), &
+               B_der(nrho),    C_der(nrho),    D_der(nrho), &
+               a1(nrho  ), a2(nrho), a3(nrho  ), a4(nrho), &
                AI(nrho-1), BI(nrho), CI(nrho-1), DI(nrho))
 
       A_temp = 0; B_temp = 0; C_temp = 0; D_temp = 0;
-      B_der = 0; C_der = 0; D_der = 0;
-      a1 = 0; a2 = 0; a3 = 0; a4 = 0;
-      AI = 0; BI = 0; CI = 0; DI = 0;
+      B_der  = 0; C_der  = 0; D_der  = 0;
+      a1     = 0; a2     = 0; a3     = 0; a4     = 0;
+      AI     = 0; BI     = 0; CI     = 0; DI     = 0;
 
-      ! Populate A,B,C,D
+      ! Calculate ABCD (values at **current** timestep)
       IF (lverbj) THEN
          WRITE(6,*)'==============================================================================='
          WRITE(6,*)' CALCULATING COEFFICIENTS A,B,C,D'
          WRITE(6,*) ' RHO  ETAPARA     DV/DPHI      DP/DRHO     <J.B>      BSQAV        S11'
       END IF
-      ! A,B,C,D should be evaluated at the current timestep
+
       ! Extrapolate source current density to magnetic axis and edge
       source_axis = (3*THRIFT_JSOURCE(1,mytimestep)-THRIFT_JSOURCE(2,mytimestep))/2
       source_edge = (-THRIFT_JSOURCE(nrho-1,mytimestep)+3*THRIFT_JSOURCE(nrho,mytimestep))/2
-      !source_edge = THRIFT_JSOURCE(nrho,itime)
 
       DO i = 1, nrho+2
          ! calculate rho, etapara, current
          IF (i==1) THEN 
             rho = 0
-            CALL get_prof_etapara(rho,t,etapara)
+            !CALL get_prof_etapara(rho,t,etapara)
             temp2 = source_axis
-            CALL get_prof_pprime(rho,t,pprime)
+            !CALL get_prof_pprime(rho,t,pprime)
          ELSE IF (i==nrho+2) THEN ! etapara breaks when rho=1
             rho = 1
-            CALL get_prof_etapara(THRIFT_RHO(nrho),t,etapara)
+            !CALL get_prof_etapara(THRIFT_RHO(nrho),t,etapara)
             temp2 = source_edge
-            CALL get_prof_pprime(THRIFT_RHO(nrho),t,pprime)
+            !CALL get_prof_pprime(THRIFT_RHO(nrho),t,pprime)
          ELSE
             rho = THRIFT_RHO(i-1)
-            CALL get_prof_etapara(rho,t,etapara)
+            !CALL get_prof_etapara(rho,t,etapara)
             temp2 = THRIFT_JSOURCE(i-1,mytimestep)
-            CALL get_prof_pprime(rho,t,pprime)
+            !CALL get_prof_pprime(rho,t,pprime)
          END IF
+         CALL get_prof_etapara(MIN(rho,THRIFT_RHO(nrho)),t,etapara)
+         CALL get_prof_pprime(rho,t,pprime)
          temp1 = 2*etapara*THRIFT_VP(i,2) ! temp1 <- 2 eta dV/dPhi 
-         IF (i /= 1) A_temp(i) = THRIFT_S11(i,2)/(4*rho*THRIFT_PHIEDGE(2)**2) ! S11/(4 rho phi_a^2)
+         IF (i /= 1) &
+         A_temp(i) = THRIFT_S11(i,2)/(4*rho*THRIFT_PHIEDGE(2)**2) ! S11/(4 rho phi_a^2)
          B_temp(i) = temp1*THRIFT_BSQAV(i,2)/mu0! 2 eta dV/dPhi <B^2>/mu_0
          C_temp(i) = temp1*pprime               ! 2 eta dV/dPhi dp/drho
          D_temp(i) = -temp1*temp2*THRIFT_BAV(i,2)   ! -2 eta dV/dPhi <J.B>
@@ -133,11 +138,12 @@
          rho, etapara, THRIFT_VP(i,2), pprime, temp2*THRIFT_BAV(i,2), THRIFT_BSQAV(i,2), THRIFT_S11(i,2)
       END DO
 
+      ! drho = grid step (assuming constant spacing)
+      drho = THRIFT_RHO(2)-THRIFT_RHO(1) 
       ! Calculate derivatives of ABCD
-      h = THRIFT_RHO(2)-THRIFT_RHO(1) ! h = Delta rho
-      CALL deriv1_rho_o2(B_temp, h, B_der)
-      CALL deriv1_rho_o2(C_temp, h, C_der)
-      CALL deriv1_rho_o2(D_temp, h, D_der)
+      CALL deriv1_rho_o2(B_temp, drho, B_der)
+      CALL deriv1_rho_o2(C_temp, drho, C_der)
+      CALL deriv1_rho_o2(D_temp, drho, D_der)
 
       IF (lverbj) THEN
          WRITE(6,*)'==============================================================================='
@@ -179,35 +185,50 @@
       t = THRIFT_T(itime)
       CALL get_prof_etapara(THRIFT_RHO(nrho),t,etapara) 
       temp2 = temp1*THRIFT_AMINOR(nrho+2,1)**2/(2*etapara*THRIFT_RMAJOR(nrho+2,1)) ! temp2 <- tau_L/R
+      IF (nsubsteps==1.and.(mytimestep==1.or.(mytimestep==2.and.tstart==0))) &
+      WRITE(6,'(A25,F8.6)') 'Estimated tau_L/R    ',temp2
+      WRITE(6,'')
+      
+
       ! Decay I_plasma at edge
-      THRIFT_IPLASMA(nrho,mytimestep) = THRIFT_IPLASMA(nrho,itime)*exp(-k/temp2)
+      THRIFT_IPLASMA(nrho,mytimestep) = THRIFT_IPLASMA(nrho,itime)*exp(-dt/temp2)
       ! I_total at edge
       temp1 = THRIFT_IPLASMA(nrho,mytimestep)+THRIFT_ISOURCE(nrho,mytimestep)
+      WRITE(6,*)'==============================================================================='
+      WRITE(6,*) 'SIMPLE FTCS RATE OF CHANGE'
+      WRITE(6,*) ''
+      WRITE(6,'(ES13.5)') 0.0
+      DO i = 1,nrho
+         WRITE(6,'(ES13.5)') a1(i)+a2(i)*THRIFT_UGRID(i,1)+a3(i)*(THRIFT_UGRID(i+1,1)-THRIFT_UGRID(i-1,1))/(2*drho)&
+         +a4(i)*(THRIFT_UGRID(i+1,1)-2*THRIFT_UGRID(i,1)+THRIFT_UGRID(i-1,1))/(drho**2)
+      END DO
+      WRITE(6,'(ES13.5)') mu0/(2*THRIFT_PHIEDGE(2))*(THRIFT_IPLASMA(nrho,mytimestep)-THRIFT_IPLASMA(nrho,itime))
 
        !! Calculate uedge for this timestep
       !t = THRIFT_T(itime) ! t = previous sim time (or current sim time if mytimestep=1)
       !CALL get_prof_etapara(THRIFT_RHO(INT(nrho/2)),t,etapara)
       !CALL get_prof_etapara(THRIFT_RHO(nrho),t,etapara)
       !CALL get_prof_pprime(THRIFT_RHO(nrho),t,pprime) 
-      !temp2 = (-8*THRIFT_UEDGE(1)+9*THRIFT_UGRID(nrho,1)-THRIFT_UGRID(nrho-1,1))/(3*h) ! du/drho at edge
+      !temp2 = (-8*THRIFT_UEDGE(1)+9*THRIFT_UGRID(nrho,1)-THRIFT_UGRID(nrho-1,1))/(3*drho) ! du/drho at edge
       ! uedge [ current timestep ] =  u - dt*mu0/(2*Phi_a)*eta/Lext*dV/dphi* ...
       ! [(p' + <B^2>/mu0)*u + <B^2>/mu0*du/drho - <J.B>]  [ edge, preceding timestep ]
-      !THRIFT_UEDGE(2) = THRIFT_UEDGE(1)-k*mu0/(2*THRIFT_PHIEDGE(1))*etapara/temp1*THRIFT_VP(nrho+2,1) * &  
+      !THRIFT_UEDGE(2) = THRIFT_UEDGE(1)-dt*mu0/(2*THRIFT_PHIEDGE(1))*etapara/temp1*THRIFT_VP(nrho+2,1) * &  
       !      (((pprime + THRIFT_BSQAV(nrho+2,1)/mu0)*THRIFT_UEDGE(1)) + THRIFT_BSQAV(nrho+2,1)/mu0*temp2 &               
       !      - THRIFT_JSOURCE(nrho,itime)*THRIFT_BAV(nrho+2,1))   
 
 
 
-      ! Populate of tridiagonal matrix and RHS; AI,CI of size nrho-1, BI,DI of size nrho
-      ! Enclosed current at magnetic axis = 0 always
+      ! Populate tridiagonal matrix and RHS
+      ! BC1: Enclosed current at magnetic axis = 0 always
       BI(1) = 1; CI(1) = 0; DI(1) = 0   
-      ! Enclosed current at edge must equal temp1 next timestep
+      ! BC2: Enclosed current at edge must equal temp1 next timestep
       BI(nrho) = 1; AI(nrho-1) = 0; DI (nrho) = mu0*temp1/(2*THRIFT_RHO(nrho)*THRIFT_PHIEDGE(2)) 
       DO i = 2, nrho-1 ! elsewhere
-         CI(i) = a3(i)/(2*h) +a4(i)/(h**2)   
-         IF (i/=nrho-1) AI(i) = -a3(i)/(2*h)+a4(i)/(h**2)   
-         BI(i) = a2(i)-2*a4(i)/(h**2)-1/k    
-         DI(i) = -THRIFT_UGRID(i,1)/k-a1(i)  
+         IF (i/=nrho-1) & 
+         AI(i) = -a3(i)/(2*drho)+a4(i)/(drho**2)  
+         BI(i) = a2(i)-2*a4(i)/(drho**2)-1/dt     
+         CI(i) = a3(i)/(2*drho) +a4(i)/(drho**2)   
+         DI(i) = -THRIFT_UGRID(i,1)/dt-a1(i)  
       END DO
       ! Solve system of equations
       CALL solve_tdm(AI,BI,CI,DI,THRIFT_UGRID(:,2))
