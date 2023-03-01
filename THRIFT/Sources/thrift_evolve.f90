ca262b1589fed5fe7a693b046c127fb14f9c4eb7
Lucas van Ham <lucas.vanham@gmail.com>
2023-03-01 11:16:39 +0100

    THRIFT: Change print statement

--- THRIFT/Sources/thrift_evolve.f90
+++ THRIFT/Sources/thrift_evolve.f90
@@ -132,7 +132,8 @@
             IF (lverb) THEN
              WRITE(6,'(1X,ES9.1E1,1X,I2,1X,F5.2,5(1X,ES11.3E1))') &
                 THRIFT_T(mytimestep),nsubsteps,eq_beta*100,&
-                THRIFT_I, THRIFT_IPLASMA, THRIFT_IBOOT, THRIFT_IECCD, THRIFT_INBCD!,&
+                THRIFT_I(nrho,mytimestep), THRIFT_IPLASMA(nhro,mytimestep), THRIFT_IBOOT(nrho,mytimestep),&
+                THRIFT_IECCD(nrho,mytimestep), THRIFT_INBCD(nrho,mytimestep)!,&
                 !THRIFT_IOHMIC,&
                 !MAXVAL(deltaj)
             END IF
