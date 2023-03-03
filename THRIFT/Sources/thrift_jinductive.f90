42a1ac056445b36398109a4020272a0cbcb1af6f
Lucas van Ham <lucas.vanham@gmail.com>
2023-03-03 16:26:42 +0100

    THRIFT: Doing things differently again

--- THRIFT/Sources/thrift_jinductive.f90
+++ THRIFT/Sources/thrift_jinductive.f90
@@ -307,15 +307,18 @@
 
       ! ITOTAL = phip*u/mu0 = 2*phi_a*rho*u/mu0
       THRIFT_I(:,mytimestep) = 2*THRIFT_PHIEDGE(2)/mu0*(rho_full*THRIFT_UGRID(:,2))
-      ! JTOTAL
-      CALL curtot_to_curden(THRIFT_I(:,mytimestep),THRIFT_AMINOR,j_full)
+      THRIFT_IPLASMA(:,mytimestep) = THRIFT_I(:,mytimestep)-THRIFT_ISOURCE(:,mytimestep)
+      CALL curtot_to_curden(THRIFT_IPLASMA(:,mytimestep),THRIFT_AMINOR,jplasma_full)
+      THRIFT_JPLASMA = jplasma_full(2:nrho+1)
+      !! JTOTAL
+      !CALL curtot_to_curden(THRIFT_I(:,mytimestep),THRIFT_AMINOR,j_full)
       !THRIFT_J(:,mytimestep) = j_full(2:nrho+1)
       ! JPLASMA = JTOTAL - JSOURCE
-      jplasma_full = j_full - jsource_full
+      !jplasma_full = j_full - jsource_full
       ! Subtract change in JSOURCE
-      CALL extrapolate_arr(THRIFT_JSOURCE(:,prevtimestep), jsourceprev_full)
-      jplasma_full = jplasma_full - (jsource_full-jsourceprev_full)
-      THRIFT_JPLASMA(:,mytimestep) = jplasma_full(2:nrho+1)
+      !CALL extrapolate_arr(THRIFT_JSOURCE(:,prevtimestep), jsourceprev_full)
+      !jplasma_full = jplasma_full - (jsource_full-jsourceprev_full)
+      !THRIFT_JPLASMA(:,mytimestep) = jplasma_full(2:nrho+1)
       
       IF (lverbj) THEN
          WRITE(6,*) '==============================================================================='
