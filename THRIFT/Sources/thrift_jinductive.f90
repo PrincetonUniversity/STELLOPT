e3a66b5ba15507b3522f47d1f6c2855086074293
Lucas van Ham <lucas.vanham@gmail.com>
2023-03-03 15:14:25 +0100

    THRIFT: Typo

--- THRIFT/Sources/thrift_jinductive.f90
+++ THRIFT/Sources/thrift_jinductive.f90
@@ -319,7 +319,7 @@
       jplasma_full = j_full - jsource_full
       ! Subtract change in JSOURCE
       CALL extrapolate_arr(THRIFT_JSOURCE(:,prevtimestep), jsourceprev_full)
-      jplasma_full = j_plasmafull - (jsource_full-jsourceprev_full)
+      jplasma_full = jplasma_full - (jsource_full-jsourceprev_full)
       THRIFT_JPLASMA(:,mytimestep) = jplasma_full(2:nrho+1)
       
       IF (lverbj) THEN
