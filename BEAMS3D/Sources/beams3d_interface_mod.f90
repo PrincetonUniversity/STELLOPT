23457b26101a0f299350dc22660cc01a07b616ae
lazersos <samuel.lazerson@ipp.mpg.de>
2023-03-29 15:40:11 +0200

    BEAMS3D: Fixed git thing

--- BEAMS3D/Sources/beams3d_interface_mod.f90
+++ BEAMS3D/Sources/beams3d_interface_mod.f90
@@ -371,9 +371,22 @@ CONTAINS
       END SUBROUTINE beams3d_init_commandline
 
       SUBROUTINE beams3d_output_header
-      USE git_version_mod
       IMPLICIT NONE
       INTEGER :: nshar
+
+#if defined(GIT_VERSION_EXT)
+      CHARACTER(64), PARAMETER :: git_repository = GIT_REPO_EXT
+      CHARACTER(32), PARAMETER :: git_version = GIT_VERSION_EXT
+      CHARACTER(40), PARAMETER :: git_hash = GIT_HASH_EXT
+      CHARACTER(32), PARAMETER :: git_branch = GIT_BRANCH_EXT
+      CHARACTER(19), PARAMETER :: built_on = BUILT_ON_EXT
+#else
+      CHARACTER(64), PARAMETER :: git_repository = "not from a git repo"
+      CHARACTER(32), PARAMETER :: git_version = ""
+      CHARACTER(40), PARAMETER :: git_hash = ""
+      CHARACTER(32), PARAMETER :: git_branch = ""
+      CHARACTER(19), PARAMETER :: built_on = ""
+#endif
 #if defined(MPI_OPT)
       CALL MPI_COMM_SIZE(MPI_COMM_SHARMEM, nshar, ierr_mpi) ! MPI
 #endif
