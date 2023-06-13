b40c1a75fdf56884b0a4c484bf01f36e4ff12492
lazersos <samuel.lazerson@ipp.mpg.de>
2023-06-13 13:49:28 +0200

    FIELDLINES: Furtehr improvements to gridgen

--- FIELDLINES/Sources/fieldlines_gridgen.f90
+++ FIELDLINES/Sources/fieldlines_gridgen.f90
@@ -24,9 +24,10 @@
 !          iunit          File ID Number
 !-----------------------------------------------------------------------
       IMPLICIT NONE
+      LOGICAL :: lfound
       INTEGER :: i, j, k, i1, i2, j2, k2, l, l1, l2, n1, ier, mystart
       REAL(rprec) :: r1, r2, r0, z0, z1, z2, rinner, zinner, phiend_temp,&
-                     phi0, ra, za, phia, dr, dz
+                     phi0, ra, za, phia, re, ze, phie, dr, dz
       LOGICAL, DIMENSION(:,:,:), POINTER :: lgoodline
       INTEGER :: win_lgoodline
       INTEGER :: MPI_COMM_LOCAL
@@ -42,7 +43,6 @@
          r0 = ra
          z0 = za
          phi0 = phia
-         !WRITE(6,'(A,3(F8.5,A))')     '   AXIS_GUESS[R,PHI,Z] = [',ra,',',phia,',',za,'];'
          CALL fieldlines_find_axis(r0,z0,phi0)
          WRITE(6,'(A,3(F8.5,A))') '         AXIS[R,PHI,Z] = [',r0,',',phi0,',',z0,']'
          ra = r0; za = z0; phia = phi0
@@ -55,33 +55,23 @@
       IF (myid_sharmem == master) lgoodline = .FALSE.
 #endif
 
-      ! Now do a grid search
+      ! Search the outboard midplane
       IF (myworkid == master) THEN
          r_start = -1; z_start = -1; phi_start = 0; phi_end = 0;
          n1 = 1
-      !  Do just Rmajor at Zaxis
-         DO i = 2, nr-1
-            r_start(n1) = raxis(i)
-            z_start(n1) = za
+      !  Do just Rmajor from magnetic axis
+         dr = (raxis(nr-1) - ra) / DBLE(nr-1)
+         dz = (0.0         - za) / DBLE(nr-1)
+         DO i = 2,nr
+            r_start(n1) = ra + dr * DBLE(i-1)/DBLE(nr-1)
+            z_start(n1) = za + dz * DBLE(i-1)/DBLE(nr-1)
             phi_start(n1)   = phiaxis(1)
             phi_end(n1)     = phiaxis(nphi)*phi_factor
             n1 = n1 + 1
          END DO
-      !  Do the entire PHI=0 Plane
-      !   DO i = 2, nr-1
-      !      DO j = 2, nz-1
-      !         r_start(n1)     = raxis(i)
-      !         z_start(n1)     = zaxis(j)
-      !         phi_start(n1)   = phiaxis(1)
-      !         phi_end(n1)     = phiaxis(1)+10*pi2
-      !         n1 = n1 + 1
-      !      END DO
-      !   END DO
          nlines = n1 - 1
-         WRITE(6,'(A)') '===========PHI = 0 Grid=========='
+         WRITE(6,'(A)') '===========Initial Raxis to Edge Grid=========='
       END IF
-      dr = (raxis(nr)-raxis(1))/DBLE(nr-1)
-      dz = (zaxis(nz)-zaxis(1))/DBLE(nz-1)
       npoinc = nphi - 1 ! so that steps are in phiaxis
 #if defined(MPI_OPT)
       CALL MPI_BARRIER(MPI_COMM_FIELDLINES,ierr_mpi)
@@ -105,11 +95,35 @@
       IF (ierr_mpi /= MPI_SUCCESS) CALL handle_err(MPI_BARRIER_ERR,'fieldlines_gridgen',ierr_mpi)
 #endif
 
-      ! Now find lines still in the grid
+      ! Search for outter gridpoint
       mystart = LBOUND(R_lines,1)
       myend   = UBOUND(R_lines,1)
       l1      = LBOUND(R_lines,2)
       l2      = UBOUND(R_lines,2)
+      i   = myend
+      DO WHILE (i >= mystart)
+         lfound = .FALSE.
+         j   = 1
+         DO WHILE (j < l2)
+            IF ((R_lines(i,j) == R_lines(i,j+1)) .and.    &
+               (Z_lines(i,j) == Z_lines(i,j+1))) lfound = .TRUE.
+            IF (lfound) EXIT
+            j = j + 1
+         END DO
+         IF (.not.lfound) EXIT
+         i = i - 1
+      END DO
+      IF (i < mystart) i = 0 ! Do this so MPI_ALLREDUCE WORKS
+      i2 = i + 1
+      IF (i2 > nlines) i2 = nlines
+      CALL MPI_ALLREDUCE(MPI_IN_PLACE,i2,1,MPI_INTEGER,MPI_MAX,MPI_COMM_FIELDLINES,ierr_mpi)
+      re = r_start(i2)
+      ze = z_start(i2)
+      phie = phi_start(i2)
+
+      ! Now find lines still in the grid
+      dr = (raxis(nr)-raxis(1))/DBLE(nr-1)
+      dz = (zaxis(nz)-zaxis(1))/DBLE(nz-1)
       DO i = mystart,myend
          IF ((R_lines(i,l2-1) < raxis(nr)) .AND. (R_lines(i,l2-1) > raxis(1)) .AND. &
             (Z_lines(i,l2-1) < zaxis(nz)) .AND. (Z_lines(i,l2-1) > zaxis(1))) THEN
@@ -149,6 +163,8 @@
       ! Now do a new search for missing lines
       IF (myworkid == master) THEN
          WRITE(6,'(A)') '=========== Creating Grid =========='
+         WRITE(6,'(A,3(F8.5,A))') '         AXIS[R,PHI,Z] = [',ra,',',phi0,',',za,']'
+         WRITE(6,'(A,3(F8.5,A))') '         EDGE[R,PHI,Z] = [',re,',',phie,',',ze,']'
          ! Axis
          r_start(1) = ra
          z_start(1) = za
@@ -156,24 +172,14 @@
          n1 = 2
          ! PHI = 0
          j = COUNT(zaxis >= za)
-         DO i = 1, nr
-            IF (.not.lgoodline(i,1,j)) CYCLE
-            r_start(n1) = raxis(i)
-            z_start(n1) = za
+         dr = (re - ra) / DBLE(nr-1)
+         dz = (ze - za) / DBLE(nr-1)
+         DO i = 2,nr
+            r_start(n1) = ra + dr * DBLE(i-1)/DBLE(nr-1)
+            z_start(n1) = za + dz * DBLE(i-1)/DBLE(nr-1)
             phi_start(n1)   = phiaxis(1)
             n1 = n1 + 1
          END DO
-         ! Old way
-         !DO l = 1, nr*nz
-         !   i = MOD(l-1,nr)+1
-         !   j = MOD(l-1,nr*nz)
-         !   j = FLOOR(REAL(j) / REAL(nr))+1
-         !   IF (.not.lgoodline(i,1,j)) CYCLE
-         !   r_start(n1)     = raxis(i)
-         !   z_start(n1)     = zaxis(j)
-         !   phi_start(n1)   = phiaxis(1)
-         !   n1 = n1 + 1
-         !END DO
          ! Now find missing points
          DO l = 1, nr*nphi*nz
             i = MOD(l-1,nr)+1
@@ -192,7 +198,7 @@
             IF (lgoodline(i+1, j, k-1)) i2 = i2+1
             IF (lgoodline(i+1, j, k  )) i2 = i2+1
             IF (lgoodline(i+1, j, k+1)) i2 = i2+1
-            IF (i2 > 8) THEN
+            IF (i2 > 6) THEN
                r_start(n1)     = raxis(i)
                phi_start(n1)   = phiaxis(j)
                z_start(n1)     = zaxis(k)
