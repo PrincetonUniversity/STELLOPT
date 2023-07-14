cc4621a32a46425e2bd96fc997b15f912c95da07
kudav <david.kulla@ipp.mpg.de>
2023-07-14 09:12:27 +0200

    LIBSTELL: removed pies (unused+compilation issues)

--- LIBSTELL/Sources/Modules/read_pies_mod.f90
+++ LIBSTELL/Sources/Modules/read_pies_mod.f90
@@ -7,47 +7,47 @@
 !                    module initializes the varialbes in the
 !                    read_wout_mod VMEC output interface module.
 !-----------------------------------------------------------------------
-      MODULE read_pies_mod
+MODULE read_pies_mod
 !-----------------------------------------------------------------------
 !     Libraries
 !-----------------------------------------------------------------------
-      USE read_wout_mod, ONLY: nfp, ns, mpol,ntor, mnmax, rmax_surf, &
-                               rmin_surf, zmax_surf, Rmajor, Itor, &
-                               rmnc, rmns, zmnc, zmns, lmnc, lmns, &
-                               bsupumnc, bsupumns, bsupvmnc, bsupvmns, &
-                               iotaf, presf, phi, xm, xn, lasym, &
-                               lthreed, input_extension, rprec
+   USE read_wout_mod, ONLY: nfp, ns, mpol,ntor, mnmax, rmax_surf, &
+      rmin_surf, zmax_surf, Rmajor, Itor, &
+      rmnc, rmns, zmnc, zmns, lmnc, lmns, &
+      bsupumnc, bsupumns, bsupvmnc, bsupvmns, &
+      iotaf, presf, phi, xm, xn, lasym, &
+      lthreed, input_extension, rprec
 !-----------------------------------------------------------------------
 !     Local Variables
 !          ier            Error Flag
 !          iunit          File ID Number
 !-----------------------------------------------------------------------
-      IMPLICIT NONE
-      INTEGER :: ier, i, j, k
-      REAL(rprec), ALLOCATABLE :: bsupsmns(:,:)
+   IMPLICIT NONE
+   INTEGER :: ier, i, j, k
+   REAL(rprec), ALLOCATABLE :: bsupsmns(:,:)
 !-----------------------------------------------------------------------
 !     Subroutines
 !          read_spec_file     Read the SPEC output file.
 !-----------------------------------------------------------------------
-      
-      INTERFACE read_pies_file
-          MODULE PROCEDURE read_pies_hdf5
-      END INTERFACE
-      
-      CONTAINS
-      
-      SUBROUTINE read_pies_hdf5(id_string)
+
+   INTERFACE read_pies_file
+      MODULE PROCEDURE read_pies_hdf5
+   END INTERFACE
+
+CONTAINS
+
+   SUBROUTINE read_pies_hdf5(id_string)
 !DEC$ IF DEFINED (NETCDF)
       include 'netcdf.inc'
 !DEC$ ENDIF
       CHARACTER(LEN=*), INTENT(in) :: id_string
-      INTEGER :: i,m,n,mn,ierr,i_alloc,ncid,status,& 
-     &           BMID, BNID, BKID, BLID, &
-     &           XMID, XNID, XKID, XLID, &
-     &           XID, RELBUPID, NPERID,RMAJID,BXBYID,&
-     &           BXMID, BXNID, BXKID, BXLID,&
-     &           IOTAID,PSIID,&
-     &           mpol,ntor,k,n2, dex
+      INTEGER :: i,m,n,mn,ierr,i_alloc,ncid,status,&
+      &           BMID, BNID, BKID, BLID, &
+      &           XMID, XNID, XKID, XLID, &
+      &           XID, RELBUPID, NPERID,RMAJID,BXBYID,&
+      &           BXMID, BXNID, BXKID, BXLID,&
+      &           IOTAID,PSIID,&
+      &           mpol,ntor,k,n2, dex
       REAL(rprec) :: temp
       REAL(rprec), DIMENSION(:,:,:,:), ALLOCATABLE :: relbup,x,bxby
 !-----------------------------------------------------------------------
@@ -59,18 +59,18 @@
       dex = INDEX(id_string,'nc',BACK=.TRUE.)
       IF (dex < LEN(id_string)) THEN
          input_extension(1:dex-2) = id_string(1:dex-2)
-      ELSE 
+      ELSE
          input_extension = TRIM(id_string)
       END IF
       ! Default some stuff
       lasym = .FALSE.
       ! Open SPEC file
       status=nf_open(trim(id_string),0,ncid)
-	   if (status .ne. NF_NOERR) then
-	      write(*,*) '*****Error Opening netCDF file*****'
-		   write(*,*) '     Filename=',trim(id_string)
-		   write(*,*) '     Status=',NF_STRERROR(status)
-		   stop
+      if (status .ne. NF_NOERR) then
+         write(*,*) '*****Error Opening netCDF file*****'
+         write(*,*) '     Filename=',trim(id_string)
+         write(*,*) '     Status=',NF_STRERROR(status)
+         stop
       endif
       status=nf_inq_dimid(ncid,'dim_x_1',XMID)
       IF (STATUS .NE. NF_NOERR) CALL HANDLE_ERR(STATUS)
@@ -122,7 +122,7 @@
       i_alloc=0
       ALLOCATE(iotaf(k),phi(k))
       ALLOCATE(relbup(mpol,ntor,k,3),x(mpol,ntor,k,2),&
-     &         bxby(mpol,ntor,k,2))
+      &         bxby(mpol,ntor,k,2))
       x=0; relbup=0;
 !     Get the Variables
       status=nf_get_var_int(ncid,NPERID,nfp)
@@ -140,19 +140,19 @@
       status=nf_get_var_double(ncid,BXBYID,bxby)
       IF (STATUS .NE. NF_NOERR) CALL HANDLE_ERR(STATUS)
 !     Close the netCDF File
-	   status=nf_close(ncid)
-	   if (status .ne. NF_NOERR) then
-	      write(*,*) '*****Error Closing the netCDF file*****'
-		   write(*,*) '     Filename=',trim(id_string)
-		   write(*,*) '     Status=',NF_STRERROR(status)
-		   stop
+      status=nf_close(ncid)
+      if (status .ne. NF_NOERR) then
+         write(*,*) '*****Error Closing the netCDF file*****'
+         write(*,*) '     Filename=',trim(id_string)
+         write(*,*) '     Status=',NF_STRERROR(status)
+         stop
       endif
       ! Define some dimensional quantities
       mnmax = (2*ntor+1)*(mpol + 1)
       ns = k ! Note k is the dimension length not the varaible k
       ALLOCATE(xm(mnmax),xn(mnmax),phi(ns),iotaf(ns))
       ALLOCATE(rmnc(mnmax,ns),zmns(mnmax,ns),&
-               bsupsmns(mnmax,ns),bsupumnc(mnmax,ns),bsupvmnc(mnmax,ns))
+         bsupsmns(mnmax,ns),bsupumnc(mnmax,ns),bsupvmnc(mnmax,ns))
       mn = 1
       DO m = 0, mpol
          DO n = -ntor, ntor
@@ -174,10 +174,10 @@
 !DEC$ ENDIF
 !-----------------------------------------------------------------------
 !     End Subroutine
-!-----------------------------------------------------------------------    
-      END SUBROUTINE read_pies_hdf5
+!-----------------------------------------------------------------------
+   END SUBROUTINE read_pies_hdf5
 
-      SUBROUTINE HANDLE_ERR(STATUS)
+   SUBROUTINE HANDLE_ERR(STATUS)
 !***********************************************************************
 !  This subroutine handles netCDF errors.  Lifted straight from:
 !        https://www.unidata.ucar.edu/software/netcdf/docs/netcdf-f77/NF_005fSTRERROR.html#NF_005fSTRERROR
@@ -192,12 +192,12 @@
       INTEGER STATUS
 !DEC$ IF DEFINED (NETCDF)
       IF (STATUS .NE. NF_NOERR) THEN
-        PRINT *, NF_STRERROR(STATUS)
-        STOP '!!!!!!DIAGNO v2.0!!!!!'
+         PRINT *, NF_STRERROR(STATUS)
+         STOP '!!!!!!DIAGNO v2.0!!!!!'
       ENDIF
 !DEC$ ENDIF
-      END SUBROUTINE HANDLE_ERR
+   END SUBROUTINE HANDLE_ERR
 !-----------------------------------------------------------------------
 !     End Module
 !-----------------------------------------------------------------------
-      END MODULE read_pies_mod
+END MODULE read_pies_mod
