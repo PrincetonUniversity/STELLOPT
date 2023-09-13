c3add2e3cf1734d60654f42874710c37acd13f23
Björn Hamstra <b.c.hamstra@student.tue.nl>
2023-09-13 18:12:21 +0200

    Added first implementation of magnetic field calculation

--- LIBSTELL/Sources/Modules/mumaterial_mod.f90
+++ LIBSTELL/Sources/Modules/mumaterial_mod.f90
@@ -1,7 +1,7 @@
 !-----------------------------------------------------------------------
 !     Module:        mumaterial_mod
-!     Authors:       S. Lazerson (lazerson@pppl.gov)
-!     Date:          January 2022
+!     Authors:       S. Lazerson (lazerson@pppl.gov), Björn Hamstra
+!     Date:          September 2023
 !     Description:   This module is designed to help calculate the
 !                    magnetic field arrising from ferromagnetic
 !                    material
@@ -11,6 +11,8 @@
 !     Libraries
 !-----------------------------------------------------------------------
       USE safe_open_mod
+      USE TileNComponents
+      USE IterateMagnetSolution
       IMPLICIT NONE
 
 !-----------------------------------------------------------------------
@@ -22,6 +24,11 @@
 !           nvertex:          Number of vertices
 !           ntet:             Number of tetrahedrons
 !           nstate:           Number of state functions
+!           tiles:            Magnetic tiles
+!           stateFunction:    Array of state functions
+!           maxErr:           Max allowed error for convergence in MagTense iteration
+!           maxIter           Max allowed number of MagTense iterations
+!           temp:             Temperature of magnetic material [K]
 !           vertex:           Vertices [m] (nvertex,3)
 !           tet:              Tetrahedron (ntet,4) index into vertex
 !           state_dex:        State function for each tetrahedron
@@ -31,7 +38,11 @@
 !           M:                Magnet M Vector for hard magnet (nstate,3)
 !
 !-----------------------------------------------------------------------
-      INTEGER, PRIVATE            :: nvertex, ntet, nstate
+      INTEGER, PRIVATE                    :: nvertex, ntet, nstate
+      TYPE(MagTile), PRIVATE, ALLOCATABLE              :: tiles(:)
+      TYPE(MagStateFunction), PRIVATE, ALLOCATABLE     :: stateFunction(:)
+      DOUBLE PRECISION, PRIVATE           :: maxErr, temp
+      INTEGER, PRIVATE                    :: maxIter
 
 
       DOUBLE PRECISION, POINTER, PRIVATE :: vertex(:,:)
@@ -39,7 +50,7 @@
       INTEGER, POINTER, PRIVATE :: state_dex(:)
       INTEGER, POINTER, PRIVATE :: state_type(:)
       DOUBLE PRECISION, POINTER, PRIVATE :: constant_mu(:)
-      DOUBLE PRECISION, POINTER, PRIVATE :: M(:,:)
+      DOUBLE PRECISION, POINTER, PRIVATE :: Mag(:,:)
       INTEGER, PRIVATE            :: win_vertex, win_tet,  &
                                      win_state_dex, win_state_type, &
                                      win_constant_mu, win_m
@@ -51,17 +62,47 @@
       INTEGER, PRIVATE                    :: mystart, myend, mydelta
       INTEGER, PRIVATE                    :: shar_rank, shar_size, shar_comm
 
+
+
+
       
 !-----------------------------------------------------------------------
 !     Subroutines
+!         muMaterial_setd: Overrides default values
 !         mumaterial_load: Loads a magnetic material file
-!         mumaterial_init: Calculates mangetic moment of material
+!         mumaterial_init: Calculates magnetization of material
 !         mumaterial_getb: Calculates magnetic field at a point in space
 !-----------------------------------------------------------------------
 !     Functions
 !-----------------------------------------------------------------------
       CONTAINS
       
+
+
+
+
+      SUBROUTINE mumaterial_setd(mE, mI, T)
+      !-----------------------------------------------------------------------
+      ! mumaterial_setd: Overrides default values
+      !-----------------------------------------------------------------------
+      ! param[in]: mE. New maxErr: max error for MagTense convergence
+      ! param[in]: mI. New maxIter: max amount of MagTense iterations
+      ! param[in]: T. New temp: temperature of magnetic material in MagTense
+      !-----------------------------------------------------------------------
+      DOUBLE PRECISION, INTENT(in) :: mE, T
+      INTEGER, INTENT(in) :: mI
+
+      maxErr = mE
+      maxIter = mI
+      temp = T
+
+      RETURN
+      END SUBROUTINE mumaterial_setd
+
+
+
+
+
       SUBROUTINE mumaterial_load(filename,istat,comm)
       !-----------------------------------------------------------------------
       ! mumaterial_load: Loads magnetic material file.
@@ -111,13 +152,13 @@
          CALL mpialloc_1d_int(state_dex,ntet,shar_rank,0,shar_comm,win_state_dex)
          CALL mpialloc_1d_int(state_type,nstate,shar_rank,0,shar_comm,win_state_type)
          CALL mpialloc_1d_dbl(constant_mu,nstate,shar_rank,0,shar_comm,win_constant_mu)
-         CALL mpialloc_2d_dbl(M,nstate,3,shar_rank,0,shar_comm,win_vertex)
+         CALL mpialloc_2d_dbl(Mag,nstate,3,shar_rank,0,shar_comm,win_vertex)
          shared = .true.
       ELSE
 #endif
          ! if no MPI, allocate everything on one node
          ALLOCATE(vertex(nvertex,3),tet(ntet,4),state_dex(ntet), &
-                  state_type(nstate),constant_mu(nstate),M(nstate,3),STAT=istat)
+                  state_type(nstate),constant_mu(nstate),Mag(nstate,3),STAT=istat)
          shared = .false.
 #if defined(MPI_OPT)
       END IF
@@ -137,7 +178,7 @@
          DO ik = 1, nstate
             READ(iunit,*) state_type(ik)
             IF (state_type(ik) == 1) THEN
-               READ(iunit,*) M(ik,1),M(ik,2),M(ik,3)
+               READ(iunit,*) constant_mu(ik)!Mag(ik,1),Mag(ik,2),Mag(ik,3) ! todo
             ELSEIF (state_type(ik) == 2) THEN
                PRINT *, 'STATE_TYPE == 2 (soft magnet) not yet supported.'
             ELSEIF (state_type(ik) == 3) THEN
@@ -154,8 +195,19 @@
       ! close file
       CLOSE(iunit)
 
+
+
+      ! set default values
+      call MUMATERIAL_SETD(1.0d-5, 100, 300.d0)
+
+
+
       RETURN
       END SUBROUTINE mumaterial_load
+
+
+
+
 
       SUBROUTINE mumaterial_info(iunit)
       IMPLICIT NONE
@@ -167,11 +219,15 @@
       WRITE(iunit,*) 'NSTATE: ',nstate
       END SUBROUTINE mumaterial_info
       
+
+
+
+
       SUBROUTINE mumaterial_init(getBfld, comm)
       !-----------------------------------------------------------------------
-      ! mumaterial_load: Loads magnetic material file.
+      ! mumaterial_init: Calculates magnetization of material
       !-----------------------------------------------------------------------
-      ! fcn           : Function which returns the vacuum magnetic field
+      ! fcn           : getBfld. Function which returns the vacuum magnetic field
       !                 SUBROUTINE FCN(x,y,z,bx,by,bz)
       ! param[in, out]: comm. MPI communicator, handles shared memory
       !-----------------------------------------------------------------------
@@ -181,11 +237,167 @@
       IMPLICIT NONE
       INTEGER, INTENT(inout), OPTIONAL :: comm
       EXTERNAL:: getBfld
+      INTEGER :: ik
+      DOUBLE PRECISION :: mu0, norm, x, y, z, Bx, By, Bz, Bx_n, By_n, Bz_n
+      
+      mu0 = 16 * atan(1.d0) * 1.d-7
+      
+      allocate(tiles(ntet))
+
+      DO ik = 1, ntet
+            ! set vertices
+            ! each tetrahedron is a tile
+            ! tet(ik,1) contains the relevant vertex index for tile vertex 1
+            ! vertex(index,:) returns the relevant coordinates
+            tiles(ik)%vert(:,1) = vertex(tet(ik,1),:)
+            tiles(ik)%vert(:,2) = vertex(tet(ik,2),:)
+            tiles(ik)%vert(:,3) = vertex(tet(ik,3),:)
+            tiles(ik)%vert(:,4) = vertex(tet(ik,4),:)
+
+            ! tetrahedron tile
+            tiles(ik)%tileType = 5
+            tiles(ik)%includeInIteration = 1
+
+            ! hard magnet or soft magnet with state function or with constant permeability
+            tiles(ik)%magnetType = state_type(state_dex(ik))
+
+            IF (tiles(ik)%magnetType == 1) THEN
+               ! tiles(ik)%M = Mag(ik,:) ! todo does this need to be set at all? 
+               !tiles(ik)%mu_r_ea = 1 ! todo it seems this needs to be set, maybe better to load this regardless of type
+               !tiles(ik)%mu_r_oa = 1
+               tiles(ik)%mu_r_ea = constant_mu(state_dex(ik))
+               tiles(ik)%mu_r_oa = constant_mu(state_dex(ik))
+            ELSEIF (tiles(ik)%magnetType == 2) THEN
+               PRINT *, 'STATE_TYPE == 2 (soft magnet) not yet supported.'
+               ! tiles(ik)%stateFunctionIndex = state_dex(ik) ! todo something different from state_dex I assume?
+               tiles(ik)%mu_r_ea = 1
+               tiles(ik)%mu_r_oa = 1
+            ELSEIF (tiles(ik)%magnetType == 3) THEN
+               tiles(ik)%mu_r_ea = constant_mu(state_dex(ik))
+               tiles(ik)%mu_r_oa = constant_mu(state_dex(ik))
+            END IF
+
+            ! tiles(ik)%stateFunctionIndex = state_dex(ik) ! todo only set when using state function?
 
 
+            ! determine centroid
+            x = (tiles(ik)%vert(1,1) + tiles(ik)%vert(1,2) + tiles(ik)%vert(1,3) + tiles(ik)%vert(1,4))/4
+            y = (tiles(ik)%vert(2,1) + tiles(ik)%vert(2,2) + tiles(ik)%vert(2,3) + tiles(ik)%vert(2,4))/4
+            z = (tiles(ik)%vert(3,1) + tiles(ik)%vert(3,2) + tiles(ik)%vert(3,3) + tiles(ik)%vert(3,4))/4
+            
+            ! get B-field and determine strength
+            call getBfld(x, y, z, Bx, By, Bz)
+            norm = sqrt(Bx*Bx + By*By + Bz*Bz)
+            Bx_n = Bx / norm
+            By_n = By / norm
+            Bz_n = Bz / norm
+            
+            ! set remanent magnetization to strength of the field [A/m] and easy axis in the direction of the field
+            tiles(ik)%Mrem = norm / mu0
+            tiles(ik)%u_ea = [Bx_n, By_n, Bz_n]
+            
+            ! get orthogonal axes
+            IF (By/=0 .or. Bz/=0) THEN          ! cross product of u_ea with [1, 0, 0] and cross product of u_ea with cross product
+                  tiles(ik)%u_oa1 = [0.d0, Bz_n, -By_n]
+                  tiles(ik)%u_oa2 = [-By_n*By_n - Bz_n*Bz_n, Bx_n*By_n, Bx_n*Bz_n]
+            ELSE                                ! cross product of u_ea with [0, 1, 0] and cross product of u_ea with cross product
+                  tiles(ik)%u_oa1 = [-Bz_n, 0.d0, Bx_n]
+                  tiles(ik)%u_oa2 = [Bx_n*By_n, -Bx_n*Bx_n - Bz_n*Bz_n, By_n*Bz_n]
+            END IF
+
+            tiles(ik)%M = tiles(ik)%Mrem * tiles(ik)%u_ea ! todo not sure if necessary, is done in python
+
+            ! call setupEvaluationPoints(tiles(ik)) ! only for cylindrical tiles
+      END DO
+
+      
+      
+      allocate(stateFunction(1)) ! todo temporary until statefunction is properly implemented
+      call loadStateFunctionFortran(stateFunction)
+
+      call iterateMagnetization(tiles, ntet, stateFunction, size(stateFunction), temp, maxErr, maxIter, 0.d0) ! todo replace size?
 
       RETURN
       END SUBROUTINE mumaterial_init
+
+
+      subroutine loadStateFunctionFortran(stateFunction) ! todo temp
+            integer :: i
+            type(MagStateFunction), dimension(1), intent(out) :: stateFunction
+            open(11, file='stateFunction.dat')
+            read(11,*) stateFunction(1)%nT,stateFunction(1)%nH
+                
+            allocate( stateFunction(1)%M(stateFunction(1)%nT,stateFunction(1)%nH) )
+            allocate( stateFunction(1)%T(stateFunction(1)%nT) )
+            allocate( stateFunction(1)%H(stateFunction(1)%nH) )
+            
+            stateFunction(1)%T(1) = 300
+            do i=1,stateFunction(1)%nH
+                read(11,*) stateFunction(1)%H(i),stateFunction(1)%M(1,i)        
+            enddo
+            
+            close (11)
+
+    end subroutine loadStateFunctionFortran
+
+
+      SUBROUTINE mumaterial_getb(x, y, z, Bx, By, Bz)
+      !-----------------------------------------------------------------------
+      ! mumaterial_getb: Calculates magnetic field at a point in space
+      !-----------------------------------------------------------------------
+      ! param[in]: x. x-coordinate of point where to get the B-field
+      ! param[in]: y. y-coordinate of point where to get the B-field
+      ! param[in]: z. z-coordinate of point where to get the B-field
+      ! param[out]: Bx. x-component of B-field at this point [T]
+      ! param[out]: By. y-component of B-field at this point [T]
+      ! param[out]: Bz. z-component of B-field at this point [T]
+      !-----------------------------------------------------------------------
+      DOUBLE PRECISION, INTENT(in) :: x, y, z
+      DOUBLE PRECISION, INTENT(out) :: Bx, By, Bz
+      DOUBLE PRECISION :: H(1,3), point(1,3), mu0
+
+      mu0 = 16 * atan(1.d0) * 1.d-7
+
+      point(1,:) = [x, y, z]
+
+      call getFieldFromTiles(tiles, H, point, ntet, 1)
+
+      Bx = H(1,1) * mu0
+      By = H(1,2) * mu0
+      Bz = H(1,3) * mu0
+
+      RETURN
+      END SUBROUTINE mumaterial_getb
+
+
+
+
+
+      SUBROUTINE mumaterial_getb_grid(points, n_points, B)
+      !-----------------------------------------------------------------------
+      ! mumaterial_getb_grid: Calculates magnetic field at multiple points in space
+      !-----------------------------------------------------------------------
+      ! param[in]: points. Points at which to determine the magnetic field
+      ! param[in]: n_points. Number of points
+      ! param[out]: B. B-field at required points [T]
+      !-----------------------------------------------------------------------
+      DOUBLE PRECISION, INTENT(in) :: points(n_points,3)
+      INTEGER, INTENT(in) :: n_points
+      DOUBLE PRECISION, INTENT(out), ALLOCATABLE :: B(:,:)
+      DOUBLE PRECISION :: mu0
+
+      allocate(B(n_points,3)) ! todo assuming this needs to be allocated here and not in main program
+
+      mu0 = 16 * atan(1.d0) * 1.d-7
+
+      call getFieldFromTiles(tiles, B, points, ntet, n_points)
+
+      B = B * mu0
+
+      RETURN
+      END SUBROUTINE mumaterial_getb_grid
+
+
 
 
 
