37eb7508445bf1145123c273155d5da2771c98f3
lazersos <lazersos@gmail.com>
2024-05-28 14:03:42 +0200

    LIBSTELL: Created fieldlines_globals.f90 for interfacing fieldlines_input_mod.f90

---
+++ LIBSTELL/Sources/Modules/fieldlines_globals.f90
@@ -0,0 +1,47 @@
+!-----------------------------------------------------------------------
+!     Module:        fieldlines_globals
+!     Authors:       S. Lazerson (samuel.lazerson@gauss-fusion.com)
+!     Date:          05/28/2024
+!     Description:   This module contains the FIELDLINES global 
+!                    variables needed by the input namelist.
+!-----------------------------------------------------------------------
+      MODULE fieldlines_globals
+!-----------------------------------------------------------------------
+!     Libraries
+!-----------------------------------------------------------------------
+      USE stel_kinds, ONLY: rprec
+      
+!-----------------------------------------------------------------------
+!     Module Variables
+!-----------------------------------------------------------------------
+      IMPLICIT NONE
+
+      ! From fiellines_runtime
+      INTEGER, PARAMETER ::  MAXLINES   = 2**19
+      LOGICAL :: lerror_field
+      INTEGER :: npoinc
+      REAL(rprec) :: dphi, follow_tol, num_hcp, delta_hc, mu
+      REAL(rprec), DIMENSION(20)           :: errorfield_amp, errorfield_phase
+      REAL(rprec), DIMENSION(MAXLINES)     :: r_start, phi_start, &
+                                              z_start, phi_end, &
+                                              r_hc, z_hc, phi_hc
+      CHARACTER(256) :: int_type
+
+
+      ! From fieldlines_lines
+      INTEGER :: nlines
+      ! From fieldlines_grid
+      INTEGER :: nr, nphi, nz
+      REAL(rprec) :: rmin, rmax, zmin, zmax, phimin, phimax, &
+                     vc_adapt_tol
+
+
+      CONTAINS
+
+      ! These expose the global variables through ctypes
+      INTEGER FUNCTION getmaxlines()
+      IMPLICIT NONE
+      getmaxlines = MAXLINES
+      END FUNCTION getmaxlines
+
+      END MODULE fieldlines_globals
