4d3fb0aa7bf3f6b4cfb0eb15fc3a807198b5175e
lazersos <lazersos@gmail.com>
2024-05-31 12:18:21 +0200

    STELLOPT/LIBSTELL: Moved stellopt_input_mod to LIBSTELL.

---
+++ LIBSTELL/Sources/STELLOPT/stellopt_globals.f90
@@ -0,0 +1,26 @@
+!-----------------------------------------------------------------------
+!     Module:        stellopt_globals
+!     Authors:       S. Lazerson (samuel.lazerson@gauss-fusion.com)
+!     Date:          05/31/2024
+!     Description:   This module contains the STELLOPT global variables
+!                    needed by the input namelist.
+!-----------------------------------------------------------------------
+      MODULE stellopt_globals
+!-----------------------------------------------------------------------
+!     Libraries
+!-----------------------------------------------------------------------
+      USE stel_kinds, ONLY: rprec
+      
+!-----------------------------------------------------------------------
+!     Module Variables
+!-----------------------------------------------------------------------
+      IMPLICIT NONE
+      ! Moved from stellopt_runtime
+      INTEGER, PARAMETER :: maxwindsurf=32  
+      REAL(rprec), PARAMETER :: bigno = 1.0E+10
+      LOGICAL :: lcentered_differences, lkeep_mins, lrefit, lcoil_geom, lno_restart, ltriangulate
+      INTEGER :: cr_strategy, npopulation, noptimizers, mode, rho_exp
+      REAL(rprec)  :: ftol, xtol, gtol, epsfcn, factor, refit_param
+      CHARACTER(256)           :: opt_type, axis_init_option
+
+      END MODULE stellopt_globals
