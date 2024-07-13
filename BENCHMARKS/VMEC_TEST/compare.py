a760d6bac15ae43ab252d2acc8cc89b4cc9838bb
lazersos <lazersos@gmail.com>
2024-07-13 21:08:55 +0200

    BENCHMARKS: VMEC converted from shelve to JSON.

--- BENCHMARKS/VMEC_TEST/compare.py
+++ BENCHMARKS/VMEC_TEST/compare.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import sys, os
-import shelve
+#import shelve
+import json
 from pathlib import Path
 from argparse import ArgumentParser
 sys.path.insert(0, '../../pySTEL/')
 import numpy as np
-_BENCH_FILE_ = 'BENCH_DATA'
+_BENCH_FILE_ = 'BENCH_DATA.json'
 # This part changes
 from libstell.vmec import VMEC
 
@@ -37,28 +38,46 @@ if __name__=="__main__":
     data['wp']=vmec.wp
     data['betatot']=vmec.betatot
     data['volume']=vmec.volume
-    data['presf']=vmec.presf.flatten()
-    data['iotaf']=vmec.iotaf.flatten()
-    data['jcurv']=vmec.jcurv.flatten()
-    data['rmnc']=vmec.rmnc[round(vmec.ns/2),:].flatten()
-    data['zmns']=vmec.zmns[round(vmec.ns/2),:].flatten()
-    data['lmns']=vmec.lmns[round(vmec.ns/2),:].flatten()
-    data['bmnc']=vmec.bmnc[round(vmec.ns/2),:].flatten()
+    data['presf']=vmec.presf.flatten().tolist()
+    data['iotaf']=vmec.iotaf.flatten().tolist()
+    data['jcurv']=vmec.jcurv.flatten().tolist()
+    data['rmnc']=vmec.rmnc[round(vmec.ns/2),:].flatten().tolist()
+    data['zmns']=vmec.zmns[round(vmec.ns/2),:].flatten().tolist()
+    data['lmns']=vmec.lmns[round(vmec.ns/2),:].flatten().tolist()
+    data['bmnc']=vmec.bmnc[round(vmec.ns/2),:].flatten().tolist()
 
     # Read or write to the database file.
     if args.lmake_db:
+        data_out = {}
         my_file = Path(_BENCH_FILE_)
         if my_file.exists():
-            shelf = shelve.open(_BENCH_FILE_, flag="a")
-        else:
-            shelf = shelve.open(_BENCH_FILE_, flag="c")
-        shelf[run_name] = data
-        shelf.close()
+            f = open(_BENCH_FILE_,"r")
+            data_out = json.load(f)
+            f.close()
+        f = open(_BENCH_FILE_,"w")
+        data_out[run_name] = data
+        json.dump(data_out, f, ensure_ascii=False, indent=4)
+        f.close()
         print('  ADDED: '+run_name)
         sys.exit(0)
     else:
-        shelf = shelve.open(_BENCH_FILE_, flag='r')
-        varlist = shelf[run_name]
+        f = open(_BENCH_FILE_,"r")
+        d = json.load(f)
+        varlist = d[run_name]
+        f.close()
+#    if args.lmake_db:
+#        my_file = Path(_BENCH_FILE_)
+#        if my_file.exists():
+#            shelf = shelve.open(_BENCH_FILE_, flag="a")
+#        else:
+#            shelf = shelve.open(_BENCH_FILE_, flag="c")
+#        shelf[run_name] = data
+#        shelf.close()
+#        print('  ADDED: '+run_name)
+#        sys.exit(0)
+#    else:
+#        shelf = shelve.open(_BENCH_FILE_, flag='r')
+#        varlist = shelf[run_name]
 
     print(f'VMEC VERSION: {vmec.version_:4.2f}')
     for temp in varlist:
@@ -68,12 +87,12 @@ if __name__=="__main__":
             perct = 100*abs(act-cal)/act
             print(f'  {temp} {cal:7.6f} {act:7.6f} {round(perct)}')
         else:
-            cal = np.where(act==0,0,cal)
-            div = np.where(act==0,1,act)
-            perct = 100*sum(abs(act-cal)/div)
+            cal[act==0] = 0.0
+            div = act
+            div[div==0] = 1.0
             print(f'  Quantity: {temp} -- CODE -- REF. -- %')
             for i in range(len(act)):
-                perct = 100*abs(act[i]-cal[i])/div[i]
+                perct = 100.0*abs(act[i]-cal[i])/div[i]
                 print(f'  {i} {cal[i]:7.6f} {act[i]:7.6f} {round(perct)}')
         if perct > failtol:
             lfail = True
