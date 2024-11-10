19ce4c5cc38c975e74af166a9cd4a608e2fd1253
lazersos <lazersos@gmail.com>
2024-11-10 14:44:12 +0100

    STELLOPT: Fixed bug in QUASIISO lh calculation.

--- STELLOPTV2/Sources/Chisq/chisq_quasiiso.f90
+++ STELLOPTV2/Sources/Chisq/chisq_quasiiso.f90
@@ -102,6 +102,7 @@
                !lh = FLOOR(nalpha*0.5)+1
                !modb = CSHIFT(modb,lmin-lh) !BOOSH
                lh = MINLOC(modb,DIM=1)
+               lh = MIN(MAX(lh,1),nalpha)
                ! Squash the array
                modbs = modb
                DO l = lh, 1, -1
