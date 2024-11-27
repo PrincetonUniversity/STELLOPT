0ef558b1e250bd5057718db2e580fb640ab32504
lazersos <lazersos@gmail.com>
2024-11-27 18:29:10 +0100

    THRIFT: VMEC restarting now implemented.

--- THRIFT/Sources/thrift_run_equil.f90
+++ THRIFT/Sources/thrift_run_equil.f90
@@ -19,12 +19,16 @@
 !-----------------------------------------------------------------------
       IMPLICIT NONE
       INTEGER :: ier
+      CHARACTER(256), PARAMETER :: reset_string = 'reset_file'
 !----------------------------------------------------------------------
 !     BEGIN SUBROUTINE
 !----------------------------------------------------------------------
 
       IF (lvmec) THEN
+         ! Run VMEC
          CALL thrift_paraexe('paravmec_run',proc_string,lscreen_subcodes)
+         ! Write out the reset file
+         CALL thrift_paraexe('paravmec_write',reset_string,.FALSE.)
          ! Read the VMEC output
          CALL read_wout_deallocate; ier = 0
          CALL read_wout_file(TRIM(proc_string),ier)
