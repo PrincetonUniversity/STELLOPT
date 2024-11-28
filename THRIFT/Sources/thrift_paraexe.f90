74fd830c5bfc4bde6d2c595c5a9511d4f08ec486
lazersos <lazersos@gmail.com>
2024-11-28 08:27:23 +0100

    THRIFT: Fix so reset not used on first iteration even if wout file exists.

--- THRIFT/Sources/thrift_paraexe.f90
+++ THRIFT/Sources/thrift_paraexe.f90
@@ -128,7 +128,9 @@
                   reset_string =''
                   lhit = .FALSE.
                   INQUIRE(FILE='wout_reset_file.nc',EXIST=lhit)
-                  IF (lhit) reset_string ='wout_reset_file.nc'
+                  IF (lhit .and. .not. lscreen_local) reset_string ='wout_reset_file.nc'
+                  CALL MPI_BCAST(reset_string,128,MPI_CHARACTER,master,MPI_COMM_MYWORLD,ierr_mpi)
+                  IF (ierr_mpi /= MPI_SUCCESS) CALL handle_err(MPI_ERR,'thrift_paraexe: BCAST:reset_string',ierr_mpi)
                   NS_RESLTN = 0 ! Need to do this otherwise situations arrise which cause problems.
                   CALL runvmec(ictrl,file_str,lscreen_local,MPI_COMM_MYWORLD,reset_string)
                   CALL FinalizeSurfaceComm(NS_COMM)
