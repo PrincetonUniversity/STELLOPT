d81d76e21d52e309300905a4e8221c19da86f723
lazersos <lazersos@gmail.com>
2024-11-28 08:26:35 +0100

    VMEC2000: Changes to reset to align with ORNL-fusion/PARVMEC fix.

--- VMEC2000/Sources/Initialization_Cleanup/load_xc_from_wout.f
+++ VMEC2000/Sources/Initialization_Cleanup/load_xc_from_wout.f
@@ -8,8 +8,8 @@
       USE vmec_dim, ONLY: mpol1
       USE vparams, ONLY: one, zero, rprec
       USE vmec_input, ONLY: lasym
-      USE vmec_main, ONLY: lthreed, p5 => cp5, phipf, hs
-      USE parallel_include_module, ONLY: rank
+      USE vmec_main, ONLY: lthreed, p5 => cp5, sp, sm, phipf
+      USE parallel_include_module, ONLY: rank, t1lglob, t1rglob, PARVMEC
       IMPLICIT NONE
 C-----------------------------------------------
 C   D u m m y   A r g u m e n t s
@@ -24,8 +24,9 @@ C   L o c a l   V a r i a b l e s
 C-----------------------------------------------
       INTEGER :: ierr, mn, m, n, n1, js
       REAL(rprec) :: t1, t2
-      REAL(rprec), ALLOCATABLE :: sm_local(:),sp_local(:)
       REAL(rprec), ALLOCATABLE :: temp(:,:)
+      INTEGER :: nsmin
+      INTEGER :: nsmax
 C-----------------------------------------------
 
 !
@@ -35,7 +36,6 @@ C-----------------------------------------------
 !
 !     SPH 12-13-11: allow for paths in wout file name (as per Ed Lazarus request)
       CALL read_wout_file (reset_file, ierr)
-!      CALL read_wout_file (reset_file(5:), ierr)
       reset_file = " "               !nullify so this routine will not be recalled with present reset_file
 
       IF (ierr .ne. 0.AND.rank.EQ.0) THEN
@@ -51,6 +51,14 @@ C-----------------------------------------------
       IF (ntor_in  .ne. ntor ) STOP 'ntor_in != ntor in load_xc'
       IF (mpol1_in .ne. mpol1) STOP 'mpol1_in != mpol1 in load_xc'
       IF (nfp .eq. 0) STOP 'nfp = 0 in load_xc'
+
+      IF (PARVMEC) THEN
+         nsmin = t1lglob
+         nsmax = t1rglob
+      ELSE
+         nsmin = 1
+         nsmax = ns
+      END IF
 
       lreset = .false.               !Signals profil3d NOT to overwrite axis values
 
@@ -120,44 +128,31 @@ C-----------------------------------------------
 !     CONVERT lambda TO INTERNAL FULL MESH REPRESENTATION
 !
 !     START ITERATION AT JS=1
-!     SM AND SP ARRAYS NEED TO BE MADE LOCALY SO THEY EXIST OVER THE 
-!     WHOLE DOMAIN
 !
-      ALLOCATE(sm_local(ns),sp_local(0:ns)) ! See allocate_ns
-
-      DO js = 2, ns
-         t1 = hs*ABS(js-1.5_rprec)
-         t2 = hs*ABS(js-2.5_rprec)
-         sm_local(js) = SQRT(t1)/SQRT(hs*ABS(js-1))
-         sp_local(js) = SQRT(t2)/SQRT(hs*ABS(js-1))
-      END DO
-
-      sm_local(1) = zero
-      sp_local(0) = zero
-      sp_local(1) = sm_local(2)
 
       lmn(1,:,0,:) = lmn(2,:,0,:)
-      lmn(1,:,1,:) = 2*lmn(2,:,1,:)/(sm_local(2) + sp_local(1))
+      IF (nsmin .eq. 1) THEN
+         lmn(1,:,1,:) = 2*lmn(2,:,1,:)/(sm(2) + sp(1))
+      END IF
       lmn(1,:,2:,:) = 0
       
       DO m = 0, mpol1, 2
-         DO js = 2, ns
+         DO js = nsmin + 1, nsmax
             lmn(js,:,m,:) = 2*lmn(js,:,m,:) - lmn(js-1,:,m,:)
          END DO
       END DO
 
       DO m = 1, mpol1, 2
-         DO js = 2, ns
+         DO js = nsmin + 1, nsmax
             lmn(js,:,m,:) = (2*lmn(js,:,m,:) 
-     1                    - sp_local(js-1)*lmn(js-1,:,m,:))/sm_local(js)
+     1                    - sp(js-1)*lmn(js-1,:,m,:))/sm(js)
          END DO
       END DO
 
-      DO js = 2, ns
+      DO js = nsmin + 1, nsmax
          lmn(js,:,:,:) = phipf(js)*lmn(js,:,:,:)
       END DO
 
-      DEALLOCATE(sm_local,sp_local)
 
       CALL read_wout_deallocate
 
