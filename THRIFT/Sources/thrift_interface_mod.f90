0ded9c434d5b8ddce098ff953f887cb24b2d659e
ajchcoelho <antonio.coelho8@gmail.com>
2024-11-29 10:32:27 +0100

    THRIFT: Make some file cleaning when running with Docker

--- THRIFT/Sources/thrift_interface_mod.f90
+++ THRIFT/Sources/thrift_interface_mod.f90
@@ -89,12 +89,18 @@ MODULE THRIFT_INTERFACE_MOD
          CALL EXECUTE_COMMAND_LINE("rm -rf dcon* jxbout* mercier* dkesout* fort.*",WAIT=.TRUE.,EXITSTAT=ier,CMDSTAT=ierr_mpi,CMDMSG=cmdtxt)
          WRITE(6,*) ' rm: EXITSTAT=',ier,' CMDSTAT=',ierr_mpi; CALL FLUSH(6)
          WRITE(6,*) '     MESSAGE: ',TRIM(cmdtxt); CALL FLUSH(6)
-         ! Zip up the results
+!DEC$ IF DEFINED (STELZIP)
+         ! Zip up the results and clean
+         CALL EXECUTE_COMMAND_LINE("rm -rf dcon* jxbout* mercier* dkesout* fort.* wout* temp_input*",WAIT=.TRUE.,EXITSTAT=ier,CMDSTAT=ierr_mpi,CMDMSG=cmdtxt)
          WRITE(6,*) ' Zipping files'; CALL FLUSH(6); ier = 0; ierr_mpi = 0; cmdtxt=''
-         CALL EXECUTE_COMMAND_LINE("zip -r thrift_files.zip *",WAIT=.TRUE.,EXITSTAT=ier,CMDSTAT=ierr_mpi,CMDMSG=cmdtxt)
+         CALL EXECUTE_COMMAND_LINE("zip -r thrift_files.zip thrift_*.h5",WAIT=.TRUE.,EXITSTAT=ier,CMDSTAT=ierr_mpi,CMDMSG=cmdtxt)
          WRITE(6,*) ' zip: EXITSTAT=',ier,' CMDSTAT=',ierr_mpi; CALL FLUSH(6)
          WRITE(6,*) '     MESSAGE: ',TRIM(cmdtxt); CALL FLUSH(6)
+         CALL EXECUTE_COMMAND_LINE("rm -rf wout* temp_input*",WAIT=.TRUE.,EXITSTAT=ier,CMDSTAT=ierr_mpi,CMDMSG=cmdtxt)
+         WRITE(6,*) ' rm: EXITSTAT=',ier,' CMDSTAT=',ierr_mpi; CALL FLUSH(6)
+         WRITE(6,*) '     MESSAGE: ',TRIM(cmdtxt); CALL FLUSH(6)
          ier = 0; ierr_mpi=0
+!DEC$ ENDIF
       END IF
 
 
