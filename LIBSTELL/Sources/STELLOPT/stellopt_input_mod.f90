e522f756cd404722ff9d0bded40d1ec0c7ac202d
lazersos <lazersos@gmail.com>
2024-12-11 09:01:55 +0100

    STELLOPT: Added 2D Rosenbrock function for testing.

--- LIBSTELL/Sources/STELLOPT/stellopt_input_mod.f90
+++ LIBSTELL/Sources/STELLOPT/stellopt_input_mod.f90
@@ -411,7 +411,8 @@
                          target_gamma_c, sigma_gamma_c, &
                          lRosenbrock_X_opt, dRosenbrock_X_opt, &
                          Rosenbrock_X, Rosenbrock_X_min, Rosenbrock_X_max, &
-                         target_Rosenbrock_F, sigma_Rosenbrock_F
+                         target_Rosenbrock_F, sigma_Rosenbrock_F, &
+                         target_Rosenbrock2D, sigma_Rosenbrock2D
        
 !-----------------------------------------------------------------------
 !     Subroutines
@@ -549,6 +550,8 @@
       Rosenbrock_X_max(1:ROSENBROCK_DIM)  = bigno
       target_Rosenbrock_F(1:ROSENBROCK_DIM) = 0
       sigma_Rosenbrock_F(1:ROSENBROCK_DIM)  = bigno
+      target_Rosenbrock2D = 0.0
+      sigma_Rosenbrock2D = bigno
 
       IF (.not.ltriangulate) THEN  ! This is done because values may be set by trinagulate
          phiedge_min     = -bigno;  phiedge_max     = bigno
@@ -2585,6 +2588,13 @@
             END DO
          END DO
       END IF
+      WRITE(iunit,'(A)') '!----------------------------------------------------------------------'
+      WRITE(iunit,'(A)') '!         Rosenbrock 2D TEST FUNCTION' 
+      WRITE(iunit,'(A)') '!----------------------------------------------------------------------'
+      IF (sigma_Rosenbrock2D < bigno) THEN
+         WRITE(iunit,outflt) 'TARGET_ROSENBROCK2D',target_Rosenbrock2D
+         WRITE(iunit,outflt) 'sigma_ROSENBROCK2D',sigma_Rosenbrock2D
+      END IF 
       WRITE(iunit,'(A)') '/'
 
       RETURN
