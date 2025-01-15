94a4da4bba77ff570ca4e9d92759454b7408e2e4
David Kulla <david.kulla@ipp.mpg.de>
2025-01-15 15:11:45 +0100

    BEAMS3D: fix for thermalization in coll. calc

--- BEAMS3D/Sources/beams3d_physics_mod.f90
+++ BEAMS3D/Sources/beams3d_physics_mod.f90
@@ -284,7 +284,7 @@ MODULE beams3d_physics_mod
             CALL R8HERM3FCN(ict,1,1,fval,i,j,k,xparam,yparam,zparam,&
                             hr(i),hri(i),hp(j),hpi(j),hz(k),hzi(k),&
                             MODB4D(1,1,1,1),nr,nphi,nz)
-            modb = fval(1)
+            modb = max(fval(1),zero)
             binv = one/modb
             CALL R8HERM3FCN(ict,1,1,fval,i,j,k,xparam,yparam,zparam,&
                             hr(i),hri(i),hp(j),hpi(j),hz(k),hzi(k),&
