f6f48b9fca3df65b45b4a74c390d4af6636a2b12
lazersos <lazersos@gmail.com>
2025-02-19 10:11:40 +0100

    pySTEL: Added support for the B10/B11 target.

--- pySTEL/STELLOPT.py
+++ pySTEL/STELLOPT.py
@@ -992,7 +992,7 @@ class MyApp(QMainWindow):
 				self.stel_data.read_stellopt_varlabels()
 				self.stel_data.plot_stellopt_jacobian(target='all',ax=self.ax2)
 				self.canvas2.draw()
-			elif plot_name in ['Boozer Spectrum','Boozer |B|']:
+			elif plot_name in ['Boozer Spectrum','Boozer |B|','B10/B11']:
 				self.ui.ComboBoxOPTplot_surf.clear()
 				self.booz_data = boozer.BOOZER()
 				self.booz_data.read_boozer(test_file)
@@ -1016,6 +1016,8 @@ class MyApp(QMainWindow):
 		if plot_name == 'Boozer Spectrum':
 			self.booz_data.plotBmnSpectrum(plot_k-1,ax=self.ax2)
 		elif plot_name == 'Boozer |B|':
+			self.booz_data.plotBsurf(plot_k-1,ax=self.ax2)
+		elif plot_name == 'B10/B11':
 			self.booz_data.plotBsurf(plot_k-1,ax=self.ax2)
 		self.canvas2.draw()
 
@@ -1124,6 +1126,20 @@ class MyApp(QMainWindow):
 			self.ax2.set_xlabel('Radial Grid')
 			self.ax2.set_ylabel('Epsilon Effective')
 			self.ax2.set_title('Neoclassical Helical Ripple (NEO)')
+			self.ax2.legend()
+		elif (plot_name == 'B10B11_evolution'):
+			x = self.stel_data.B10B11_K
+			y = self.stel_data.B10B11_VAL
+			t = self.stel_data.B10B11_TARGET
+			d = self.stel_data.B10B11_SIGMA
+			self.ax2.errorbar(x[0,:],t[0,:],yerr=d[0,:],fmt='ok',fillstyle='none',label='Target')
+			self.ax2.plot(x[0,:],y[0,:],'o',fillstyle='none',label='Initial',color='red')
+			for i in range(1,niter-1,1):
+				self.ax2.plot(x[i,:],y[i,:],'.k',fillstyle='none')
+			self.ax2.plot(x[niter-1,:],y[niter-1,:],'o',fillstyle='none',label='Final',color='green')
+			self.ax2.set_xlabel('Radial Grid')
+			self.ax2.set_ylabel(r'$B_{01}/B_{11}$')
+			self.ax2.set_title('Boozer Harmonic Ratios')
 			self.ax2.legend()
 		elif (plot_name == 'BOOTSTRAP_evolution'):
 			x = self.stel_data.BOOTSTRAP_RHO # actually flux
@@ -1878,6 +1894,27 @@ class MyApp(QMainWindow):
 			for item in file_list:
 				self.ui.ComboBoxOPTplot_iter.addItem(item)
 			self.UpdateIterFile()
+		elif (plot_name == 'B10/B11'):
+			#file_list = sorted(glob.glob("boozmn*"))
+			#for item in file_list:
+			#	self.ui.ComboBoxOPTplot_iter.addItem(item)
+			#self.UpdateIterFile()
+			booz_data = boozer.BOOZER()
+			l=0
+			dl = len(self.booz_files)-1
+			if dl == 0 : dl = 1 
+			for string in self.booz_files:
+				if 'boozmn' in string:
+					booz_data.read_boozer(self.workdir+string)
+					b10b11 = booz_data.calcB10B11()
+					s = np.squeeze(booz_data.phi_b)
+					s = s/s[-1]
+					self.ax2.plot(s,b10b11,'o',color=_plt.cm.brg(l/dl))
+					l = l + 1
+			self.ax2.set_xlabel('Norm Tor. Flux (s)')
+			self.ax2.set_ylabel(r'$B_{10}/B_{11}$')
+			self.ax2.set_title('Boozer Spectrum Ratio')
+			self.ax2.set_xlim((0,1))
 		elif (plot_name == 'Pressure'):
 			vmec_data = vmec.VMEC()
 			l=0
@@ -2066,7 +2103,7 @@ class MyApp(QMainWindow):
 			for string in self.booz_files:
 				if 'boozmn' in string:
 					booz_data.read_boozer(self.workdir+string)
-					disp('NOT DONE!')
+					print('NOT DONE!')
 		elif (plot_name == 'QAS_ERROR'):
 			booz_data = boozer.BOOZER()
 			l=0
