06cf3acf92f3728b50910c0aab721c687f1dac99
lazersos <lazersos@gmail.com>
2025-02-26 14:34:31 +0100

    pySTEL: Added more scripts to installation file.

--- pySTEL/setup.py
+++ pySTEL/setup.py
@@ -11,8 +11,11 @@ setup(name='pySTEL',
 	author_email = 'lazersos@gmail.com',
 	url = 'https://github.com/PrincetonUniversity/STELLOPT',
 	packages=['libstell'],
-	scripts = ['VMECplot.py','FIELDLINESplot.py','vmec_util.py',\
-		'boozer_util.py','coils_util.py','fieldlines_util.py'],
+	scripts = ['VMECplot.py','FIELDLINESplot.py', 'bootsj_util.py', \
+		'boozer_util.py','coils_util.py','fieldlines_util.py',\
+		'focus_util.py','gist_util.py','make_mesh.py','nescoil_util.py',\
+		'STELLOPT.py','stellopt_renorm.py','vmec2beams3d.py','vmec2focus.py',\
+		'vmec_util.py','wall_util.py'],
 	install_requires=['numpy','matplotlib','PyQt5','scipy', \
 		'contourpy','PyVTK','numpy-stl','gmsh','vtk']
 	)
