c28e7410d96b516a5933f5284052897295e985fd
lazersos <lazersos@gmail.com>
2025-04-16 13:48:24 +0200

    LIBSTELL: Type modification and expansion of fusion_mod

--- LIBSTELL/Sources/Modules/fusion_mod.f90
+++ LIBSTELL/Sources/Modules/fusion_mod.f90
@@ -9,14 +9,13 @@
 !-----------------------------------------------------------------------
 !     Libraries
 !-----------------------------------------------------------------------
-      USE stel_kinds, ONLY: rprec
 
 !-----------------------------------------------------------------------
 !     Module Variables
 !         
 !-----------------------------------------------------------------------
       IMPLICIT NONE
-      REAL(rprec), PRIVATE, PARAMETER :: &
+      DOUBLE PRECISION, PRIVATE, PARAMETER :: &
                               electron_charge = 1.602176634D-19 !e_c
 !-----------------------------------------------------------------------
       CONTAINS
@@ -30,15 +29,15 @@
             !   RESULT       Bremstrahlung power density [W/m^3]
             !
             ! This func computes the Bremsstrahlung irradiated power density
-		    ! according to Freidberg's Plasma Physics and Fusion Energy, 
-		    ! page 56, Eq. (3.42)
+            ! according to Freidberg's Plasma Physics and Fusion Energy, 
+            ! page 56, Eq. (3.42)
             !--------------------------------------------------------------
             DOUBLE PRECISION, INTENT(in) :: ni,ne,Te
             INTEGER, INTENT(in) :: Z
             DOUBLE PRECISION :: result, CB, n20, Tk, Zeff
-            CB = 5.35E3_rprec
-            n20 = ne / 1.0E20_rprec
-            Tk = Te / 1.0E3_rprec 
+            CB = 5.35D3
+            n20 = ne / 1.0D20
+            Tk = Te / 1.0D3 
             Zeff = Z*Z * ni/ne
             result = CB * Zeff * n20**2 * SQRT(Tk)  !W/m^3
             RETURN
@@ -51,27 +50,114 @@
             !
             ! This function calculates the fusion cross section <sigma.v> 
             ! given the ion temperature using the Bosch Hale model.
-		    ! H.-S. Bosch and G. M. Hale 1992 Nucl. Fusion 32 611
-		    ! https://doi.org/10.1088/0029-5515/32/4/I07
+            ! H.-S. Bosch and G. M. Hale 1992 Nucl. Fusion 32 611
+            ! https://doi.org/10.1088/0029-5515/32/4/I07
             !--------------------------------------------------------------
             DOUBLE PRECISION, INTENT(in) :: Ti
             INTEGER, PARAMETER :: mrc2 = 1124656
             DOUBLE PRECISION, PARAMETER :: BG = 34.3827
             DOUBLE PRECISION, DIMENSION(7), PARAMETER :: &
-                CARR = (/ 1.17302E-09,  1.51361E-02,  7.51886E-02, &
-                          4.60643E-03,  1.35000E-02, -1.06750E-04, &
-                          1.36600E-05/)
+                CARR = (/ 1.17302D-09,  1.51361D-02,  7.51886D-02, &
+                          4.60643D-03,  1.35000D-02, -1.06750D-04, &
+                          1.36600D-05/)
             DOUBLE PRECISION :: Tk, zeta, theta, eta, result
 
-            Tk = Ti*1E-3_rprec ! to keV
-            zeta =  1.0_rprec - ((((CARR(6)*Tk)+CARR(4))*Tk+CARR(2))*Tk)/ &
-                        ((((CARR(7)*Tk)+CARR(5))*Tk+CARR(3))*Tk+1.0_rprec)
+            Tk = Ti*1.0D-3 ! to keV
+            zeta =  1.0D0 - ((((CARR(6)*Tk)+CARR(4))*Tk+CARR(2))*Tk)/ &
+                        ((((CARR(7)*Tk)+CARR(5))*Tk+CARR(3))*Tk+1.0D0)
             theta = Tk/zeta
-            eta   = (BG*BG/(4*theta))**(1.0_rprec/3._rprec)
+            eta   = (BG*BG/(4*theta))**(1.0D0/3.0D0)
     
-            result = 1.0E-6_rprec*CARR(1)*theta*SQRT(eta/(mrc2*Tk*Tk*Tk))*EXP(-3*eta)
+            result = 1.0D-6*CARR(1)*theta*SQRT(eta/(mrc2*Tk*Tk*Tk))*EXP(-3*eta)
             RETURN
         END FUNCTION DT_CROSS_SECTION
+
+        FUNCTION DD_CROSS_SECTION(Ti) RESULT(result)
+            !--------------------------------------------------------------
+            !   TI           Ion Temperature [eV]
+            !   RESULT       Cross section <sigma.v> [m^3/s]
+            !
+            ! This function calculates the fusion cross section <sigma.v> 
+            ! given the ion temperature using the Bosch Hale model.
+            ! H.-S. Bosch and G. M. Hale 1992 Nucl. Fusion 32 611
+            ! https://doi.org/10.1088/0029-5515/32/4/I07
+            !--------------------------------------------------------------
+            DOUBLE PRECISION, INTENT(in) :: Ti
+            INTEGER, PARAMETER :: mrc2 = 937814
+            DOUBLE PRECISION, PARAMETER :: BG = 31.3970
+            DOUBLE PRECISION, DIMENSION(7), PARAMETER :: &
+                CARR = (/ 5.65718D-12,  3.41267D-03,  1.99167D-03, &
+                          0.00000D+00,  1.05060D-05,  0.00000D+00, &
+                          0.00000D-00/)
+            DOUBLE PRECISION :: Tk, zeta, theta, eta, result
+
+            Tk = Ti*1.0D-3 ! to keV
+            zeta =  1.0D0 - ((((CARR(6)*Tk)+CARR(4))*Tk+CARR(2))*Tk)/ &
+                        ((((CARR(7)*Tk)+CARR(5))*Tk+CARR(3))*Tk+1.0D0)
+            theta = Tk/zeta
+            eta   = (BG*BG/(4*theta))**(1.0D0/3.0D0)
+    
+            result = 1.0D-6*CARR(1)*theta*SQRT(eta/(mrc2*Tk*Tk*Tk))*EXP(-3*eta)
+            RETURN
+        END FUNCTION DD_CROSS_SECTION
+
+        FUNCTION DDHe3_CROSS_SECTION(Ti) RESULT(result)
+            !--------------------------------------------------------------
+            !   TI           Ion Temperature [eV]
+            !   RESULT       Cross section <sigma.v> [m^3/s]
+            !
+            ! This function calculates the fusion cross section <sigma.v> 
+            ! given the ion temperature using the Bosch Hale model.
+            ! H.-S. Bosch and G. M. Hale 1992 Nucl. Fusion 32 611
+            ! https://doi.org/10.1088/0029-5515/32/4/I07
+            !--------------------------------------------------------------
+            DOUBLE PRECISION, INTENT(in) :: Ti
+            INTEGER, PARAMETER :: mrc2 = 937814
+            DOUBLE PRECISION, PARAMETER :: BG = 31.3970
+            DOUBLE PRECISION, DIMENSION(7), PARAMETER :: &
+                CARR = (/ 5.43360D-12,  5.85778D-03,  7.68222D-03, &
+                          0.00000D+00, -2.96400D-06,  0.00000D+00, &
+                          0.00000D+00/)
+            DOUBLE PRECISION :: Tk, zeta, theta, eta, result
+
+            Tk = Ti*1.0D-3 ! to keV
+            zeta =  1.0D0 - ((((CARR(6)*Tk)+CARR(4))*Tk+CARR(2))*Tk)/ &
+                        ((((CARR(7)*Tk)+CARR(5))*Tk+CARR(3))*Tk+1.0D0)
+            theta = Tk/zeta
+            eta   = (BG*BG/(4*theta))**(1.0D0/3.0D0)
+    
+            result = 1.0D-6*CARR(1)*theta*SQRT(eta/(mrc2*Tk*Tk*Tk))*EXP(-3*eta)
+            RETURN
+        END FUNCTION DDHe3_CROSS_SECTION
+
+        FUNCTION DHe3_CROSS_SECTION(Ti) RESULT(result)
+            !--------------------------------------------------------------
+            !   TI           Ion Temperature [eV]
+            !   RESULT       Cross section <sigma.v> [m^3/s]
+            !
+            ! This function calculates the fusion cross section <sigma.v> 
+            ! given the ion temperature using the Bosch Hale model.
+            ! H.-S. Bosch and G. M. Hale 1992 Nucl. Fusion 32 611
+            ! https://doi.org/10.1088/0029-5515/32/4/I07
+            !--------------------------------------------------------------
+            DOUBLE PRECISION, INTENT(in) :: Ti
+            INTEGER, PARAMETER :: mrc2 = 1124572
+            DOUBLE PRECISION, PARAMETER :: BG = 68.7508
+            DOUBLE PRECISION, DIMENSION(7), PARAMETER :: &
+                CARR = (/ 5.51036D-10,  6.41918D-03, -2.02896D-03, &
+                         -1.91080D-05,  1.35776D-04,  0.00000D+00, &
+                          0.00000D+00/)
+            DOUBLE PRECISION :: Tk, zeta, theta, eta, result
+
+            Tk = Ti*1.0D-3 ! to keV
+            zeta =  1.0D0 - ((((CARR(6)*Tk)+CARR(4))*Tk+CARR(2))*Tk)/ &
+                        ((((CARR(7)*Tk)+CARR(5))*Tk+CARR(3))*Tk+1.0D0)
+            theta = Tk/zeta
+            eta   = (BG*BG/(4*theta))**(1.0D0/3.0D0)
+    
+            result = 1.0D-6*CARR(1)*theta*SQRT(eta/(mrc2*Tk*Tk*Tk))*EXP(-3*eta)
+            RETURN
+        END FUNCTION DHe3_CROSS_SECTION
 
         FUNCTION ALPHA_POWER(nD,nT,TD,TT) RESULT(result)
             !--------------------------------------------------------------
@@ -85,14 +171,13 @@
             ! nD*nT*<sigma.v>*E_alpha
             !--------------------------------------------------------------
             DOUBLE PRECISION, INTENT(in) :: nD,nT,TD,TT
-            DOUBLE PRECISION, PARAMETER :: E_alpha = 3.52E6*electron_charge
+            DOUBLE PRECISION, PARAMETER :: E_alpha = 3.52D6*electron_charge
             DOUBLE PRECISION :: Ti, sigmav, result
-		    Ti = 0.5_rprec * (TD+TT)
+            Ti = 0.5D+00 * (TD+TT)
             sigmav = DT_CROSS_SECTION(Ti) ! m^3/s
-		    result = nD * nT * sigmav * E_alpha ! W/m^3
+            result = nD * nT * sigmav * E_alpha ! W/m^3
             RETURN
         END FUNCTION ALPHA_POWER
-
 
     END MODULE fusion_mod
 
