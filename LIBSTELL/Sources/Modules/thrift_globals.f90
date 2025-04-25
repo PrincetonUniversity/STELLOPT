79a744d960b374c0375f88f30583fb2a3391cd62
ajchcoelho <antonio.coelho8@gmail.com>
2025-04-25 11:37:24 +0200

    THRIFT: Remove cp_all and cn_ions from globals

--- LIBSTELL/Sources/Modules/thrift_globals.f90
+++ LIBSTELL/Sources/Modules/thrift_globals.f90
@@ -54,8 +54,8 @@
       INTEGER, PARAMETER :: nions_max = 6
       REAL(rprec) :: dt_plasma_solver, tol_plasma_solver
       INTEGER :: max_subiter_plasma_solver, Nr_plasma_solver
-      REAL(rprec), DIMENSION(nions_max+1) :: chi_all, cp_all
-      REAL(rprec), DIMENSION(nions_max) :: Dn_ions, cn_ions
+      REAL(rprec), DIMENSION(nions_max+1) :: chi_all
+      REAL(rprec), DIMENSION(nions_max) :: Dn_ions
 
       CONTAINS
 
