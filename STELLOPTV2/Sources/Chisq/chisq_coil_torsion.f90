af6159502ace66a781128ad0f3db9257cd4c7f74
Samuel Lazerson <lazerson@gmail.com>
2025-06-17 16:58:08 +0200

    STELLOPT: Modified how coil curvature and torsion chisq are calculated.

--- STELLOPTV2/Sources/Chisq/chisq_coil_torsion.f90
+++ STELLOPTV2/Sources/Chisq/chisq_coil_torsion.f90
@@ -11,7 +11,8 @@
 !-----------------------------------------------------------------------
       USE stellopt_runtime
       USE stellopt_targets
-      USE spline_coils_mod, ONLY: get_coil_torsion
+      USE stellopt_vars, ONLY: nw_coil, nh_coil, rho_coil_kts
+      USE spline_coils_mod, ONLY: get_coil_torsion, get_coil_dl
       
 !-----------------------------------------------------------------------
 !     Input/Output Variables
@@ -27,29 +28,41 @@
 !     Local Variables
 !
 !-----------------------------------------------------------------------
-      INTEGER :: ncoilgroups, ntotal_coils,i,j,nc,nc1
-      REAL(rprec) :: torsion_mean, torsion_min, torsion_max, hs
-      REAL(rprec), DIMENSION(:), ALLOCATABLE :: xc,yc,zc,xcp,ycp,zcp, &
-         xcpp,ycpp,zcpp,xcppp,ycppp,zcppp,torsion
-      
+      INTEGER :: numcoilgroups, k, n
+      REAL(rprec) :: tors, tors_hold, tors0, dl, L, val
 !----------------------------------------------------------------------
 !     BEGIN SUBROUTINE
 !----------------------------------------------------------------------
       IF (iflag < 0) RETURN
-      IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'COIL_TORSION ',1,5
-      IF (iflag == 1) WRITE(iunit_out,'(A)') 'TARGET  SIGMA  MEAN  MIN  MAX'
+      numcoilgroups = COUNT(ANY(rho_coil_kts>0,DIM=2))
+      IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'COIL_TORSION ',numcoilgroups*nw_coil*nh_coil,3
+      IF (iflag == 1) WRITE(iunit_out,'(A)') 'TARGET  SIGMA  MEAN'
       IF (niter >= 0) THEN
-         torsion_min = 0.0; torsion_max = 0.0; torsion_mean = 0.0
-         CALL get_coil_torsion(torsion_mean,torsion_max,torsion_min)
-         mtargets = mtargets + 1
-         targets(mtargets) = target
-         sigmas(mtargets)  = sigma
-         vals(mtargets)     = torsion_mean
-         IF (iflag == 1) WRITE(iunit_out,'(5ES22.12E3)') target,sigma,torsion_mean,torsion_min,torsion_max
+         numcoilgroups = COUNT(ANY(rho_coil_kts>0,DIM=2))
+         DO k = 1, numcoilgroups*nw_coil*nh_coil
+            CALL get_coil_torsion(k,1,tors0)
+            CALL get_coil_dl(k,1,dl)
+            tors0 = tors0 * dl
+            val = tors0; L = dl
+            DO n = 2, 128
+               CALL get_coil_torsion(k,n,tors)
+               CALL get_coil_dl(k,n,dl)
+               val = val + tors * dl
+               L   = L + dl
+            END DO
+            mtargets = mtargets + 1
+            targets(mtargets) = target
+            sigmas(mtargets)  = sigma
+            vals(mtargets)    = (val-tors0)/DBLE(128*L)
+            IF (iflag == 1) WRITE(iunit_out,'(5ES22.12E3)') target,sigma,vals(mtargets)
+         END DO
       ELSE
          IF (sigma < bigno) THEN
-            mtargets = mtargets + 1
-            IF (niter == -2) target_dex(mtargets)=jtarget_coil_torsion
+            numcoilgroups = COUNT(ANY(rho_coil_kts>0,DIM=2))
+            DO k = 1, numcoilgroups*nw_coil*nh_coil
+               mtargets = mtargets + 1
+               IF (niter == -2) target_dex(mtargets)=jtarget_coil_torsion
+            END DO
          END IF
       END IF
       RETURN
