ee4e7f99911cc37e7ef1e8435eec840fc43687ad
Samuel Lazerson <lazerson@gmail.com>
2025-10-17 20:41:48 +0200

    NESCOIL: Coil cutting method added to interface.

--- NESCOIL/Sources/nescoil_stellopt_mod.f90
+++ NESCOIL/Sources/nescoil_stellopt_mod.f90
@@ -9,7 +9,6 @@
 !-----------------------------------------------------------------------
 !     Libraries
 !-----------------------------------------------------------------------
-      USE stel_kinds, ONLY: rprec
       USE Vmeshes
       USE Vprecal1
       USE Vvacuum2, ONLY: iota_edge, phip_edge, curpol, ms1, ns1, cr1, &
@@ -22,11 +21,16 @@
       USE Vbiopre,  ONLY: vx, vy, vz, dx, dy, dz
       USE Vculine1, ONLY: xw, yw, zw, curre, nw
       USE Vprecal4, ONLY: cok, sik
-      USE NumParams, ONLY : inesc, zero
+      USE NumParams, ONLY : inesc, zero, one, pi2
+      USE Vdiagno2, ONLY: pot
+      USE bsc_T
+      USE biotsavart, ONLY: coil_group, nfp => nfp_bs
+      USE stel_kinds, ONLY: rprec
 !-----------------------------------------------------------------------
 !     Module Variables
 !-----------------------------------------------------------------------
       IMPLICIT NONE
+      DOUBLE PRECISION, PRIVATE :: factor
       
 !-----------------------------------------------------------------------
 !     Module SUBROUTINES/FUNCTIONS
@@ -69,6 +73,7 @@
       iota_edge = iota_edge_in
       phip_edge = phip_edge_in
       curpol = curpol_in
+      factor = pi2/np
       RETURN
       END SUBROUTINE set_nescoil_plasma
 
@@ -130,10 +135,10 @@
       RETURN
       END SUBROUTINE set_nescoil_plasma_boundary
 
-      SUBROUTINE set_nescoil_current_surface(mnmax_in,xm_in,xn_in,rbc_in,zbs_in,lbs_in)
+      SUBROUTINE set_nescoil_current_surface(mnmax_in,xm_in,xn_in,rbc_in,zbs_in)
       IMPLICIT NONE
       INTEGER, INTENT(in) :: mnmax_in
-      REAL(rprec), DIMENSION(mnmax_in), INTENT(in) :: xm_in, xn_in, rbc_in, zbs_in, lbs_in
+      REAL(rprec), DIMENSION(mnmax_in), INTENT(in) :: xm_in, xn_in, rbc_in, zbs_in
       INTEGER :: mn, m, n
       ms = INT(MAXVAL(ABS(xm_in)))
       ns = INT(MAXVAL(ABS(xn_in)))
@@ -223,6 +228,120 @@
       RETURN
  100  format('DATE = ',a2,'-',a2,'-',a4,' ',' TIME = ',2(a2,':'),a2)
       END SUBROUTINE run_nescoil
+
+      SUBROUTINE cut_nescoil(ncoils)
+      IMPLICIT NONE
+      INTEGER, INTENT(in) :: ncoils
+      INTEGER :: m, n, i, j, k, mn
+      REAL(rprec) :: u, v, arg, potmax, potmin, level, cop, sip
+      REAL(rprec), DIMENSION(:), ALLOCATABLE :: xu,xv, theta_coil, zeta_coil, &
+                                                r_coil, z_coil
+      REAL(rprec), DIMENSION(:,:), ALLOCATABLE :: potuv, xnod_in, xnod_ss, xnod_fp
+      CHARACTER(len=100) :: s_name
+      CHARACTER(len=100) :: l_name
+      CHARACTER(len=100) :: c_name
+      TYPE(bsc_coil)     :: coil_temp
+      TYPE(bsc_rs)       :: rot_mat
+      ! Fourier transform pot
+      ALLOCATE(xu(nu),xv(nv),potuv(nu,nv))
+      potuv = 0.0
+      FORALL(i=1:nu) xu(i) = DBLE(i-1)/DBLE(nu-1)
+      FORALL(j=1:nv) xv(j) = 0.5*DBLE(j-1)/DBLE(nv-1)
+      DO i = 1, nu
+         DO j = 1, nv
+            DO m = 0, mf
+               DO n = -nf, nf
+                  arg = pi2*(m*xu(i)+n*xv(j))
+                  potuv(i,j) = potuv(i,j) + pot(m,n)*SIN(arg)
+               END DO
+            END DO
+         END DO
+      END DO
+      ! Do secular parts
+      FORALL(i=1:nu) potuv(i,:) = potuv(i,:) - cut*0.5*xu(i)
+      FORALL(j=1:nv) potuv(:,j) = potuv(:,j) - cup*0.5*xv(i)
+      ! Compute min/max
+      potmax = MAXVAL(potuv)
+      potmin = MINVAL(potuv)
+      IF (ALLOCATED(theta_coil)) DEALLOCATE(theta_coil)
+      IF (ALLOCATED(zeta_coil)) DEALLOCATE(zeta_coil)
+      ALLOCATE(theta_coil(1),zeta_coil(1))
+      m = 0
+      ! Find longest paths
+      DO i = 1, ncoils
+         level = potmin + (potmax-potmin)*(i-0.5)/ncoils
+         CALL conrec(potuv,1,nu,1,nv,xu,xv,level,0,n,theta_coil,zeta_coil)
+         m = MAX(n,m)
+      END DO
+      DEALLOCATE(theta_coil,zeta_coil)
+      ! Deallocated the coils if allocated
+      IF (ALLOCATED(coil_group)) THEN
+         DO i = 1, SIZE(coil_group)
+            CALL bsc_destroy(coil_group(i))
+         END DO
+         DEALLOCATE(coil_group)
+      END IF
+      ! Allocate the coilgroups and helpers
+      ALLOCATE(coil_group(ncoils))
+      ALLOCATE(theta_coil(m),zeta_coil(m))
+      theta_coil = 0; zeta_coil = -3.14
+      ! Compute the coils
+      DO i = 1, ncoils
+         WRITE(l_name,*) 'i = ',i
+         WRITE(c_name,'(A,I2.2)') 'MODULAR_COIL_',i
+         CALL bsc_construct_coilcoll(coil_group(i),TRIM(c_name),l_name)
+         level = potmin + (potmax-potmin)*(i-0.5)/ncoils
+         CALL conrec(potuv,1,nu,1,nv,xu,xv,level,1,k,theta_coil,zeta_coil)
+         ALLOCATE(r_coil(n),z_coil(n))
+         r_coil = 0.0
+         z_coil = 0.0
+         ! Fourier Transform
+         DO j = 1, k
+            DO m = 0, mf
+               DO n = -nf, nf
+                  arg = pi2*(m*theta_coil(j)+n*zeta_coil(j))
+                  r_coil(j) = r_coil(j) + cr(m,n)*COS(arg)
+                  z_coil(j) = z_coil(j) + cz(m,n)*SIN(arg)
+               END DO
+            END DO
+         END DO
+         ALLOCATE(xnod_in(3,k),xnod_ss(3,k),xnod_fp(3,k))
+         xnod_in(1,:) = r_coil * COS(zeta_coil*factor)
+         xnod_in(2,:) = r_coil * SIN(zeta_coil*factor)
+         xnod_in(3,:) = z_coil
+         xnod_in(:,k) = xnod_in(:,1)
+         ! Now create the first coil
+         WRITE(s_name, '(a4,i5.5)') 'ID #', 1
+         CALL bsc_construct_coil(coil_temp,'fil_loop',s_name,'',one,xnod_in(1:3,1:k))
+         CALL bsc_append(coil_group(i),coil_temp)
+         ! Now create the stellarator symmetric coil
+         xnod_ss(1,:) = r_coil * COS((1.0-zeta_coil)*factor)
+         xnod_ss(2,:) = r_coil * SIN((1.0-zeta_coil)*factor)
+         xnod_ss(3,:) =-z_coil
+         xnod_ss(:,2:k) = xnod_ss(:,k-1:1:-1)
+         xnod_ss(:,1) = xnod_ss(:,k)
+         CALL bsc_construct_coil(coil_temp,'fil_loop',s_name,'',one,xnod_ss(1:3,1:ns))
+         CALL bsc_append(coil_group(i),coil_temp)
+         DO j = 2, np
+            cop  = cos((j-1)*factor)
+            sip  = sin((j-1)*factor)
+            xnod_fp(1,:) = xnod_in(1,:)*cop - xnod_in(2,:)*sip
+            xnod_fp(2,:) = xnod_in(2,:)*cop + xnod_in(1,:)*sip
+            xnod_fp(3,:) = xnod_in(3,:)
+            CALL bsc_construct_coil(coil_temp,'fil_loop',s_name,'',one,xnod_fp(1:3,1:ns))
+            CALL bsc_append(coil_group(i),coil_temp)
+            xnod_fp(1,:) = xnod_ss(1,:)*cop - xnod_ss(2,:)*sip
+            xnod_fp(2,:) = xnod_ss(2,:)*cop + xnod_ss(1,:)*sip
+            xnod_fp(3,:) = xnod_ss(3,:)
+            CALL bsc_construct_coil(coil_temp,'fil_loop',s_name,'',one,xnod_fp(1:3,1:ns))
+            CALL bsc_append(coil_group(i),coil_temp)
+         END DO
+         DEALLOCATE(xnod_in,xnod_ss,xnod_fp)
+         DEALLOCATE(r_coil,z_coil)
+      END DO
+      DEALLOCATE(xu,xv,potuv)
+      RETURN
+      END SUBROUTINE cut_nescoil
 
 !-----------------------------------------------------------------------
 !     End Module
