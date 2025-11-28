pySTEL: Add covariant tensor computation, fix gradrho2 and add gradrho
The function calc_grad_rhosq was broken. Now it is fixed. Also added calc_grad_rho. A new routine that returns the covariant tensor elements of the VMEC equilibrium has been included

pySTEL/libstell/vmec.py
@@ -149,7 +149,7 @@ class VMEC(FourierRep):
 		Parameters
 		----------
 		theta : ndarray
-			Polidal angle grid [rad]
+			Poloidal angle grid [rad]
 		phi : ndarray
 			Toroidal angle grid [rad]
 		Returns
@@ -199,57 +199,213 @@ class VMEC(FourierRep):
 		dBsqav = np.gradient(Bsqav)
 		return V * ( 8E-7 * np.pi * pp / vp + dBsqav)/Bsqav
 
-	def calc_grad_rhosq(self):
-		"""Compute <|grad(rho)|^2> 
+	def calc_covariant_tensor(self,theta,phi):
+		"""
+		Computes the covariant metric tensor g_ij(s, theta, phi) in VMEC coordinates.
 
-		This routine flux surface average of |grad(rho)|^2 
+		Parameters
+		----------
+		theta : ndarray
+			Poloidal angle grid [rad]
+		phi : ndarray
+			Toroidal angle grid [rad]
+
+		Description
+		-----------
+		This function evaluates the covariant basis vectors
+			e_i = ∂x/∂q^i
+		where q = (s, u, v) are the VMEC coordinates. The covariant vectors are expressed in the Cartesian basis (ex, ey, ez) as:
+
+        e_s = (dR/ds * cos(v)) ex
+              + (dR/ds * sin(v)) ey
+              + (dZ/ds)          ez
+
+        e_u = (dR/du * cos(v)) ex
+              + (dR/du * sin(v)) ey
+              + (dZ/du)          ez
+
+        e_v = [dR/dv * cos(v) - R * sin(v)] ex
+              + [dR/dv * sin(v) + R * cos(v)] ey
+              + (dZ/dv)  ez
+    	
+     	The covariant metric tensor is then constructed as
+
+			g_ij = e_i · e_j ,
+
+		and arranged in the 3x3 block
+
+			g = [ g_ss   g_su   g_sv
+				  g_us   g_uu   g_uv
+				  g_vs   g_vu   g_vv ]
+
+		Each component g_ij is returned on the full VMEC spatial grid and therefore
+		has shape (ns, ntheta, nphi), where ns is the radial grid defined in the
+		VMEC equilibrium and (ntheta, nphi) are determined by the shapes of the
+		input arrays `theta` and `phi`.
 
 		Returns
-		----------
-		avgrho2 : ndarray
-			Flux surface average of |grad(rho)|^2
+		-------
+		g : dict
+			The covariant tensor components returned as:
+				- a dictionary with keys ("ss","su","sv","us","uu","uv","vs","vu","vv"),
+				each mapped to an array g_ij[s,theta,phi]
 		"""
 		import numpy as np
-		nu = 64
-		nv = 128
-		theta = np.linspace(0,2*np.pi,nu)
-		phi   = np.linspace(0,2*np.pi,nv)
-		# Create derivatives
-		xm2d  = np.broadcast_to(self.xm,(self.ns,self.mnmax))
-		xn2d  = np.broadcast_to(self.xn,(self.ns,self.mnmax))
-		rumns = - xm2d * self.rmnc
-		rvmns = - xn2d * self.rmnc
-		zumnc =   xm2d * self.zmns
-		zvmnc =   xn2d * self.zmns
+		R = self.cfunct(theta,phi,self.rmnc,self.xm,self.xn)
+		Z = self.sfunct(theta,phi,self.zmns,self.xm,self.xn)
 		if self.iasym==1:
-			rumnc =   xm2d * self.rmns
-			rvmnc =   xn2d * self.rmns
-			zumns = - xm2d * self.zmnc
-			zvmns = - xn2d * self.zmnc
-		r = self.cfunct(theta,zeta,self.rmnc,self.xm,self.xn)
-		g = self.cfunct(theta,zeta,self.gmnc,self.xm_nyq,self.xn_nyq)
-		ru = self.sfunct(theta,zeta,rumns,self.xm,self.xn)
-		rv = self.sfunct(theta,zeta,rvmns,self.xm,self.xn)
-		zu = self.cfunct(theta,zeta,zumnc,self.xm,self.xn)
-		zv = self.cfunct(theta,zeta,zvmnc,self.xm,self.xn)
-		if self.iasym==1:
-			r  = r  + self.sfunct(theta,zeta,self.rmns,self.xm,self.xn)
-			g  = g  + self.sfunct(theta,zeta,self.gmns,self.xm_nyq,self.xn_nyq)
-			ru = ru + self.cfunct(theta,zeta,rumnc,self.xm,self.xn)
-			rv = rv + self.cfunct(theta,zeta,rvmnc,self.xm,self.xn)
-			zu = zu + self.sfunct(theta,zeta,zumns,self.xm,self.xn)
-			zv = zv + self.sfunct(theta,zeta,zvmns,self.xm,self.xn)
-		# Calc metrics
-		gsr = - zu * r
-		gsp = zu * rv - ru * zv
-		gsz = ru * r
-		gs  = ( gsr * gsr + gsp * gsp + gsz * gsz ) / ( g * g )
-		for i in range(self.ns):
-			gs[i,:,:] = 0.25 * gs[i,:,:] / ( rho[i] * rho[i] )
-		vp = np.sum(g,axis=(1,2))
-		avgrho2 = np.sum(gs * g,axis=(1,2)) / vp
-		avgrho2[1] = 2.0 * avgrho2[1] - avgrho2[2]
+			R = R + self.sfunct(theta,phi,self.rmns,self.xm,self.xn)
+			Z = Z + self.cfunct(theta,phi,self.zmnc,self.xm,self.xn)
+		#
+		s = np.linspace(0,1,self.ns)
+		#
+		dRds = np.gradient(R,s, axis=0)
+		dZds = np.gradient(Z,s, axis=0)
+		#
+		dRdu = np.gradient(R, theta.ravel(), axis=1)
+		dZdu = np.gradient(Z, theta.ravel(), axis=1)
+		#
+		dRdv = np.gradient(R, phi.ravel(), axis=2)
+		dZdv = np.gradient(Z, phi.ravel(), axis=2)
+		#
+		g = {}
+		g['ss'] = dRds**2 + dZds**2
+		g['su'] = dRds*dRdu + dZds*dZdu
+		g['sv'] = dRds*dRdv + dZds*dZdv
+		g['us'] = g['su']
+		g['uu'] = dRdu**2 + dZdu**2
+		g['uv'] = dRdu*dRdv + dZdu*dZdv
+		g['vs'] = g['sv']
+		g['vu'] = g['uv']
+		g['vv'] = dRdv**2 + dZdv**2 + R**2
+		#
+		return g
+ 
+	def calc_grad_rhosq(self):
+		"""
+		Computes the flux-surface average <|∇ρ|²>, where ρ = sqrt(s) and s
+		is the normalized toroidal flux, s = phi_t / phi_t_LCFS
+
+		Description
+		-----------
+		This function computes the quantity
+
+			<|∇ρ|²> = <|∇s|²> / (4 ρ²) ,
+
+		where |∇s|² = g^(ss) = ( g_uu * g_vv - g_uv * g_vu ) / det(g).
+
+		Here g_ij are the covariant metric tensor components,
+		det(g) = J² is the determinant of the covariant metric,
+		and J is the VMEC Jacobian.
+
+		The flux-surface average of any scalar-field Q is given by
+
+			<Q> = (1 / (dV/ds))  ∫₀^{2π} ∫₀^{2π}  J(s,u,v) * Q(s,u,v)  du dv.
+
+		The flux-surface average of |∇ρ|² becomes
+
+			<|∇ρ|²> =
+				(1 / (dV/ds))
+				∫₀^{2π} ∫₀^{2π}
+					J * ( g_uu * g_vv - g_uv * g_vu ) /
+						( J² * 4 ρ² )
+				du dv .
+
+		Returns
+		-------
+		avgrho2 : ndarray
+			An array of size (ns,) containing the flux-surface averaged values
+			<|∇ρ|²> on each VMEC radial grid point s.
+		"""
+		import numpy as np
+		nu = 200
+		nv = 500
+		theta = np.linspace(0,2*np.pi,nu).reshape(-1,1)
+		phi   = np.linspace(0,2*np.pi,nv).reshape(-1,1)
+		s     = np.linspace(0,1,self.ns)
+		rho   = np.sqrt(s)
+		#
+		g = self.calc_covariant_tensor(theta=theta,phi=phi)
+		guu = g['uu']
+		gvv = g['vv']
+		guv = g['uv']
+		#
+		dVds = self.vp[:].flatten() * (4*np.pi*np.pi)
+		# Jacobian
+		J = self.cfunct(theta,phi,self.gmnc,self.xm_nyq,self.xn_nyq)
+		if(self.iasym==1):
+			J = J + self.sfunct(theta,phi,self.gmns,self.xm_nyq,self.xn_nyq)
+		# It's important to keep rho inside the integrand instead of diving afterwards
+		# otherwise the integral will diverge close to the axis
+		integrand = (guu*gvv - guv*guv) / (rho[:,None,None]*rho[:,None,None]*np.abs(J))
+		integral = np.trapz(integrand,x=phi.ravel(),  axis=2)
+		integral = np.trapz(integral, x=theta.ravel(),axis=1)
+		avgrho2 = integral / (4*dVds)
+		# Because at rho=0 the integrand is Nan, we interpolate:
+		avgrho2[0] = ( -avgrho2[2]*rho[1] + avgrho2[1]*rho[2] ) / (rho[2]-rho[1])
+		#		
 		return avgrho2
+
+	def calc_grad_rho(self):
+		"""
+		Computes the flux-surface average <|∇ρ|>, where ρ = sqrt(s) and s
+		is the normalized toroidal flux, s = phi_t / phi_t_LCFS
+
+		Description
+		-----------
+		This function computes the quantity
+
+			<|∇ρ|> = <|∇s|> / (2 ρ) ,
+
+		where |∇s|² = g^(ss) = ( g_uu * g_vv - g_uv * g_vu ) / det(g).
+
+		Here g_ij are the covariant metric tensor components,
+		det(g) = J² is the determinant of the covariant metric,
+		and J is the VMEC Jacobian.
+
+		The flux-surface average of any scalar-field Q is given by
+
+			<Q> = (1 / (dV/ds))  ∫₀^{2π} ∫₀^{2π}  J(s,u,v) * Q(s,u,v)  du dv.
+
+		The flux-surface average of |∇ρ| becomes
+
+			<|∇ρ|> =
+				(1 / (dV/ds))
+				∫₀^{2π} ∫₀^{2π}
+					( sqrt[g_uu * g_vv - g_uv * g_vu] ) /
+						( 2 ρ )
+				du dv .
+
+		Returns
+		-------
+		avgrho : ndarray
+			An array of size (ns,) containing the flux-surface averaged values
+			<|∇ρ|> on each VMEC radial grid point s.
+		"""
+		import numpy as np
+		nu = 200
+		nv = 500
+		theta = np.linspace(0,2*np.pi,nu).reshape(-1,1)
+		phi   = np.linspace(0,2*np.pi,nv).reshape(-1,1)
+		s     = np.linspace(0,1,self.ns)
+		rho   = np.sqrt(s)
+		#
+		g = self.calc_covariant_tensor(theta=theta,phi=phi)
+		guu = g['uu']
+		gvv = g['vv']
+		guv = g['uv']
+		#
+		dVds = self.vp[:].flatten() * (4*np.pi*np.pi)
+		# It's important to keep rho inside the integrand instead of diving afterwards
+		# otherwise the integral will diverge close to the axis
+		integrand = np.sqrt(guu*gvv - guv*guv) / (rho[:,None,None])
+		integral = np.trapz(integrand,x=phi.ravel(),  axis=2)
+		integral = np.trapz(integral, x=theta.ravel(),axis=1)
+		avgrho = integral / (2*dVds)
+		# Because at rho=0 the integrand is Nan, we interpolate:
+		avgrho[0] = ( -avgrho[2]*rho[1] + avgrho[1]*rho[2] ) / (rho[2]-rho[1])
+		#		
+		return avgrho
 
 	def calc_susceptance(self):
 		"""Compute susceptance matrix elements 