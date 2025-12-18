e3a87ea302cafd985b931001a9f9aacd63fd0804
Lucas van Ham <lucas.van.ham@ipp.mpg.de>
2025-12-18 14:27:44 +0100

    BEAMS3D: Out_beams3d_part now imports reaction_lines

--- BEAMS3D/Sources/out_beams3d_part.f90
+++ BEAMS3D/Sources/out_beams3d_part.f90
@@ -25,7 +25,8 @@ SUBROUTINE out_beams3d_part(t, q)
                              dist5d_prof, dist5d_fida, win_dist5d, nsh_prof4, &
                              h2_prof, h3_prof, h4_prof, h5_prof, my_end, &
                              r_h, p_h, z_h, e_h, pi_h, E_by_v, h1_prof, &
-                             charge_lines, mass_lines, mymass_int, mycharge_int
+                             charge_lines, mass_lines, mymass_int, mycharge_int, &
+                             reaction_lines
     USE beams3d_grid
     USE beams3d_physics_mod, ONLY: beams3d_physics_fo, beams3d_physics_boxsim
     USE wall_mod, ONLY: collide, get_wall_ik, get_wall_area
