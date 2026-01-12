THRIFT/pySTEL: Fix sign of DKES_D31
This correction is motivated by comparison with experiments

pySTEL/libstell/dkes.py
@@ -82,10 +82,8 @@ class DKES:
         self.D31_star = self.L31 * np.sqrt(self.Bsq)
         self.D33_star = self.L33 * self.Bsq
         
-        # according to J. Lore documentation and also C. Beidler
-        # this is also what is done internally in PENTA 
-        # (this sign basically sets sign of JBS, and calculations agree w/ SFINCS, so this should be correct!)
-        self.D13_star = self.D31_star
+        # We need to take the negative value in order to get, at the end, the correct sign of JBS.B
+        self.D13_star = -self.D31_star
         
     def check_convergence(self):
     