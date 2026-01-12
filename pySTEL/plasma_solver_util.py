b5c4f339c23fde1d7804c488f25c0f08fc6f23be
Samuel Lazerson <lazerson@gmail.com>
2026-01-12 09:47:06 +0100

    pySTEL: Fixed iota bug in plasma_solver_util.py

--- pySTEL/plasma_solver_util.py
+++ pySTEL/plasma_solver_util.py
@@ -69,8 +69,7 @@ if __name__=="__main__":
 		except:
 			B = solver.B
 		try:
-			ir2o3 = np.argmin(np.abs(solver.rho_grid-2/3))
-			iota = solver.iota[:,ir2o3]
+			iota = solver.iota23
 		except:
 			print('WARNING: Using iota=0.9 to compute ISS04')
 			iota = 0.9
