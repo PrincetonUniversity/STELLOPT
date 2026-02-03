0b33ce97f9ddb4825d5640afb06d22cb0ed600e6
Lucas van Ham <lucas.van.ham@ipp.mpg.de>
2026-02-03 17:21:15 +0100

    MUMAT: Change sync function

--- LIBSTELL/Sources/Modules/mumaterial_mod.f90
+++ LIBSTELL/Sources/Modules/mumaterial_mod.f90
@@ -1863,7 +1863,7 @@
       END IF
       CALL MPI_BARRIER(comm_shar, ierr_mpi)
 
-      END SUBROUTINE
+      END SUBROUTINE mumaterial_calcquad
 
       SUBROUTINE mumaterial_syncM()
       !-----------------------------------------------------------------------
@@ -1892,7 +1892,8 @@
         ALLOCATE(M_local(3,ntet))
         M_local = 0.0
         M_local(:, dom_shar(1:ntet_shar)) = M(:, dom_shar(1:ntet_shar))
-        CALL MPI_ALLREDUCE(M_local, M, 3*ntet, MPI_DOUBLE_PRECISION, MPI_SUM, comm_master, ierr_mpi )
+        CALL MPI_ALLREDUCE(MPI_IN_PLACE, M_local, 3*ntet, MPI_DOUBLE_PRECISION, MPI_SUM, comm_master, ierr_mpi )
+        M = M_local
         DEALLOCATE(M_local)
       END IF
       CALL MPI_BARRIER( comm_shar, ierr_mpi)
