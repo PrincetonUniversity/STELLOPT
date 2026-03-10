a10065e840844b73b9bc456ec0b9986ec17e89cc
Rohan Ramasamy <rramasamy@proximafusion.com>
2026-03-10 23:03:29 +0100

    Improved floating point precision to remove incorrect array indexing when single point precision is exceeded in high resolution grids

--- FIELDLINES/Sources/fieldlines_init_mgrid.f90
+++ FIELDLINES/Sources/fieldlines_init_mgrid.f90
@@ -108,8 +108,8 @@
          DO s = mystart, myend
             i = MOD(s-1,nr)+1
             j = MOD(s-1,nr*nphi)
-            j = FLOOR(REAL(j) / REAL(nr))+1
-            k = CEILING(REAL(s) / REAL(nr*nphi))
+            j = FLOOR(REAL(j, rprec) / REAL(nr, rprec)) + 1
+            k = CEILING(REAL(s, rprec) / REAL(nr*nphi, rprec))
             CALL mgrid_bcyl(raxis(i), phiaxis(j), zaxis(k), &
                             br, bphi, bz, ier)
             IF (ier /= 0) stop 'mgrid_bcyl error!'
