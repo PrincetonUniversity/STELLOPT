4a68543b67a5023c23a9598dce09d6b9947b1a63
Samuel Lazerson <lazerson@gmail.com>
2026-05-27 11:50:09 +0200

    pySTEL: Added missing omeg_aux_s and omeg_aux_f to libstell interface.

--- pySTEL/libstell/libstell.py
+++ pySTEL/libstell/libstell.py
@@ -509,8 +509,8 @@ class LIBSTELL():
 		realList.extend(['ne_aux_s', 'te_aux_s', 'ti_aux_s', \
 						 'ne_aux_f', 'te_aux_f', 'ti_aux_f', \
 						 'pot_aux_s', 'pot_aux_f', 'zeff_aux_s', \
-						 'zeff_aux_f'])
-		realLen.extend([(maxproflen,1)]*10)
+						 'zeff_aux_f','omeg_aux_s','omeg_aux_f'])
+		realLen.extend([(maxproflen,1)]*12)
 		realList.extend(['ni_aux_s', 'ni_aux_f', 'ni_aux_m'])
 		realLen.extend([(maxproflen,1),(nion,maxproflen),(nion,1)])
 		module_name = self.s1+'beams3d_globals_'+self.s2
