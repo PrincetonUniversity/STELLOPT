508ab8428e32a68d4b6f53e4b4cff98eb337e0f9
ajchcoelho <antonio.coelho8@gmail.com>
2026-06-11 11:57:00 +0200

    STELLOPT: Apply same changes done in thrift_penta

--- STELLOPTV2/Sources/General/stellopt_penta.f90
+++ STELLOPTV2/Sources/General/stellopt_penta.f90
@@ -267,7 +267,8 @@
       CALL PENTA_SCREEN_INFO
       CALL PENTA_ALLOCATE_DKESCOEFF
       CALL PENTA_FIT_DXX_COEF
-      CALL PENTA_FIT_RAD_TRANS
+      CALL PENTA_LMAT_MATRIX
+      CALL PENTA_SET_INTEGRATION_ARRAYS
 
       ! Technically speaking proc_string contains the unique name of this equilibrium 
       WRITE(temp_str,'(A,A,i4.4)') TRIM(proc_string),'_k',ii
