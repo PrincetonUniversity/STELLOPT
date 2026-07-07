75ade15ffb66419ee6fd325dfd852b70014a6b6f
WenyinWei <wenyin.wei.ww@gmail.com>
2026-07-07 14:24:27 +0800

    Avoid over-specifying VMEC current metadata

--- LIBSTELL/Sources/Modules/read_wout_mod.f90
+++ LIBSTELL/Sources/Modules/read_wout_mod.f90
@@ -219,10 +219,10 @@
         ln_bsubvmns = 'sinmn covariant v-component of B, half mesh',    &
         ln_bsubsmnc = 'cosmn covariant s-component of B, half mesh',    &
 
-        ln_currumnc = 'cosmn sqrt(g)*contravariant u-component of J, full mesh', &
-        ln_currumns = 'sinmn sqrt(g)*contravariant u-component of J, full mesh', &
-        ln_currvmnc = 'cosmn sqrt(g)*contravariant v-component of J, full mesh', &
-        ln_currvmns = 'sinmn sqrt(g)*contravariant v-component of J, full mesh', &
+        ln_currumnc = 'cosmn contravariant u-component of J, full mesh', &
+        ln_currumns = 'sinmn contravariant u-component of J, full mesh', &
+        ln_currvmnc = 'cosmn contravariant v-component of J, full mesh', &
+        ln_currvmns = 'sinmn contravariant v-component of J, full mesh', &
 
         ln_bsubumns_sur = 'sinmn covaiant u-component of B, surface',   &
         ln_bsubvmns_sur = 'sinmn covaiant v-component of B, surface',   &
